3343acfacc5c1c11387a329628712ea23ad44ef1
Daniel Noland <daniel@githedgehog.com>
2024-11-12 13:48:30 -0700

    Add in proper test runner
    This allows the tests to be run with the minimum privileges required by production.

---
+++ scripts/runner.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+set -euo pipefail
+
+# this is a workaround for PATH getting set in config.toml
+export PATH="${PATH}:/usr/bin:/bin:/sbin"
+
+declare -r CAPS="cap_net_raw,cap_net_admin,cap_sys_rawio"
+
+if [ -x "${1}" ]; then
+  sudo setcap "${CAPS}=+ep" "${1}"
+  # shellcheck disable=SC2064
+  trap "sudo setcap ${CAPS}=-ep ${1}" EXIT
+else
+  >&2 echo "${1} not executable"
+  exit 1
+fi
+"${@}"
