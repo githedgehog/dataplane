b612906ea11c24c241e16a50302003adffccbae5
Fredi Raspall <fredi@githedgehog.com>
2025-03-03 14:02:28 +0100

    feat(routing): ignore ifindex evpn route
    The ifindex learnt via the CPI is of no interest as it refers to
    a bridge which we don't need.
    
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

--- routing/src/rpc_adapt.rs
+++ routing/src/rpc_adapt.rs
@@ -57,16 +57,18 @@ impl From<ForwardAction> for FwAction {
 }
 impl From<&NextHop> for RouteNhop {
     fn from(nh: &NextHop) -> Self {
+        let mut ifindex = nh.ifindex;
         let mut encap = nh.encap.as_ref().map(Encapsulation::from);
         if let Some(Encapsulation::Vxlan(vxlan)) = &mut encap {
             if let Some(address) = nh.address {
                 vxlan.remote = address;
             }
+            ifindex = None
         }
         RouteNhop {
             key: NhopKey::new(
                 nh.address,
-                nh.ifindex,
+                ifindex,
                 encap, /* fixme */
                 FwAction::from(nh.fwaction),
             ),
