17a92c2b3a7f609defa86788a3e499ed4c472228
Daniel Noland <daniel@githedgehog.com>
2025-05-12 15:41:40 -0600

    fix: align RouteTableId type
    This is part of an ongoing effort to align the types between routing and net crates
    
    Signed-off-by: Daniel Noland <daniel@githedgehog.com>

--- mgmt/src/processor/confbuild.rs
+++ mgmt/src/processor/confbuild.rs
@@ -4,12 +4,12 @@
 #[allow(unused)]
 use tracing::{debug, error};
 
-use routing::prefix::Prefix;
-use std::net::Ipv4Addr;
-
 use crate::models::external::overlay::vpc::Vpc;
 use crate::models::external::overlay::vpcpeering::VpcManifest;
 use crate::models::external::{ConfigError, overlay::Overlay};
+use net::route::RouteTableId;
+use routing::prefix::Prefix;
+use std::net::Ipv4Addr;
 
 use crate::models::external::gwconfig::{ExternalConfig, GwConfig};
 
@@ -154,7 +154,7 @@ fn vpc_vrf_config(vpc: &Vpc, asn: u32, router_id: Option<Ipv4Addr>) -> VrfConfig
     /* set table-id: table ids should be unique per VRF. We should track them and pick unused ones.
     Setting this to the VNI is not too bad atm, except that we should avoid picking reserved values
     which may cause internal failures. FIXME: fredi */
-    vrf_cfg = vrf_cfg.set_table_id(vpc.vni.as_u32());
+    vrf_cfg = vrf_cfg.set_table_id(RouteTableId::from(*vpc.vni.as_ref()));
 
     /* build BGP config for vrf */
     vrf_cfg.set_bgp(vpc_vrf_bgp_config(vpc, asn, router_id));
