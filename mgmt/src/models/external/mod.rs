8543cec1cfd37b23472e28789e06ede06405555a
Daniel Noland <daniel@githedgehog.com>
2025-05-12 17:44:47 -0600

    fix: improve type precision in error
    Signed-off-by: Daniel Noland <daniel@githedgehog.com>

--- mgmt/src/models/external/mod.rs
+++ mgmt/src/models/external/mod.rs
@@ -7,6 +7,7 @@ pub mod overlay;
 use crate::models::external::gwconfig::GenId;
 use crate::models::external::overlay::vpc::VpcId;
 
+use net::vxlan::{InvalidVni, Vni};
 use thiserror::Error;
 
 /// The reasons why we may reject a configuration
@@ -17,13 +18,13 @@ pub enum ConfigError {
     #[error("A VPC with id '{0}' already exists")]
     DuplicateVpcId(VpcId),
     #[error("VNI '{0}' is already in use")]
-    DuplicateVpcVni(u32),
+    DuplicateVpcVni(Vni),
     #[error("A VPC peering with id '{0}' already exists")]
     DuplicateVpcPeeringId(String),
     #[error("A VPC peering object refers to non-existent VPC '{0}'")]
     NoSuchVpc(String),
-    #[error("'{0}' is not a valid VNI")]
-    InvalidVpcVni(u32),
+    #[error(transparent)]
+    InvalidVpcVni(InvalidVni),
     #[error("Config with id {0} not found")]
     NoSuchConfig(GenId),
     #[error("A config with id {0} already exists")]
