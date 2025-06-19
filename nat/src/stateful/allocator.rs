503b18659357b53dc7513dc244fb8edcefaddffc
Quentin Monnet <qmo@qmon.net>
2025-06-19 17:46:45 +0100

    feat(nat): Add a (yet non-functional) allocator to struct Nat
    Rework the skeleton that we had for the NAT allocator. Make a
    NatAllocator trait, implement a default allocator (just like we did for
    the NAT session managers). Add one to struct Nat.
    
    Make allocate() a method of the allocator, rather than having the
    "NatPool" indirection to first retrieve a pool and then an IP/port.
    Finding the right pool, if necessary, can remain internal to the
    allocator.
    
    Signed-off-by: Quentin Monnet <qmo@qmon.net>

--- nat/src/stateful/allocator.rs
+++ nat/src/stateful/allocator.rs
@@ -2,22 +2,50 @@
 // Copyright Open Network Fabric Authors
 
 use super::NatIp;
-use super::port::{NatPort, NatPortError};
-use std::collections::HashSet;
+use super::NatTuple;
+use super::port::NatPort;
+use routing::rib::vrf::VrfId;
+use std::collections::{HashMap, HashSet};
 use std::fmt::Debug;
+use std::net::Ipv4Addr;
 
-pub trait NatPool<I: NatIp> {
-    fn allocate(&self) -> Result<(I, Option<NatPort>), NatPortError>;
+pub trait NatAllocator<I: NatIp>: Debug {
+    fn new() -> Self;
+    fn allocate(&self, tuple: &NatTuple<I>) -> Option<(I, Option<NatPort>)>;
 }
 
-#[derive(Debug, Clone)]
-pub struct NatDefaultPool<I: NatIp> {
+#[derive(Debug)]
+pub struct NatDefaultAllocator {
+    pools_v4: HashMap<VrfId, NatPool<Ipv4Addr>>,
+}
+
+impl NatAllocator<Ipv4Addr> for NatDefaultAllocator {
+    fn new() -> Self {
+        Self {
+            pools_v4: HashMap::new(),
+        }
+    }
+
+    fn allocate(&self, tuple: &NatTuple<Ipv4Addr>) -> Option<(Ipv4Addr, Option<NatPort>)> {
+        let pool = self.pools_v4.get(&tuple.vrf_id)?;
+        pool.allocate(tuple)
+    }
+}
+
+impl NatDefaultAllocator {
+    pub fn update(&mut self, pools_v4: HashMap<VrfId, NatPool<Ipv4Addr>>) {
+        self.pools_v4 = pools_v4;
+    }
+}
+
+#[derive(Debug)]
+pub struct NatPool<I: NatIp> {
     ips: HashSet<I>,
     allocated: NatAllocations,
 }
 
-impl<I: NatIp> NatPool<I> for NatDefaultPool<I> {
-    fn allocate(&self) -> Result<(I, Option<NatPort>), NatPortError> {
+impl NatPool<Ipv4Addr> {
+    fn allocate(&self, tuple: &NatTuple<Ipv4Addr>) -> Option<(Ipv4Addr, Option<NatPort>)> {
         todo!()
     }
 }
