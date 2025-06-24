githedgehog/dataplane

commit fa913737810850191732daa304d5e010fbbbceb7

fix(ci): Update compile-env container name after dpdk-sys changes
With commit 879e0e94d629 ("bump(dpdk-sys): automated bump of dpdk-sys")
we recently bumped the commit SHA for the compile environment. This
broke the setup! This is because we had recent changes in the dpdk-sys
repo, that caused the name of the container to change [0], and the
refresh command to fail:

    $ just create-compile-env

    /!\ Latest compile-env not found, try "just refresh-compile-env"

    Unable to find image 'ghcr.io/githedgehog/dpdk-sys/compile-env:d7a124dccb2e0c3d7931b64584f1f06d5d8f321e' locally

We missed it in CI [1], because we recently reshuffled the CI workflows
in the dataplane repository, and apparently we forgot to mark the
workflows as required to merge Pull Requests (this should now be fixed).

Anyway, we also need to fix the container name for the environment
image. Let's do it by appending the profile name, just like dpdk-sys
does now. And do the same for libc-env container names in the justfile
and in test-runner.sh, as well.

[0] githedgehog/dpdk-sys@a5c9ebd9a8f27b48e74b1c7a97aa08b12080eb4d
[1] https://github.com/githedgehog/dataplane/pull/620#issuecomment-2995983655

Signed-off-by: Quentin Monnet <qmo@qmon.net>

justfile
@@ -33,17 +33,19 @@ _container_repo := "ghcr.io/githedgehog/dataplane"
 
 # Docker images
 [private]
+_image_profile := if profile == "release" { "release" } else { "debug" }
+[private]
 _dpdk_sys_container_repo := "ghcr.io/githedgehog/dpdk-sys"
 [private]
 _dpdk_sys_container_tag := dpdk_sys_commit
 [private]
-_libc_container := _dpdk_sys_container_repo + "/libc-env:" + _dpdk_sys_container_tag
+_libc_container := _dpdk_sys_container_repo + "/libc-env:" + _dpdk_sys_container_tag + "." + _image_profile
 [private]
 _doc_env_container := _dpdk_sys_container_repo + "/doc-env:" + _dpdk_sys_container_tag
 [private]
 _compile_env_image_name := _dpdk_sys_container_repo + "/compile-env"
 [private]
-_compile_env_container := _compile_env_image_name + ":" + _dpdk_sys_container_tag
+_compile_env_container := _compile_env_image_name + ":" + _dpdk_sys_container_tag + "." + _image_profile
 
 # Warn if the compile-env image is deprecated (or missing)
 
@@ -131,6 +133,7 @@ cargo *args:
           ;;
         --release|--profile=release|--cargo-profile=release)
           declare -rx RUSTFLAGS="${RUSTFLAGS_RELEASE}"
+          declare -rx LIBC_ENV_PROFILE="release"
           extra_args+=("$arg")
           ;;
         --profile=fuzz|--cargo-profile=fuzz)

scripts/test-runner.sh
@@ -195,5 +195,5 @@ fi
   --cap-add SYS_ADMIN \
   --cap-add SYS_RAWIO \
   --read-only \
-  "ghcr.io/githedgehog/dpdk-sys/libc-env:${DPDK_SYS_COMMIT}" \
+  "ghcr.io/githedgehog/dpdk-sys/libc-env:${DPDK_SYS_COMMIT}.${LIBC_ENV_PROFILE:-debug}" \
   "${@}"

.github/workflows/dev.yml
@@ -123,7 +123,10 @@ jobs:
 
       - name: "set up compile-env"
         run: |
-          just --yes debug_justfile="${{matrix.debug_justfile}}" refresh-compile-env
+          just --yes \
+            debug_justfile="${{matrix.debug_justfile}}" \
+            profile=${{matrix.profile.name}} \
+            refresh-compile-env
           just --yes debug_justfile="${{matrix.debug_justfile}}" fake-nix
 
       - name: "cargo deny check"

commit c1c13bae6a77cb9209eca97e395d5e54dc957f23

ci: Use "release" environment for fuzzing
In a recent commit we adjusted the build and CI environments to use
either the release or the debug images for the compile-env and libc-env
containers. We made the "release" profile use the "release" environment,
the "debug" profile use the "debug" environment, as one might expect. As
for the "fuzz" profile? We made it use the "debug" environment, but this
was the wrong pick: it should be "release", because we're using the same
LTO parameters for fuzzing as with the "release" environment. This
commit makes "release" the default environment used with everything
other than "debug", so that the "fuzz" profile gets the correct
environment.

Fixes: fa9137378108 ("fix(ci): Update compile-env container name after dpdk-sys changes")
Signed-off-by: Quentin Monnet <qmo@qmon.net>

justfile
@@ -33,7 +33,7 @@ _container_repo := "ghcr.io/githedgehog/dataplane"
 
 # Docker images
 [private]
-_image_profile := if profile == "release" { "release" } else { "debug" }
+_image_profile := if profile == "debug" { "debug" } else { "release" }
 [private]
 _dpdk_sys_container_repo := "ghcr.io/githedgehog/dpdk-sys"
 [private]
@@ -128,10 +128,10 @@ cargo *args:
       case "$arg" in
         --debug|--profile=debug|--cargo-profile=debug)
           declare -rx RUSTFLAGS="${RUSTFLAGS_DEBUG}"
+          declare -rx LIBC_ENV_PROFILE="debug"
           ;;
         --release|--profile=release|--cargo-profile=release)
           declare -rx RUSTFLAGS="${RUSTFLAGS_RELEASE}"
-          declare -rx LIBC_ENV_PROFILE="release"
           extra_args+=("$arg")
           ;;
         --profile=fuzz|--cargo-profile=fuzz)

scripts/test-runner.sh
@@ -195,5 +195,5 @@ fi
   --cap-add SYS_ADMIN \
   --cap-add SYS_RAWIO \
   --read-only \
-  "ghcr.io/githedgehog/dpdk-sys/libc-env:${DPDK_SYS_COMMIT}.${LIBC_ENV_PROFILE:-debug}" \
+  "ghcr.io/githedgehog/dpdk-sys/libc-env:${DPDK_SYS_COMMIT}.${LIBC_ENV_PROFILE:-release}" \
   "${@}"