8deb0b5db7e52ce42a24704b6f70912f0c69e2f6
Quentin Monnet <qmo@qmon.net>
2025-07-17 14:59:00 +0100

    feat(nat): Rework allocator structure, add allocator to StatefulNat
    Rework our allocator draft to introduce a NatAllocator trait and a
    default NatDefaultAllocator implementation. It should be easy to replace
    this default allocator with alternative NatAllocator implementations for
    future experiments.
    
    The default allocator implementation is currently only a skeleton, and
    will be completed in future commits.
    
    One change worth noting is that we no longer "find" the NAT pool prior
    to calling the allocation function. Instead, the allocator itself will
    be responsible for looking up from the right pool - or whatever internal
    structure it decides to use - and return a valid IP for the Peering in
    use, based on the information it gets from the NatTuple object.
    
    Signed-off-by: Quentin Monnet <qmo@qmon.net>

---
+++ nat/src/stateful/default_allocator/mod.rs
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Open Network Fabric Authors
+
+use super::NatAllocator;
+use super::NatTuple;
+use super::allocator::AllocatorError;
+use crate::stateful::port::NatPort;
+use std::net::{Ipv4Addr, Ipv6Addr};
+
+#[derive(Debug)]
+pub struct NatDefaultAllocator {
+}
+
+impl NatAllocator for NatDefaultAllocator {
+    fn new() -> Self {
+        todo!()
+    }
+
+    fn allocate_v4(
+        &mut self,
+        tuple: &NatTuple<Ipv4Addr>,
+    ) -> Result<(Option<(Ipv4Addr, NatPort)>, Option<(Ipv4Addr, NatPort)>), AllocatorError> {
+        todo!()
+    }
+
+    fn allocate_v6(
+        &mut self,
+        tuple: &NatTuple<Ipv6Addr>,
+    ) -> Result<(Option<(Ipv6Addr, NatPort)>, Option<(Ipv6Addr, NatPort)>), AllocatorError> {
+        todo!()
+    }
+}
