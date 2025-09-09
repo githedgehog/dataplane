githedgehog/dataplane

commit fae5f7c66d9e4f9fdbe9b40385c0d0fd6ac87168

feat(nat): Add code to build the stateful NAT allocator from a VpcTable
Similar to what we have for stateless NAT, add the code to build the
context - in our case, the IP and port allocator - for stateful NAT.

Note: This code is not called yet! We'll need to call it, likely from
the mgmt crate, in the future.

Now that the relevant functions are used for building up the allocator,
we can remove the clippy warning about dead code for the stateful NAT
module - we just need to keep it for the NAT sessions submodule.

Signed-off-by: Quentin Monnet <qmo@qmon.net>

nat/src/stateful/apalloc/setup.rs
@@ -1,0 +1,284 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Open Network Fabric Authors
+
+use super::NatIpWithBitmap;
+use super::alloc::{IpAllocator, NatPool, PoolBitmap};
+use super::{NatDefaultAllocator, PoolTable, PoolTableKey};
+use crate::stateful::allocator::AllocatorError;
+use crate::stateful::{NatAllocator, NatIp, NatVpcId};
+use config::ConfigError;
+use config::external::overlay::vpc::{Peering, VpcTable};
+use config::external::overlay::vpcpeering::VpcExpose;
+use config::utils::collapse_prefixes_peering;
+use lpm::prefix::{IpPrefix, Prefix};
+use net::ip::NextHeader;
+use net::vxlan::Vni;
+use std::collections::{BTreeMap, BTreeSet};
+
+// TODO: stateless NAT uses the same helper, we should likely make it a VpcTable method.
+fn get_remote_vni(peering: &Peering, vpc_table: &VpcTable) -> Vni {
+    vpc_table
+        .get_vpc_by_vpcid(&peering.remote_id)
+        .unwrap_or_else(|| unreachable!())
+        .vni
+}
+
+/// Build a [`NatDefaultAllocator`] from a [`VpcTable`]
+///
+/// # Returns
+///
+/// A [`NatDefaultAllocator`] that can be used to allocate NAT addresses, or a [`ConfigError`]
+/// if building the allocator fails.
+///
+/// # Errors
+///
+/// [`ConfigError::FailureApply`] if adding a peering fails.
+//
+// TODO: Call me maybe
+#[allow(dead_code)]
+pub fn build_nat_allocator(vpc_table: &VpcTable) -> Result<NatDefaultAllocator, ConfigError> {
+    let mut allocator = NatDefaultAllocator::new();
+    for vpc in vpc_table.values() {
+        for peering in &vpc.peerings {
+            let dst_vni = get_remote_vni(peering, vpc_table);
+            allocator
+                .add_peering_addresses(peering, vpc.vni, dst_vni)
+                .map_err(|e| ConfigError::FailureApply(e.to_string()))?;
+        }
+    }
+    Ok(allocator)
+}
+
+impl NatDefaultAllocator {
+    fn add_peering_addresses(
+        &mut self,
+        peering: &Peering,
+        src_vpc_id: NatVpcId,
+        dst_vpc_id: NatVpcId,
+    ) -> Result<(), AllocatorError> {
+        let new_peering = collapse_prefixes_peering(peering)
+            .map_err(|e| AllocatorError::InternalIssue(e.to_string()))?;
+
+        // Update table for source NAT
+        self.update_src_nat_pool_for_expose(&new_peering, src_vpc_id, dst_vpc_id)?;
+
+        // Update table for destination NAT
+        self.update_dst_nat_pool_for_expose(&new_peering, src_vpc_id, dst_vpc_id)?;
+
+        Ok(())
+    }
+
+    fn update_src_nat_pool_for_expose(
+        &mut self,
+        peering: &Peering,
+        src_vpc_id: NatVpcId,
+        dst_vpc_id: NatVpcId,
+    ) -> Result<(), AllocatorError> {
+        filter_v4_exposes(&peering.local.exposes).try_for_each(|expose| {
+            let ip_allocator = ip_allocator_for_prefixes(&expose.as_range)?;
+            update_src_nat_pool_generic(
+                &mut self.pools_src44,
+                expose,
+                src_vpc_id,
+                dst_vpc_id,
+                &ip_allocator,
+            )
+        })?;
+
+        filter_v6_exposes(&peering.local.exposes).try_for_each(|expose| {
+            let ip_allocator = ip_allocator_for_prefixes(&expose.as_range)?;
+            update_src_nat_pool_generic(
+                &mut self.pools_src66,
+                expose,
+                src_vpc_id,
+                dst_vpc_id,
+                &ip_allocator,
+            )
+        })?;
+
+        Ok(())
+    }
+
+    fn update_dst_nat_pool_for_expose(
+        &mut self,
+        peering: &Peering,
+        src_vpc_id: NatVpcId,
+        dst_vpc_id: NatVpcId,
+    ) -> Result<(), AllocatorError> {
+        filter_v4_exposes(&peering.remote.exposes).try_for_each(|expose| {
+            let ip_allocator = ip_allocator_for_prefixes(&expose.ips)?;
+            update_dst_nat_pool_generic(
+                &mut self.pools_dst44,
+                expose,
+                src_vpc_id,
+                dst_vpc_id,
+                &ip_allocator,
+            )
+        })?;
+
+        filter_v6_exposes(&peering.remote.exposes).try_for_each(|expose| {
+            let ip_allocator = ip_allocator_for_prefixes(&expose.ips)?;
+            update_dst_nat_pool_generic(
+                &mut self.pools_dst66,
+                expose,
+                src_vpc_id,
+                dst_vpc_id,
+                &ip_allocator,
+            )
+        })?;
+
+        Ok(())
+    }
+}
+
+fn filter_v4_exposes(exposes: &[VpcExpose]) -> impl Iterator<Item = &VpcExpose> {
+    exposes.iter().filter(|e| {
+        matches!(
+            (e.ips.first(), e.as_range.first()),
+            (Some(Prefix::IPV4(_)), Some(Prefix::IPV4(_)))
+        )
+    })
+}
+
+fn filter_v6_exposes(exposes: &[VpcExpose]) -> impl Iterator<Item = &VpcExpose> {
+    exposes.iter().filter(|e| {
+        matches!(
+            (e.ips.first(), e.as_range.first()),
+            (Some(Prefix::IPV6(_)), Some(Prefix::IPV6(_)))
+        )
+    })
+}
+
+fn update_src_nat_pool_generic<I: NatIpWithBitmap, J: NatIpWithBitmap>(
+    table: &mut PoolTable<I, J>,
+    expose: &VpcExpose,
+    src_vpc_id: NatVpcId,
+    dst_vpc_id: NatVpcId,
+    allocator: &IpAllocator<J>,
+) -> Result<(), AllocatorError> {
+    add_pool_entries(table, &expose.ips, src_vpc_id, dst_vpc_id, allocator)
+}
+
+fn update_dst_nat_pool_generic<I: NatIpWithBitmap, J: NatIpWithBitmap>(
+    table: &mut PoolTable<I, J>,
+    expose: &VpcExpose,
+    src_vpc_id: NatVpcId,
+    dst_vpc_id: NatVpcId,
+    allocator: &IpAllocator<J>,
+) -> Result<(), AllocatorError> {
+    add_pool_entries(table, &expose.as_range, src_vpc_id, dst_vpc_id, allocator)
+}
+
+fn add_pool_entries<I: NatIpWithBitmap, J: NatIpWithBitmap>(
+    table: &mut PoolTable<I, J>,
+    prefixes: &BTreeSet<Prefix>,
+    src_vpc_id: NatVpcId,
+    dst_vpc_id: NatVpcId,
+    allocator: &IpAllocator<J>,
+) -> Result<(), AllocatorError> {
+    for prefix in prefixes {
+        let key = pool_table_key_for_expose(prefix, src_vpc_id, dst_vpc_id)?;
+        insert_per_proto_entries(table, key, allocator);
+    }
+    Ok(())
+}
+
+fn insert_per_proto_entries<I: NatIpWithBitmap, J: NatIpWithBitmap>(
+    table: &mut PoolTable<I, J>,
+    key: PoolTableKey<I>,
+    allocator: &IpAllocator<J>,
+) {
+    // We insert twice the entry, once for TCP and once for UDP. Allocations for TCP do not affect
+    // allocations for UDP, the space defined by the combination of IP addresses and L4 ports is
+    // distinct for each protocol.
+
+    let mut tcp_key = key.clone();
+    tcp_key.protocol = NextHeader::TCP;
+    table.add_entry(tcp_key, allocator.clone());
+
+    let mut udp_key = key;
+    udp_key.protocol = NextHeader::UDP;
+    table.add_entry(udp_key, allocator.clone());
+}
+
+fn ip_allocator_for_prefixes<J: NatIpWithBitmap>(
+    prefixes: &BTreeSet<Prefix>,
+) -> Result<IpAllocator<J>, AllocatorError> {
+    let pool = create_natpool(prefixes)?;
+    let allocator = IpAllocator::new(pool);
+    Ok(allocator)
+}
+
+fn create_natpool<J: NatIpWithBitmap>(
+    prefixes: &BTreeSet<Prefix>,
+) -> Result<NatPool<J>, AllocatorError> {
+    // Build mappings for IPv6 <-> u32 bitmap translation
+    let (bitmap_mapping, reverse_bitmap_mapping) = create_ipv6_bitmap_mappings(prefixes)?;
+
+    // Mark all addresses as available (free) in bitmap
+    let mut bitmap = PoolBitmap::new();
+    prefixes
+        .iter()
+        .try_for_each(|prefix| bitmap.add_prefix(prefix, &reverse_bitmap_mapping))?;
+
+    Ok(NatPool::new(bitmap, bitmap_mapping, reverse_bitmap_mapping))
+}
+
+fn pool_table_key_for_expose<I: NatIp>(
+    prefix: &Prefix,
+    src_vpc_id: NatVpcId,
+    dst_vpc_id: NatVpcId,
+) -> Result<PoolTableKey<I>, AllocatorError> {
+    Ok(PoolTableKey::new(
+        NextHeader::TCP,
+        src_vpc_id,
+        dst_vpc_id,
+        I::try_from_addr(prefix.as_address()).map_err(|()| {
+            AllocatorError::InternalIssue("Failed to build IP address".to_string())
+        })?,
+        match prefix {
+            Prefix::IPV4(p) => I::try_from_ipv4_addr(p.last_address()).map_err(|()| {
+                AllocatorError::InternalIssue(
+                    "Failed to build IPv4 address from prefix".to_string(),
+                )
+            })?,
+            Prefix::IPV6(p) => I::try_from_ipv6_addr(p.last_address()).map_err(|()| {
+                AllocatorError::InternalIssue(
+                    "Failed to build IPv6 address from prefix".to_string(),
+                )
+            })?,
+        },
+    ))
+}
+
+// The allocator's bitmap contains u32 only. For IPv4, it maps well to the address space. For IPv6,
+// we need some mapping to associate IPv6 addresses with u32 indices. This also means that we cannot
+// use more than 2^32 addresses for one expose, for NAT. If the prefixes we get contain more, we'll
+// just ignore the remaining addresses. Hardware limitations are such that working with 4 billion
+// allocated addresses is unreallistic anyway.
+#[allow(clippy::type_complexity)]
+fn create_ipv6_bitmap_mappings(
+    prefixes: &BTreeSet<Prefix>,
+) -> Result<(BTreeMap<u32, u128>, BTreeMap<u128, u32>), AllocatorError> {
+    let mut bitmap_mapping = BTreeMap::new();
+    let mut reverse_bitmap_mapping = BTreeMap::new();
+    let mut index = 0;
+
+    for prefix in prefixes {
+        if let Prefix::IPV6(p) = prefix {
+            let start_address = p.network().to_bits();
+            bitmap_mapping.insert(index, start_address);
+            reverse_bitmap_mapping.insert(start_address, index);
+            if p.size() + u128::from(index) >= 2_u128.pow(32) {
+                break;
+            }
+            index += u32::try_from(u128::try_from(p.size()).map_err(|_| {
+                AllocatorError::InternalIssue("Failed to convert prefix size to u128".to_string())
+            })?)
+            .map_err(|_| {
+                AllocatorError::InternalIssue("Failed to convert prefix size to u32".to_string())
+            })?;
+        }
+    }
+    Ok((bitmap_mapping, reverse_bitmap_mapping))
+}

nat/src/stateful/apalloc/mod.rs
@@ -74,6 +74,7 @@ use std::net::{Ipv4Addr, Ipv6Addr};
 mod alloc;
 mod natip_with_bitmap;
 mod port_alloc;
+mod setup;
 
 ///////////////////////////////////////////////////////////////////////////////
 // PoolTableKey

nat/src/stateful/mod.rs
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Open Network Fabric Authors
 
-#![allow(dead_code)]
-
 mod allocator;
 mod apalloc;
 mod natip;

nat/src/stateful/sessions.rs
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Open Network Fabric Authors
 
+#![allow(dead_code)]
+
 use super::NatTuple;
 use super::port::NatPort;
 use crate::stateful::NatIp;

commit 1c482e0db110e3ab10aec9d4e59e6482e4995c3a

fix(nat): Use separate allocators for TCP and UDP
We use sepearate entries in the NatPool table for TCP and UDP
allocators, however, these separate entries have been using Arc
references pointing to the same objects (with the same bitmaps). As a
consequence, we've been using a single allocator for both TCP and UDP
allocations for any given VpcExpose. This is not what we want: TCP and
UDP should be decorrelated when it comes to allocating IP addresses and
ports.

Fix this by using distinct allocators for TCP and UDP. We will likely
have to use yet another one for ICMP in the future.

Fixes: fae5f7c66d9e ("feat(nat): Add code to build the stateful NAT allocator from a VpcTable")
Signed-off-by: Quentin Monnet <qmo@qmon.net>

nat/src/stateful/apalloc/alloc.rs
@@ -39,6 +39,14 @@ impl<I: NatIpWithBitmap> IpAllocator<I> {
         }
     }
 
+    pub(crate) fn deep_clone(&self) -> Result<IpAllocator<I>, AllocatorError> {
+        let nat_pool = self
+            .pool
+            .read()
+            .map_err(|_| AllocatorError::InternalIssue("Failed to read pool".to_string()))?;
+        Ok(IpAllocator::new((*nat_pool).clone()))
+    }
+
     fn deallocate_ip(&self, ip: I) {
         self.pool.write().unwrap().deallocate_from_pool(ip);
     }
@@ -178,7 +186,7 @@ impl<I: NatIpWithBitmap> Drop for AllocatedIp<I> {
 /// A [`NatPool`] is a pool of IP addresses that can be allocated from. It contains a bitmap of
 /// available IP addresses, and a list of weak references to [`AllocatedIp`] objects representing
 /// the allocated IPs potentially available for use (if they still have free ports)
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub(crate) struct NatPool<I: NatIpWithBitmap> {
     bitmap: PoolBitmap,
     bitmap_mapping: BTreeMap<u32, u128>,
@@ -264,7 +272,7 @@ impl<I: NatIpWithBitmap> NatPool<I> {
 
 /// A [`PoolBitmap`] is a bitmap of available IP addresses in a [`NatPool`]. It wraps around a
 /// [`RoaringBitmap`], and provides a few methods to manage the bitmap.
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub(crate) struct PoolBitmap(RoaringBitmap);
 
 impl PoolBitmap {

nat/src/stateful/apalloc/setup.rs
@@ -66,24 +66,28 @@ impl NatDefaultAllocator {
         dst_vpc_id: NatVpcId,
     ) -> Result<(), AllocatorError> {
         filter_v4_exposes(&peering.local.exposes).try_for_each(|expose| {
-            let ip_allocator = ip_allocator_for_prefixes(&expose.as_range)?;
+            let tcp_ip_allocator = ip_allocator_for_prefixes(&expose.as_range)?;
+            let udp_ip_allocator = tcp_ip_allocator.deep_clone()?;
             build_src_nat_pool_generic(
                 &mut self.pools_src44,
                 expose,
                 src_vpc_id,
                 dst_vpc_id,
-                &ip_allocator,
+                &tcp_ip_allocator,
+                &udp_ip_allocator,
             )
         })?;
 
         filter_v6_exposes(&peering.local.exposes).try_for_each(|expose| {
-            let ip_allocator = ip_allocator_for_prefixes(&expose.as_range)?;
+            let tcp_ip_allocator = ip_allocator_for_prefixes(&expose.as_range)?;
+            let udp_ip_allocator = tcp_ip_allocator.deep_clone()?;
             build_src_nat_pool_generic(
                 &mut self.pools_src66,
                 expose,
                 src_vpc_id,
                 dst_vpc_id,
-                &ip_allocator,
+                &tcp_ip_allocator,
+                &udp_ip_allocator,
             )
         })?;
 
@@ -97,24 +101,28 @@ impl NatDefaultAllocator {
         dst_vpc_id: NatVpcId,
     ) -> Result<(), AllocatorError> {
         filter_v4_exposes(&peering.remote.exposes).try_for_each(|expose| {
-            let ip_allocator = ip_allocator_for_prefixes(&expose.ips)?;
+            let tcp_ip_allocator = ip_allocator_for_prefixes(&expose.ips)?;
+            let udp_ip_allocator = tcp_ip_allocator.deep_clone()?;
             build_dst_nat_pool_generic(
                 &mut self.pools_dst44,
                 expose,
                 src_vpc_id,
                 dst_vpc_id,
-                &ip_allocator,
+                &tcp_ip_allocator,
+                &udp_ip_allocator,
             )
         })?;
 
         filter_v6_exposes(&peering.remote.exposes).try_for_each(|expose| {
-            let ip_allocator = ip_allocator_for_prefixes(&expose.ips)?;
+            let tcp_ip_allocator = ip_allocator_for_prefixes(&expose.ips)?;
+            let udp_ip_allocator = tcp_ip_allocator.deep_clone()?;
             build_dst_nat_pool_generic(
                 &mut self.pools_dst66,
                 expose,
                 src_vpc_id,
                 dst_vpc_id,
-                &ip_allocator,
+                &tcp_ip_allocator,
+                &udp_ip_allocator,
             )
         })?;
 
@@ -145,9 +153,17 @@ fn build_src_nat_pool_generic<I: NatIpWithBitmap, J: NatIpWithBitmap>(
     expose: &VpcExpose,
     src_vpc_id: NatVpcId,
     dst_vpc_id: NatVpcId,
-    allocator: &IpAllocator<J>,
+    tcp_allocator: &IpAllocator<J>,
+    udp_allocator: &IpAllocator<J>,
 ) -> Result<(), AllocatorError> {
-    add_pool_entries(table, &expose.ips, src_vpc_id, dst_vpc_id, allocator)
+    add_pool_entries(
+        table,
+        &expose.ips,
+        src_vpc_id,
+        dst_vpc_id,
+        tcp_allocator,
+        udp_allocator,
+    )
 }
 
 fn build_dst_nat_pool_generic<I: NatIpWithBitmap, J: NatIpWithBitmap>(
@@ -155,9 +171,17 @@ fn build_dst_nat_pool_generic<I: NatIpWithBitmap, J: NatIpWithBitmap>(
     expose: &VpcExpose,
     src_vpc_id: NatVpcId,
     dst_vpc_id: NatVpcId,
-    allocator: &IpAllocator<J>,
+    tcp_allocator: &IpAllocator<J>,
+    udp_allocator: &IpAllocator<J>,
 ) -> Result<(), AllocatorError> {
-    add_pool_entries(table, &expose.as_range, src_vpc_id, dst_vpc_id, allocator)
+    add_pool_entries(
+        table,
+        &expose.as_range,
+        src_vpc_id,
+        dst_vpc_id,
+        tcp_allocator,
+        udp_allocator,
+    )
 }
 
 fn add_pool_entries<I: NatIpWithBitmap, J: NatIpWithBitmap>(
@@ -165,11 +189,12 @@ fn add_pool_entries<I: NatIpWithBitmap, J: NatIpWithBitmap>(
     prefixes: &BTreeSet<Prefix>,
     src_vpc_id: NatVpcId,
     dst_vpc_id: NatVpcId,
-    allocator: &IpAllocator<J>,
+    tcp_allocator: &IpAllocator<J>,
+    udp_allocator: &IpAllocator<J>,
 ) -> Result<(), AllocatorError> {
     for prefix in prefixes {
         let key = pool_table_key_for_expose(prefix, src_vpc_id, dst_vpc_id)?;
-        insert_per_proto_entries(table, key, allocator);
+        insert_per_proto_entries(table, key, tcp_allocator, udp_allocator);
     }
     Ok(())
 }
@@ -177,7 +202,8 @@ fn add_pool_entries<I: NatIpWithBitmap, J: NatIpWithBitmap>(
 fn insert_per_proto_entries<I: NatIpWithBitmap, J: NatIpWithBitmap>(
     table: &mut PoolTable<I, J>,
     key: PoolTableKey<I>,
-    allocator: &IpAllocator<J>,
+    tcp_allocator: &IpAllocator<J>,
+    udp_allocator: &IpAllocator<J>,
 ) {
     // We insert twice the entry, once for TCP and once for UDP. Allocations for TCP do not affect
     // allocations for UDP, the space defined by the combination of IP addresses and L4 ports is
@@ -185,11 +211,11 @@ fn insert_per_proto_entries<I: NatIpWithBitmap, J: NatIpWithBitmap>(
 
     let mut tcp_key = key.clone();
     tcp_key.protocol = NextHeader::TCP;
-    table.add_entry(tcp_key, allocator.clone());
+    table.add_entry(tcp_key, tcp_allocator.clone());
 
     let mut udp_key = key;
     udp_key.protocol = NextHeader::UDP;
-    table.add_entry(udp_key, allocator.clone());
+    table.add_entry(udp_key, udp_allocator.clone());
 }
 
 fn ip_allocator_for_prefixes<J: NatIpWithBitmap>(

nat/src/stateful/apalloc/test_alloc.rs
@@ -336,6 +336,108 @@ mod std_tests {
         assert!(in_use.front().unwrap().upgrade().is_none()); // But it no longer resolves
     }
 
+    #[test]
+    // Allocate an IP for a TCP packet, then for a UDP packet.
+    fn test_tcp_udp() {
+        let tcp_tuple = NatTuple::new(
+            addr_v4("1.1.0.0"),
+            addr_v4("10.3.0.2"),
+            Some(1234),
+            Some(5678),
+            NextHeader::TCP,
+            vni1(),
+            vni2(),
+        );
+        let udp_tuple = NatTuple::new(
+            addr_v4("1.1.0.0"),
+            addr_v4("10.3.0.2"),
+            Some(1234),
+            Some(5678),
+            NextHeader::UDP,
+            vni1(),
+            vni2(),
+        );
+
+        let mut allocator = build_allocator().unwrap();
+        let (bitmap, in_use) = get_ip_allocator_v4(
+            &mut allocator.pools_src44,
+            vni1(),
+            vni2(),
+            NextHeader::TCP,
+            addr_v4("1.1.0.0"),
+        )
+        .get_pool_clone_for_tests();
+        assert_eq!(bitmap.len(), 3); // 3 IP addresses available to NAT 1.1.0.0 (TCP)
+        assert_eq!(in_use.len(), 0); // None allocated yet
+
+        let (bitmap, in_use) = get_ip_allocator_v4(
+            &mut allocator.pools_src44,
+            vni1(),
+            vni2(),
+            NextHeader::UDP,
+            addr_v4("1.1.0.0"),
+        )
+        .get_pool_clone_for_tests();
+        assert_eq!(bitmap.len(), 3); // 3 free IP addresses left to NAT 1.1.0.0 (UDP)
+        assert_eq!(in_use.len(), 0); // None allocated yet
+
+        // Allocate for TCP
+        let tcp_allocation = allocator.allocate_v4(&tcp_tuple).unwrap();
+        print_allocation(&tcp_allocation);
+
+        // Check number of allocated IPs for TCP after we have allocated for TCP
+        let (bitmap, in_use) = get_ip_allocator_v4(
+            &mut allocator.pools_src44,
+            vni1(),
+            vni2(),
+            NextHeader::TCP,
+            addr_v4("1.1.0.0"),
+        )
+        .get_pool_clone_for_tests();
+        assert_eq!(bitmap.len(), 2); // 2 free IP addresses left to NAT 1.1.0.0 (TCP)
+        assert_eq!(in_use.len(), 1); // 1 allocated, in use
+
+        // Check number of allocated IPs for UDP after we have allocated for TCP
+        let (bitmap, in_use) = get_ip_allocator_v4(
+            &mut allocator.pools_src44,
+            vni1(),
+            vni2(),
+            NextHeader::UDP,
+            addr_v4("1.1.0.0"),
+        )
+        .get_pool_clone_for_tests();
+        assert_eq!(bitmap.len(), 3); // 3 free IP addresses left to NAT 1.1.0.0 (UDP)
+        assert_eq!(in_use.len(), 0); // None allocated yet
+
+        // Allocate for UDP
+        let udp_allocation = allocator.allocate_v4(&udp_tuple).unwrap();
+        print_allocation(&udp_allocation);
+
+        // Check number of allocated IPs for TCP after we have allocated for UDP
+        let (bitmap, in_use) = get_ip_allocator_v4(
+            &mut allocator.pools_src44,
+            vni1(),
+            vni2(),
+            NextHeader::TCP,
+            addr_v4("1.1.0.0"),
+        )
+        .get_pool_clone_for_tests();
+        assert_eq!(bitmap.len(), 2); // 2 free IP addresses left to NAT 1.1.0.0 (TCP)
+        assert_eq!(in_use.len(), 1); // 1 allocated, in use
+
+        // Check number of allocated IPs for UDP after we have allocated for UDP
+        let (bitmap, in_use) = get_ip_allocator_v4(
+            &mut allocator.pools_src44,
+            vni1(),
+            vni2(),
+            NextHeader::UDP,
+            addr_v4("1.1.0.0"),
+        )
+        .get_pool_clone_for_tests();
+        assert_eq!(bitmap.len(), 2); // 2 free IP addresses left to NAT 1.1.0.0 (UDP)
+        assert_eq!(in_use.len(), 1); // 1 allocated, in use
+    }
+
     // This test is NOT a shuttle test. It validates that a basic example with threads works
     // with or without shuttle components (depending on how we compile), as a control test in
     // case shuttle tests do not work. For example, it helped understand that memory usage for