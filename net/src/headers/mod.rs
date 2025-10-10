githedgehog/dataplane

commit 684c65ca41f6fc8f359f3b5ed1dcda98764cab6c

feat(net): Introduce EmbeddedHeaders to parse ICMP's inner packets
Introduce new enum EmbeddedHeader and struct EmbeddedHeaders,
counterparts to enum Header and struct Headers, but for parsing IP
packets embedded inside of ICMP Error messages (such as "Destination
Unreachable").

It is not possible to reuse the existing enum and structs for IP packets
embedded in ICMP messages, because the packets may not be complete, but
we still need to access the transport-layer ports if available, for
example, even if the transport header is truncated; and the
implementation of the Parse trait for the existing structs does not
allow that (nor should it).

Make an (optional) EmbeddedHeaders object part of struct Headers, so we
can hold the parsed data and reuse it when "deparsing" the packet,
although the DeParse trait implementation for EmbeddedHeaders will be
completed in a follow-up commit.

Signed-off-by: Quentin Monnet <qmo@qmon.net>

net/src/headers/mod.rs
@@ -32,6 +32,9 @@ use tracing::{debug, error, trace};
 #[cfg(any(test, feature = "bolero"))]
 pub use contract::*;
 
+mod embedded;
+pub use embedded::*;
+
 const MAX_VLANS: usize = 4;
 const MAX_NET_EXTENSIONS: usize = 2;
 
@@ -45,6 +48,7 @@ pub struct Headers {
     pub net_ext: ArrayVec<NetExt, MAX_NET_EXTENSIONS>,
     pub transport: Option<Transport>,
     pub udp_encap: Option<UdpEncap>,
+    pub embedded_ip: Option<EmbeddedHeaders>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, thiserror::Error)]
@@ -271,13 +275,16 @@ pub enum Header {
     IpAuth(IpAuth),
     IpV6Ext(Ipv6Ext), // TODO: break out nested enum.  Nesting is counter productive here
     Encap(UdpEncap),
+    EmbeddedIp(EmbeddedHeaders),
 }
 
 impl ParsePayload for Header {
     type Next = Header;
 
     fn parse_payload(&self, cursor: &mut Reader) -> Option<Header> {
-        use Header::{Encap, Eth, Icmp4, Icmp6, IpAuth, IpV6Ext, Ipv4, Ipv6, Tcp, Udp, Vlan};
+        use Header::{
+            EmbeddedIp, Encap, Eth, Icmp4, Icmp6, IpAuth, IpV6Ext, Ipv4, Ipv6, Tcp, Udp, Vlan,
+        };
         match self {
             Eth(eth) => eth.parse_payload(cursor).map(Header::from),
             Vlan(vlan) => vlan.parse_payload(cursor).map(Header::from),
@@ -294,7 +301,7 @@ impl ParsePayload for Header {
                 }
             }
             Udp(udp) => udp.parse_payload(cursor).map(Header::from),
-            Encap(_) | Tcp(_) | Icmp4(_) | Icmp6(_) => None,
+            Encap(_) | Tcp(_) | Icmp4(_) | Icmp6(_) | EmbeddedIp(_) => None,
         }
     }
 }
@@ -313,6 +320,7 @@ impl Parse for Headers {
             vlan: ArrayVec::default(),
             net_ext: ArrayVec::default(),
             udp_encap: None,
+            embedded_ip: None,
         };
         let mut prior = Header::Eth(eth);
         loop {
@@ -347,6 +355,7 @@ impl Parse for Headers {
                         break;
                     }
                 }
+                Header::EmbeddedIp(embedded) => this.embedded_ip = Some(embedded),
             }
             match header {
                 None => {
@@ -449,6 +458,20 @@ impl DeParse for Headers {
                 cursor.write(vxlan)?;
             }
         }
+
+        match self.embedded_ip {
+            None => {
+                #[allow(clippy::cast_possible_truncation)] // length bounded on cursor creation
+                return Ok(
+                    NonZero::new((cursor.inner.len() - cursor.remaining as usize) as u16)
+                        .unwrap_or_else(|| unreachable!()),
+                );
+            }
+            Some(ref embedded_ip) => {
+                cursor.write(embedded_ip)?;
+            }
+        }
+
         #[allow(clippy::cast_possible_truncation)] // length bounded on cursor creation
         Ok(
             NonZero::new((cursor.inner.len() - cursor.remaining as usize) as u16)
@@ -930,6 +953,7 @@ impl_from_for_enum![
     IpAuth(IpAuth),
     IpV6Ext(Ipv6Ext),
     Encap(UdpEncap),
+    EmbeddedIp(EmbeddedHeaders),
 ];
 
 impl From<Net> for Header {
@@ -1293,6 +1317,7 @@ mod contract {
                                 net_ext: Default::default(),
                                 transport: Some(Transport::Tcp(tcp)),
                                 udp_encap: None,
+                                embedded_ip: None,
                             };
                             Some(headers)
                         }
@@ -1311,6 +1336,7 @@ mod contract {
                                 net_ext: Default::default(),
                                 transport: Some(Transport::Udp(udp)),
                                 udp_encap,
+                                embedded_ip: None,
                             };
                             Some(headers)
                         }
@@ -1323,6 +1349,7 @@ mod contract {
                                 net_ext: Default::default(),
                                 transport: Some(Transport::Icmp4(icmp)),
                                 udp_encap: None,
+                                embedded_ip: None,
                             };
                             Some(headers)
                         }
@@ -1342,6 +1369,7 @@ mod contract {
                                 net_ext: Default::default(),
                                 transport: Some(Transport::Tcp(tcp)),
                                 udp_encap: None,
+                                embedded_ip: None,
                             };
                             Some(headers)
                         }
@@ -1360,6 +1388,7 @@ mod contract {
                                 net_ext: Default::default(),
                                 transport: Some(Transport::Udp(udp)),
                                 udp_encap,
+                                embedded_ip: None,
                             };
                             Some(headers)
                         }
@@ -1372,6 +1401,7 @@ mod contract {
                                 net_ext: Default::default(),
                                 transport: Some(Transport::Icmp6(icmp6)),
                                 udp_encap: None,
+                                embedded_ip: None,
                             };
                             Some(headers)
                         }

dataplane/src/packet_processor/ipforward.rs
@@ -214,6 +214,7 @@ impl IpForwarder {
             net_ext: ArrayVec::default(),
             transport: None, /* should be UDP, but it is automatically done */
             udp_encap: Some(udp_encap),
+            embedded_ip: None,
         };
         VxlanEncap::new(headers).map_err(|e| format!("{e}"))
     }

net/src/headers/embedded.rs
@@ -1,0 +1,152 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Open Network Fabric Authors
+
+use crate::eth::EthError;
+use crate::headers::{MAX_NET_EXTENSIONS, Net, NetExt};
+use crate::impl_from_for_enum;
+use crate::ip_auth::IpAuth;
+use crate::ipv4::Ipv4;
+use crate::ipv6::{Ipv6, Ipv6Ext};
+use crate::parse::{
+    DeParse, DeParseError, IllegalBufferLength, LengthError, ParseError, ParseHeader, ParsePayload,
+    ParseWith, Reader,
+};
+use crate::tcp::TruncatedTcp;
+use crate::udp::TruncatedUdp;
+use arrayvec::ArrayVec;
+use core::fmt::Debug;
+use std::num::NonZero;
+
+pub enum EmbeddedIpVersion {
+    Ipv4,
+    Ipv6,
+}
+
+// Structure representing the set of headers for an IP packet embedded as the payload for an ICMP
+// Error message. We need a dedicated struct and processing, because this packet may be truncated.
+// RFC 792 stipulates that an ICMP Error message should embed an IP header and only a minimum of 64
+// bits of the IP payload. Section 4.3.2.3 of RFC 1812 recommends an ICMP Error originator include
+// as much of the original packet as possible in the payload, as long as the length of the resulting
+// ICMP datagram does not exceed 576 bytes.
+#[derive(Debug, PartialEq, Eq, Clone, Default)]
+pub struct EmbeddedHeaders {
+    net: Option<Net>,
+    net_ext: ArrayVec<NetExt, MAX_NET_EXTENSIONS>,
+    transport: Option<EmbeddedTransport>,
+}
+
+impl ParseWith for EmbeddedHeaders {
+    type Error = EthError;
+    type Param = EmbeddedIpVersion;
+
+    fn parse_with(
+        param: Self::Param,
+        buf: &[u8],
+    ) -> Result<(Self, NonZero<u16>), ParseError<Self::Error>> {
+        let mut cursor =
+            Reader::new(buf).map_err(|IllegalBufferLength(len)| ParseError::BufferTooLong(len))?;
+        let mut this = EmbeddedHeaders::default();
+        let mut prior = match param {
+            EmbeddedIpVersion::Ipv4 => {
+                cursor
+                    .parse_header::<Ipv4, EmbeddedHeader>()
+                    .ok_or(ParseError::Length(LengthError {
+                        expected: NonZero::new(1).unwrap_or_else(|| unreachable!()),
+                        actual: 0,
+                    }))?
+            }
+            EmbeddedIpVersion::Ipv6 => {
+                cursor
+                    .parse_header::<Ipv6, EmbeddedHeader>()
+                    .ok_or(ParseError::Length(LengthError {
+                        expected: NonZero::new(1).unwrap_or_else(|| unreachable!()),
+                        actual: 0,
+                    }))?
+            }
+        };
+        loop {
+            let header = prior.parse_payload(&mut cursor);
+            match prior {
+                EmbeddedHeader::Ipv4(ipv4) => {
+                    this.net = Some(Net::Ipv4(ipv4));
+                }
+                EmbeddedHeader::Ipv6(ipv6) => {
+                    this.net = Some(Net::Ipv6(ipv6));
+                }
+                EmbeddedHeader::IpAuth(auth) => {
+                    this.net_ext.push(NetExt::IpAuth(auth));
+                }
+                EmbeddedHeader::IpV6Ext(ext) => {
+                    this.net_ext.push(NetExt::Ipv6Ext(ext));
+                }
+                EmbeddedHeader::Tcp(tcp) => {
+                    this.transport = Some(EmbeddedTransport::Tcp(tcp));
+                }
+                EmbeddedHeader::Udp(udp) => {
+                    this.transport = Some(EmbeddedTransport::Udp(udp));
+                }
+            }
+            match header {
+                None => {
+                    break;
+                }
+                Some(next) => {
+                    prior = next;
+                }
+            }
+        }
+        #[allow(unsafe_code, clippy::cast_possible_truncation)] // Non zero checked by parse impl
+        let consumed = unsafe {
+            NonZero::new_unchecked((cursor.inner.len() - cursor.remaining as usize) as u16)
+        };
+        Ok((this, consumed))
+    }
+}
+
+impl DeParse for EmbeddedHeaders {
+    type Error = ();
+
+    fn size(&self) -> NonZero<u16> {
+        todo!()
+    }
+
+    fn deparse(&self, _buf: &mut [u8]) -> Result<NonZero<u16>, DeParseError<Self::Error>> {
+        todo!()
+    }
+}
+
+// Header variants used for the potentially-truncated IP packet fragment embedded in an ICMP Error
+// message
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub(crate) enum EmbeddedHeader {
+    Ipv4(Ipv4),
+    Ipv6(Ipv6),
+    Tcp(TruncatedTcp),
+    Udp(TruncatedUdp),
+    IpAuth(IpAuth),
+    IpV6Ext(Ipv6Ext), // TODO: break out nested enum.  Nesting is counter productive here
+}
+
+impl ParsePayload for EmbeddedHeader {
+    type Next = EmbeddedHeader;
+
+    fn parse_payload(&self, _cursor: &mut Reader) -> Option<EmbeddedHeader> {
+        todo!()
+    }
+}
+
+impl_from_for_enum![
+    EmbeddedHeader,
+    Ipv4(Ipv4),
+    Ipv6(Ipv6),
+    Udp(TruncatedUdp),
+    Tcp(TruncatedTcp),
+    IpAuth(IpAuth),
+    IpV6Ext(Ipv6Ext)
+];
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum EmbeddedTransport {
+    Tcp(TruncatedTcp),
+    Udp(TruncatedUdp),
+}

net/src/tcp/truncated.rs
@@ -129,7 +129,6 @@ impl DeParse for TruncatedTcpHeader {
     }
 }
 
-#[allow(unused)]
 /// A TCP header, possibly truncated.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub(crate) enum TruncatedTcp {

net/src/udp/truncated.rs
@@ -129,7 +129,6 @@ impl DeParse for TruncatedUdpHeader {
     }
 }
 
-#[allow(unused)]
 /// A UDP header, possibly truncated.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub(crate) enum TruncatedUdp {

commit f276516313a4c8953d083df33635ef65b4cea1a6

fix(net): Fix length computation for Headers
Now that we have introduced field embedded_ip for struct Headers, we
need to account for its size when computing the size of the whole set of
headers as part of the implementation of the DeParse trait.

Fixes: 684c65ca41f6 ("feat(net): Introduce EmbeddedHeaders to parse ICMP's inner packets")
Signed-off-by: Quentin Monnet <qmo@qmon.net>

net/src/headers/mod.rs
@@ -402,7 +402,12 @@ impl DeParse for Headers {
             None => 0,
             Some(UdpEncap::Vxlan(vxlan)) => vxlan.size().get(),
         };
-        NonZero::new(eth + vlan + net + transport + encap).unwrap_or_else(|| unreachable!())
+        let embedded_ip = match self.embedded_ip {
+            None => 0,
+            Some(ref e) => e.size().get(),
+        };
+        NonZero::new(eth + vlan + net + transport + encap + embedded_ip)
+            .unwrap_or_else(|| unreachable!())
     }
 
     fn deparse(&self, buf: &mut [u8]) -> Result<NonZero<u16>, DeParseError<Self::Error>> {