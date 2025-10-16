githedgehog/dataplane

commit 85f935e9027b20b6c32327879580c3388e8349e2

feat(net): Update embedded IP's checksum in Headers' update_checksum()
When updating checksums for a packet, we update the nework-layer and
transport-layer headers' checksums for the packet. If this packet is an
ICMP Error message and embeds a (possibly truncated) IP packet, we want
to update the checksum for that inner IP header, too. Let's add it.

As mentioned in the comment, it would be desirable to update the inner
transport layer as well. But we refrain from doing that, because it's
possible that the inner packet is truncated, in which case we cannot
compute the new checksum correctly. We can check whether the payload is
full and update accordingly, or we can perform an unconditional,
incremental update of the checksum, based on the old and new values
changed in the header.

Given that the unconditional, incremental update looks cheaper (and more
robust) than evaluating whether the inner packet is full, we choose this
option. This means we cannot do the incremental checksum update at this
location, because we do not know what fields have changed in the header
and what their old values were. Instead, leave it to the processing
code, such as the NAT logic, to run the transport checksum update as
they see fit, and skip the layer here in update_checksum().

Signed-off-by: Quentin Monnet <qmo@qmon.net>

net/src/headers/mod.rs
@@ -604,6 +604,19 @@ impl Headers {
             return;
         };
         transport.update_checksum(net, payload.as_ref());
+
+        if let Some(embedded_ip) = self.embedded_ip.as_mut() {
+            let Some(inner_net) = embedded_ip.try_inner_ip_mut() else {
+                trace!("no inner network header in ICMP: can't update checksum");
+                return;
+            };
+            inner_net.update_checksum();
+
+            // WARNING: We do NOT update ICMP Error message inner transport checksum here!
+            // This is because we're not sure the transport header and payload are full, so we want
+            // incremental checksum updates that require knowledge of the previous values, if we've
+            // changed them (for example: NAT). Leave this to (for example) the NAT code.
+        }
     }
 }
 

commit f7378add77717539f423841665cb338de5c827e0

fix(net): Fix checksums update order for ICMP Error msg's inner headers
When we added support for ICMP Error messages' inner headers checksum
update, we added the udpate at the end of the function to update
headers' checksums, below the update of the transport-layer header's
checksum. This sounds logical: we process each layer in order. But it is
wrong: if we update the checksums of the inner layers after the one for
the outer transport layer, we have an issue. This is because the inner
headers are part of the ICMP payload, and therefore, they need to be
accounted for when computing the checksum for the ICMP header. If we
update these headers _after_ updating the ICMP checksum, we invalidate
this checksum.

Reverse the order to make sure that we update the inner headers before
we update the ICMP checksum.

Fixes: 85f935e9027b ("feat(net): Update embedded IP's checksum in Headers' update_checksum()")
Signed-off-by: Quentin Monnet <qmo@qmon.net>

net/src/headers/mod.rs
@@ -617,24 +617,27 @@ impl Headers {
             return;
         }
 
-        let Some(transport) = self.transport.as_mut() else {
-            trace!("no transport header: can't update checksum");
-            return;
-        };
-        transport.update_checksum(net, payload.as_ref());
-
-        if let Some(embedded_ip) = self.embedded_ip.as_mut() {
-            let Some(inner_net) = embedded_ip.try_inner_ip_mut() else {
-                trace!("no inner network header in ICMP: can't update checksum");
-                return;
-            };
-            inner_net.update_checksum();
+        // Update inner IP header checksum, if any, before updating transport checksum.
+        // This is because the inner headers are part of transport header's payload, so updating
+        // them later would invalidate transport's payload.
+        if let Some(inner_ip) = self
+            .embedded_ip
+            .as_mut()
+            .and_then(|ip| ip.try_inner_ip_mut())
+        {
+            inner_ip.update_checksum();
 
             // WARNING: We do NOT update ICMP Error message inner transport checksum here!
             // This is because we're not sure the transport header and payload are full, so we want
             // incremental checksum updates that require knowledge of the previous values, if we've
             // changed them (for example: NAT). Leave this to (for example) the NAT code.
         }
+
+        let Some(transport) = self.transport.as_mut() else {
+            trace!("no transport header: can't update checksum");
+            return;
+        };
+        transport.update_checksum(net, self.embedded_ip.as_ref(), payload.as_ref());
     }
 }
 