githedgehog/dataplane

commit 803338d3667d7bad5698e377ccf2da49994cad5d

feat(net): Parse payload for ICMP
ICMP Error messages (such as "Destination Unreachable") may contain a
payload, made of a full or truncated version of the original IP packet
that triggered the error. Parse this packet using the existing
EmbeddedHeaders structure added in previous commits.

Signed-off-by: Quentin Monnet <qmo@qmon.net>

net/src/icmp4/mod.rs
@@ -3,13 +3,15 @@
 
 //! `ICMPv4` header type and logic.
 
-use crate::parse::{DeParse, DeParseError, IntoNonZeroUSize, LengthError, Parse, ParseError};
-use etherparse::{Icmpv4Header, Icmpv4Type};
-
 mod checksum;
 
 pub use checksum::*;
 
+use crate::headers::{EmbeddedHeaders, EmbeddedIpVersion};
+use crate::parse::{
+    DeParse, DeParseError, IntoNonZeroUSize, LengthError, Parse, ParseError, ParseWith, Reader,
+};
+use etherparse::{Icmpv4Header, Icmpv4Type};
 use std::{net::IpAddr, num::NonZero};
 
 #[allow(unused_imports)] // re-export
@@ -126,6 +128,16 @@ impl Icmp4 {
             icmp_type,
             checksum: 0,
         })
+    }
+
+    pub(crate) fn parse_payload(&self, cursor: &mut Reader) -> Option<EmbeddedHeaders> {
+        if !self.is_error_message() {
+            return None;
+        }
+        let (headers, consumed) =
+            EmbeddedHeaders::parse_with(EmbeddedIpVersion::Ipv4, cursor.inner).ok()?;
+        cursor.consume(consumed).ok()?;
+        Some(headers)
     }
 }
 

net/src/icmp6/mod.rs
@@ -7,7 +7,10 @@ mod checksum;
 
 pub use checksum::*;
 
-use crate::parse::{DeParse, DeParseError, IntoNonZeroUSize, LengthError, Parse, ParseError};
+use crate::headers::{EmbeddedHeaders, EmbeddedIpVersion};
+use crate::parse::{
+    DeParse, DeParseError, IntoNonZeroUSize, LengthError, Parse, ParseError, ParseWith, Reader,
+};
 use etherparse::{Icmpv6Header, Icmpv6Type};
 use std::num::NonZero;
 
@@ -31,6 +34,26 @@ impl Icmp6 {
         &mut self.0.icmp_type
     }
 
+    /// Returns true if the ICMP type is an error message
+    #[must_use]
+    pub fn is_error_message(&self) -> bool {
+        // List all types to make it sure we catch any new addition to the enum
+        match self.icmp_type() {
+            Icmpv6Type::DestinationUnreachable(_)
+            | Icmpv6Type::PacketTooBig { .. }
+            | Icmpv6Type::TimeExceeded(_)
+            | Icmpv6Type::ParameterProblem(_) => true,
+            Icmpv6Type::Unknown { .. }
+            | Icmpv6Type::EchoRequest(_)
+            | Icmpv6Type::EchoReply(_)
+            | Icmpv6Type::RouterSolicitation
+            | Icmpv6Type::RouterAdvertisement(_)
+            | Icmpv6Type::NeighborSolicitation
+            | Icmpv6Type::NeighborAdvertisement(_)
+            | Icmpv6Type::Redirect => false,
+        }
+    }
+
     /// Creates a new `Icmp6` with the given type.
     ///
     /// The checksum will be set to zero.
@@ -40,6 +63,16 @@ impl Icmp6 {
             icmp_type,
             checksum: 0,
         })
+    }
+
+    pub(crate) fn parse_payload(&self, cursor: &mut Reader) -> Option<EmbeddedHeaders> {
+        if !self.is_error_message() {
+            return None;
+        }
+        let (headers, consumed) =
+            EmbeddedHeaders::parse_with(EmbeddedIpVersion::Ipv6, cursor.inner).ok()?;
+        cursor.consume(consumed).ok()?;
+        Some(headers)
     }
 }
 

net/src/headers/mod.rs
@@ -300,8 +300,10 @@ impl ParsePayload for Header {
                     None
                 }
             }
+            Icmp4(icmp4) => icmp4.parse_payload(cursor).map(Header::from),
+            Icmp6(icmp6) => icmp6.parse_payload(cursor).map(Header::from),
             Udp(udp) => udp.parse_payload(cursor).map(Header::from),
-            Encap(_) | Tcp(_) | Icmp4(_) | Icmp6(_) | EmbeddedIp(_) => None,
+            Encap(_) | Tcp(_) | EmbeddedIp(_) => None,
         }
     }
 }

net/src/parse.rs
@@ -152,7 +152,7 @@ impl Reader<'_> {
         })
     }
 
-    fn consume(&mut self, n: NonZero<u16>) -> Result<(), LengthError> {
+    pub(crate) fn consume(&mut self, n: NonZero<u16>) -> Result<(), LengthError> {
         if n.get() > self.remaining {
             return Err(LengthError {
                 expected: n.into_non_zero_usize(),

commit 32324557faccc0c6d71156af2b2306b760762566

fix(net): Pass the right buffer slice when parsing Icmp4/Icmp6
We do not account for the consumed bytes in the packet's buffer when
passing the slice to Icmp4.parse_with()/Icmp6.parse_with(), usually
resulting in a failure to parse the headers. Let's adjust by using the
right offset: the length of the buffer, minus the length remaining to
parse.

Fixes: 803338d3667d ("feat(net): Parse payload for ICMP")
Signed-off-by: Quentin Monnet <qmo@qmon.net>

net/src/icmp4/mod.rs
@@ -153,8 +153,11 @@ impl Icmp4 {
         if !self.is_error_message() {
             return None;
         }
-        let (mut headers, consumed) =
-            EmbeddedHeaders::parse_with(EmbeddedIpVersion::Ipv4, cursor.inner).ok()?;
+        let (mut headers, consumed) = EmbeddedHeaders::parse_with(
+            EmbeddedIpVersion::Ipv4,
+            &cursor.inner[cursor.inner.len() - cursor.remaining as usize..],
+        )
+        .ok()?;
         cursor.consume(consumed).ok()?;
 
         // Mark whether the payload of the embedded IP packet is full

net/src/icmp6/mod.rs
@@ -89,8 +89,11 @@ impl Icmp6 {
         if !self.is_error_message() {
             return None;
         }
-        let (mut headers, consumed) =
-            EmbeddedHeaders::parse_with(EmbeddedIpVersion::Ipv6, cursor.inner).ok()?;
+        let (mut headers, consumed) = EmbeddedHeaders::parse_with(
+            EmbeddedIpVersion::Ipv6,
+            &cursor.inner[cursor.inner.len() - cursor.remaining as usize..],
+        )
+        .ok()?;
         cursor.consume(consumed).ok()?;
 
         // Mark whether the payload of the embedded IP packet is full