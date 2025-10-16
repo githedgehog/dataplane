githedgehog/dataplane

commit 85f935e9027b20b6c32327879580c3388e8349e2

feat(net): Update embedded IP's checksum in Headers' update_checksum()
When updating checksums for a packet, we update the nework-layer and
transport-layer headers' checksums for the packet. If this packet is an
ICMP Error message and embeds a (possibly truncated) IP packet, we want
to update the checksum for that inner IP header, too. Let's add it.

As mentioned in the comment, it would be desirable to update the inner
transport layer as well. But we refrain from doing that, because it's
possible that the inner packet is truncated, in which case we cannot
compute the new checksum correctly. We can check whether the payload is
full and update accordingly, or we can perform an unconditional,
incremental update of the checksum, based on the old and new values
changed in the header.

Given that the unconditional, incremental update looks cheaper (and more
robust) than evaluating whether the inner packet is full, we choose this
option. This means we cannot do the incremental checksum update at this
location, because we do not know what fields have changed in the header
and what their old values were. Instead, leave it to the processing
code, such as the NAT logic, to run the transport checksum update as
they see fit, and skip the layer here in update_checksum().

Signed-off-by: Quentin Monnet <qmo@qmon.net>

net/src/headers/mod.rs
@@ -604,6 +604,19 @@ impl Headers {
             return;
         };
         transport.update_checksum(net, payload.as_ref());
+
+        if let Some(embedded_ip) = self.embedded_ip.as_mut() {
+            let Some(inner_net) = embedded_ip.try_inner_ip_mut() else {
+                trace!("no inner network header in ICMP: can't update checksum");
+                return;
+            };
+            inner_net.update_checksum();
+
+            // WARNING: We do NOT update ICMP Error message inner transport checksum here!
+            // This is because we're not sure the transport header and payload are full, so we want
+            // incremental checksum updates that require knowledge of the previous values, if we've
+            // changed them (for example: NAT). Leave this to (for example) the NAT code.
+        }
     }
 }
 

commit 1459dcb5710abe6ffac08397ba7bd0f256fdd98b

fix(net): Fix ICMP Error messages checksum payload computation
Some headers such as TCP, UDP, ICMP, have their checksum computed based
on their payload. In the case of ICMPv4 and ICMPv6, this payload may
contain an inner IP packet (possibly truncated).

Now that we parse this inner IP packet, we've got a problem: we store
the inner headers in dedicated structures, and the "payload" for the
packet is the payload _under these headers_, meaning that it's no longer
equal to the outer transport payload - in other words, packet.payload()
is no longer equal to the ICMP header's payload.

As a consequence, we need to regenerate a payload for ICMP Error
messages by de-parsing the embedded headers to a buffer, followed by the
packet's payload. This is painful, but not really avoidable.

Note: In the future we might want to refactor this step as part as the
Icmp4Checksum and Icmp6Checksum generation; but these objects take
pointers to existing payload (they don't store a vector of their own)
and this would involve too much refactoring, so we leave it out for now.

Also drop IcmpAnyMut's implementation of is_error_message(), which is
unused at the moment. We can always re-add it later if necessary, it's
identical to IcmpAny's implementation anyway.

Fixes: 85f935e9027b ("feat(net): Update embedded IP's checksum in Headers' update_checksum()")
Signed-off-by: Quentin Monnet <qmo@qmon.net>

net/src/headers/mod.rs
@@ -166,7 +166,12 @@ impl Net {
 }
 
 impl Transport {
-    pub(crate) fn update_checksum(&mut self, net: &Net, payload: impl AsRef<[u8]>) {
+    pub(crate) fn update_checksum(
+        &mut self,
+        net: &Net,
+        embedded_headers: Option<&EmbeddedHeaders>,
+        payload: impl AsRef<[u8]>,
+    ) {
         match (net, self) {
             (net, Transport::Tcp(tcp)) => {
                 tcp.update_checksum(&TcpChecksumPayload::new(net, payload.as_ref()))
@@ -177,18 +182,38 @@ impl Transport {
                     .unwrap_or_else(|()| unreachable!()); // Updating UDP checksum never fails
             }
             (Net::Ipv4(_), Transport::Icmp4(icmp4)) => {
-                icmp4
-                    .update_checksum(payload.as_ref())
-                    .unwrap_or_else(|()| unreachable!()); // Updating ICMPv4 checksum never fails
+                if icmp4.is_error_message() && embedded_headers.is_some() {
+                    let checksum_payload =
+                        icmp4.get_payload_for_checksum(embedded_headers, payload.as_ref());
+                    icmp4
+                        .update_checksum(checksum_payload.as_ref())
+                        .unwrap_or_else(|()| unreachable!()); // Updating ICMPv4 checksum never fails
+                } else {
+                    icmp4
+                        .update_checksum(payload.as_ref())
+                        .unwrap_or_else(|()| unreachable!()); // Updating ICMPv4 checksum never fails
+                };
             }
-            (Net::Ipv6(ip), Transport::Icmp6(icmpv6)) => {
-                icmpv6
-                    .update_checksum(&Icmp6ChecksumPayload::new(
-                        ip.source().inner(),
-                        ip.destination(),
-                        payload.as_ref(),
-                    ))
-                    .unwrap_or_else(|()| unreachable!()); // Updating ICMPv6 checksum never fails
+            (Net::Ipv6(ip), Transport::Icmp6(icmp6)) => {
+                if icmp6.is_error_message() && embedded_headers.is_some() {
+                    let checksum_payload =
+                        icmp6.get_payload_for_checksum(embedded_headers, payload.as_ref());
+                    icmp6
+                        .update_checksum(&Icmp6ChecksumPayload::new(
+                            ip.source().inner(),
+                            ip.destination(),
+                            checksum_payload.as_ref(),
+                        ))
+                        .unwrap_or_else(|()| unreachable!()); // Updating ICMPv6 checksum never fails
+                } else {
+                    icmp6
+                        .update_checksum(&Icmp6ChecksumPayload::new(
+                            ip.source().inner(),
+                            ip.destination(),
+                            payload.as_ref(),
+                        ))
+                        .unwrap_or_else(|()| unreachable!()); // Updating ICMPv6 checksum never fails
+                }
             }
             // TODO: statically ensure that this is unreachable
             (Net::Ipv6(_), Transport::Icmp4(_)) => debug!("illegal: icmpv4 in ipv6"),

net/src/icmp4/mod.rs
@@ -7,7 +7,8 @@ mod checksum;
 
 pub use checksum::*;
 
-use crate::headers::{EmbeddedHeaders, EmbeddedIpVersion};
+use crate::headers::{AbstractEmbeddedHeaders, EmbeddedHeaders, EmbeddedIpVersion};
+use crate::icmp_any::get_payload_for_checksum;
 use crate::parse::{
     DeParse, DeParseError, IntoNonZeroUSize, LengthError, Parse, ParseError, ParseWith, Reader,
 };
@@ -169,6 +170,19 @@ impl Icmp4 {
         );
 
         Some(headers)
+    }
+
+    /// Generate the payload for checksum calculation
+    #[must_use]
+    pub fn get_payload_for_checksum(
+        &self,
+        embedded_headers: Option<&impl AbstractEmbeddedHeaders>,
+        payload: &[u8],
+    ) -> Vec<u8> {
+        if !self.is_error_message() {
+            return payload.to_vec();
+        }
+        get_payload_for_checksum(embedded_headers, payload)
     }
 }
 

net/src/icmp6/mod.rs
@@ -7,7 +7,8 @@ mod checksum;
 
 pub use checksum::*;
 
-use crate::headers::{EmbeddedHeaders, EmbeddedIpVersion};
+use crate::headers::{AbstractEmbeddedHeaders, EmbeddedHeaders, EmbeddedIpVersion};
+use crate::icmp_any::get_payload_for_checksum;
 use crate::parse::{
     DeParse, DeParseError, IntoNonZeroUSize, LengthError, Parse, ParseError, ParseWith, Reader,
 };
@@ -105,6 +106,19 @@ impl Icmp6 {
         );
 
         Some(headers)
+    }
+
+    /// Generate the payload for checksum calculation
+    #[must_use]
+    pub fn get_payload_for_checksum(
+        &self,
+        embedded_headers: Option<&impl AbstractEmbeddedHeaders>,
+        payload: &[u8],
+    ) -> Vec<u8> {
+        if !self.is_error_message() {
+            return payload.to_vec();
+        }
+        get_payload_for_checksum(embedded_headers, payload)
     }
 }
 

net/src/icmp_any/checksum.rs
@@ -5,9 +5,10 @@
 
 use super::{IcmpAny, IcmpAnyMut};
 use crate::checksum::Checksum;
-use crate::headers::Net;
+use crate::headers::{AbstractEmbeddedHeaders, Net};
 use crate::icmp4::Icmp4Checksum;
 use crate::icmp6::{Icmp6Checksum, Icmp6ChecksumPayload};
+use crate::parse::DeParse;
 use core::fmt::{Display, Formatter};
 use std::fmt::Debug;
 
@@ -220,4 +221,41 @@ impl Checksum for IcmpAnyMut<'_> {
         }
         Ok(self)
     }
+}
+
+pub(crate) fn get_payload_for_checksum(
+    embedded_headers: Option<&impl AbstractEmbeddedHeaders>,
+    payload: &[u8],
+) -> Vec<u8> {
+    let Some(embedded_headers) = embedded_headers else {
+        return payload.to_vec();
+    };
+    let Some(embedded_ip_header) = embedded_headers.try_inner_ip() else {
+        return payload.to_vec();
+    };
+    let embedded_transport_header = embedded_headers.try_embedded_transport();
+
+    let inner_ip_header_length = embedded_ip_header.size().get() as usize;
+    let inner_transport_header_length =
+        embedded_transport_header.map_or(0, |header| header.size().get() as usize);
+
+    let mut offset = 0;
+    let mut icmp_payload =
+        vec![0; payload.len() + inner_ip_header_length + inner_transport_header_length];
+    // Write inner IP header
+    offset += embedded_ip_header
+        .deparse(&mut icmp_payload[offset..])
+        .unwrap_or_else(|_| unreachable!())
+        .get() as usize;
+    // Write inner transport header
+    if let Some(transport) = embedded_transport_header {
+        offset += transport
+            .deparse(&mut icmp_payload[offset..])
+            .unwrap_or_else(|_| unreachable!())
+            .get() as usize;
+    }
+    // Write payload (including ICMP padding/extensions, if any)
+    icmp_payload[offset..].copy_from_slice(payload);
+
+    icmp_payload
 }

net/src/icmp_any/mod.rs
@@ -3,7 +3,7 @@
 
 //! Common logic for `ICMPv4` and `ICMPv6`
 
-use crate::headers::Transport;
+use crate::headers::{AbstractEmbeddedHeaders, Transport};
 use crate::icmp4::Icmp4;
 use crate::icmp6::Icmp6;
 
@@ -47,6 +47,19 @@ impl IcmpAny<'_> {
             IcmpAny::V6(icmp6) => icmp6.is_error_message(),
         }
     }
+
+    /// Returns the payload for checksum computation.
+    #[must_use]
+    pub fn get_payload_for_checksum(
+        &self,
+        embedded_headers: Option<&impl AbstractEmbeddedHeaders>,
+        payload: &[u8],
+    ) -> Vec<u8> {
+        match self {
+            IcmpAny::V4(icmp4) => icmp4.get_payload_for_checksum(embedded_headers, payload),
+            IcmpAny::V6(icmp6) => icmp6.get_payload_for_checksum(embedded_headers, payload),
+        }
+    }
 }
 
 /// Enum representing a mutable [`Icmp4`] or [`Icmp6`].
@@ -64,17 +77,6 @@ impl<'a> TryFrom<&'a mut Transport> for IcmpAnyMut<'a> {
             Transport::Icmp4(icmp4) => Ok(IcmpAnyMut::V4(icmp4)),
             Transport::Icmp6(icmp6) => Ok(IcmpAnyMut::V6(icmp6)),
             _ => Err(IcmpAnyError::NotIcmp),
-        }
-    }
-}
-
-impl IcmpAnyMut<'_> {
-    /// Returns `true` if this is an ICMP Error message, `false` otherwise.
-    #[must_use]
-    pub fn is_error_message(&self) -> bool {
-        match self {
-            IcmpAnyMut::V4(icmp4) => icmp4.is_error_message(),
-            IcmpAnyMut::V6(icmp6) => icmp6.is_error_message(),
         }
     }
 }

net/src/packet/test_utils.rs
@@ -94,7 +94,7 @@ pub fn build_test_ipv4_packet_with_transport(
     let net = Net::Ipv4(ipv4);
 
     if let Some(transport) = transport.as_mut() {
-        transport.update_checksum(&net, []);
+        transport.update_checksum(&net, None, []);
     }
 
     headers.net(Some(net));