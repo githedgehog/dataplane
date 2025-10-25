githedgehog/dataplane

commit f9652c527b2e60219f778b418b242ffd10751c00

feat(net): Support ICMP-in-ICMP
So we recently added support for parsing ICMP Error messages' inner
messages. We processed the network layer of the inner packet, and the
transport layer of the inner packet, which can be only TCP or UDP.

... Says who? As it turns out, we can just as well have an error message
for an ICMP Echo Request, for example, in which case we need to NAT the
message just like for TCP or UDP.

Add the required infrastructure. It's more code that I'd like, but I
don't really see a simple alternative: we get new traits for
EmbeddedTransport, and support for potentially truncated ICMP headers
messages, even though it's unlikely we get truncated ones - RFC 792 says
there should be at least 8 bytes of the transport header, which is
enough to hold most ICMPv4 or ICMPv6 headers (as defined in etherparse,
at least), with the exception of Timestamp Requests and Replies. The
code for the truncated headers (TruncatedIcmp4, TruncatedIcmp6) is
largely derived from TruncatedTcp and TruncatedUdp. Similarly, we
implement Checksum for these new types. The annoying bit, as always when
adding ICMP support for a transport layer, is that we suddenly get a
header that may not have source and destination ports. Happily, the only
location where this matters is in NAT code, where we don't support fully
ICMP yet - we'll add the relevant processing soon.

Signed-off-by: Quentin Monnet <qmo@qmon.net>

net/src/headers/embedded.rs
@@ -4,6 +4,8 @@
 use crate::checksum::Checksum;
 use crate::eth::EthError;
 use crate::headers::{MAX_NET_EXTENSIONS, Net, NetExt};
+use crate::icmp4::{Icmp4Checksum, TruncatedIcmp4};
+use crate::icmp6::{Icmp6Checksum, TruncatedIcmp6};
 use crate::impl_from_for_enum;
 use crate::ip_auth::IpAuth;
 use crate::ipv4::Ipv4;
@@ -123,13 +125,17 @@ impl EmbeddedHeaders {
         match &mut self.transport {
             None
             | Some(EmbeddedTransport::Tcp(TruncatedTcp::PartialHeader(_)))
-            | Some(EmbeddedTransport::Udp(TruncatedUdp::PartialHeader(_))) => {
+            | Some(EmbeddedTransport::Udp(TruncatedUdp::PartialHeader(_)))
+            | Some(EmbeddedTransport::Icmp4(TruncatedIcmp4::PartialHeader(_)))
+            | Some(EmbeddedTransport::Icmp6(TruncatedIcmp6::PartialHeader(_))) => {
                 // We couldn't parse the full transport header, of course we don't have the full,
                 // valid payload
                 return;
             }
             Some(EmbeddedTransport::Tcp(TruncatedTcp::FullHeader(_)))
-            | Some(EmbeddedTransport::Udp(TruncatedUdp::FullHeader(_))) => {
+            | Some(EmbeddedTransport::Udp(TruncatedUdp::FullHeader(_)))
+            | Some(EmbeddedTransport::Icmp4(TruncatedIcmp4::FullHeader(_)))
+            | Some(EmbeddedTransport::Icmp6(TruncatedIcmp6::FullHeader(_))) => {
                 // There's a chance payload is full, keep going
             }
         }
@@ -423,46 +429,62 @@ impl_from_for_enum![
     IpV6Ext(Ipv6Ext)
 ];
 
+#[derive(Debug, thiserror::Error)]
+pub enum EmbeddedHeaderError {
+    #[error("No ports used by embedded transport header")]
+    NoPorts,
+}
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum EmbeddedTransport {
     Tcp(TruncatedTcp),
     Udp(TruncatedUdp),
+    Icmp4(TruncatedIcmp4),
+    Icmp6(TruncatedIcmp6),
 }
 
 impl EmbeddedTransport {
-    pub fn source(&self) -> NonZero<u16> {
+    pub fn source(&self) -> Option<NonZero<u16>> {
         match self {
-            EmbeddedTransport::Tcp(tcp) => tcp.source().into(),
-            EmbeddedTransport::Udp(udp) => udp.source().into(),
+            EmbeddedTransport::Tcp(tcp) => Some(tcp.source().into()),
+            EmbeddedTransport::Udp(udp) => Some(udp.source().into()),
+            _ => None,
         }
     }
 
-    pub fn destination(&self) -> NonZero<u16> {
+    pub fn destination(&self) -> Option<NonZero<u16>> {
         match self {
-            EmbeddedTransport::Tcp(tcp) => tcp.destination().into(),
-            EmbeddedTransport::Udp(udp) => udp.destination().into(),
+            EmbeddedTransport::Tcp(tcp) => Some(tcp.destination().into()),
+            EmbeddedTransport::Udp(udp) => Some(udp.destination().into()),
+            _ => None,
         }
     }
 
-    pub fn set_source(&mut self, port: NonZero<u16>) {
+    pub fn set_source(&mut self, port: NonZero<u16>) -> Result<(), EmbeddedHeaderError> {
         match self {
             EmbeddedTransport::Tcp(tcp) => {
                 tcp.set_source(TcpPort::new(port));
+                Ok(())
             }
             EmbeddedTransport::Udp(udp) => {
                 udp.set_source(UdpPort::new(port));
+                Ok(())
             }
+            _ => Err(EmbeddedHeaderError::NoPorts),
         }
     }
 
-    pub fn set_destination(&mut self, port: NonZero<u16>) {
+    pub fn set_destination(&mut self, port: NonZero<u16>) -> Result<(), EmbeddedHeaderError> {
         match self {
             EmbeddedTransport::Tcp(tcp) => {
                 tcp.set_destination(TcpPort::new(port));
+                Ok(())
             }
             EmbeddedTransport::Udp(udp) => {
                 udp.set_destination(UdpPort::new(port));
+                Ok(())
             }
+            _ => Err(EmbeddedHeaderError::NoPorts),
         }
     }
 
@@ -470,6 +492,8 @@ impl EmbeddedTransport {
         match self {
             EmbeddedTransport::Tcp(tcp) => tcp.checksum().map(u16::from),
             EmbeddedTransport::Udp(udp) => udp.checksum().map(u16::from),
+            EmbeddedTransport::Icmp4(icmp) => icmp.checksum().map(u16::from),
+            EmbeddedTransport::Icmp6(icmp) => icmp.checksum().map(u16::from),
         }
     }
 
@@ -491,6 +515,22 @@ impl EmbeddedTransport {
                     new_value,
                 );
             }
+            EmbeddedTransport::Icmp4(icmp) => {
+                // Silently ignore errors if transport header is truncated
+                let _ = icmp.increment_update_checksum(
+                    Icmp4Checksum::new(current_checksum),
+                    old_value,
+                    new_value,
+                );
+            }
+            EmbeddedTransport::Icmp6(icmp) => {
+                // Silently ignore errors if transport header is truncated
+                let _ = icmp.increment_update_checksum(
+                    Icmp6Checksum::new(current_checksum),
+                    old_value,
+                    new_value,
+                );
+            }
         }
     }
 }
@@ -502,6 +542,8 @@ impl DeParse for EmbeddedTransport {
         match self {
             EmbeddedTransport::Tcp(tcp) => tcp.size(),
             EmbeddedTransport::Udp(udp) => udp.size(),
+            EmbeddedTransport::Icmp4(icmp) => icmp.size(),
+            EmbeddedTransport::Icmp6(icmp) => icmp.size(),
         }
     }
 
@@ -509,6 +551,8 @@ impl DeParse for EmbeddedTransport {
         match self {
             EmbeddedTransport::Tcp(tcp) => tcp.deparse(buf),
             EmbeddedTransport::Udp(udp) => udp.deparse(buf),
+            EmbeddedTransport::Icmp4(icmp) => icmp.deparse(buf),
+            EmbeddedTransport::Icmp6(icmp) => icmp.deparse(buf),
         }
     }
 }
@@ -649,6 +693,62 @@ impl TryTruncatedUdpMut for EmbeddedHeaders {
     }
 }
 
+// ICMPv4 traits
+
+pub trait TryTruncatedIcmp4 {
+    fn try_truncated_icmp4(&self) -> Option<&TruncatedIcmp4>;
+}
+
+pub trait TryTruncatedIcmp4Mut {
+    fn try_truncated_icmp4_mut(&mut self) -> Option<&mut TruncatedIcmp4>;
+}
+
+impl TryTruncatedIcmp4 for EmbeddedHeaders {
+    fn try_truncated_icmp4(&self) -> Option<&TruncatedIcmp4> {
+        match &self.transport {
+            Some(EmbeddedTransport::Icmp4(header)) => Some(header),
+            _ => None,
+        }
+    }
+}
+
+impl TryTruncatedIcmp4Mut for EmbeddedHeaders {
+    fn try_truncated_icmp4_mut(&mut self) -> Option<&mut TruncatedIcmp4> {
+        match &mut self.transport {
+            Some(EmbeddedTransport::Icmp4(header)) => Some(header),
+            _ => None,
+        }
+    }
+}
+
+// ICMPv6 traits
+
+pub trait TryTruncatedIcmp6 {
+    fn try_truncated_icmp6(&self) -> Option<&TruncatedIcmp6>;
+}
+
+pub trait TryTruncatedIcmp6Mut {
+    fn try_truncated_icmp6_mut(&mut self) -> Option<&mut TruncatedIcmp6>;
+}
+
+impl TryTruncatedIcmp6 for EmbeddedHeaders {
+    fn try_truncated_icmp6(&self) -> Option<&TruncatedIcmp6> {
+        match &self.transport {
+            Some(EmbeddedTransport::Icmp6(header)) => Some(header),
+            _ => None,
+        }
+    }
+}
+
+impl TryTruncatedIcmp6Mut for EmbeddedHeaders {
+    fn try_truncated_icmp6_mut(&mut self) -> Option<&mut TruncatedIcmp6> {
+        match &mut self.transport {
+            Some(EmbeddedTransport::Icmp6(header)) => Some(header),
+            _ => None,
+        }
+    }
+}
+
 // Generic Transport traits
 
 pub trait TryEmbeddedTransport {
@@ -678,6 +778,8 @@ pub trait AbstractEmbeddedHeaders:
     + TryInnerIp
     + TryTruncatedTcp
     + TryTruncatedUdp
+    + TryTruncatedIcmp4
+    + TryTruncatedIcmp6
     + TryEmbeddedTransport
     + DeParse
 {
@@ -690,6 +792,8 @@ impl<T> AbstractEmbeddedHeaders for T where
         + TryInnerIp
         + TryTruncatedTcp
         + TryTruncatedUdp
+        + TryTruncatedIcmp4
+        + TryTruncatedIcmp6
         + TryEmbeddedTransport
         + DeParse
 {
@@ -702,6 +806,8 @@ pub trait AbstractEmbeddedHeadersMut:
     + TryInnerIpMut
     + TryTruncatedTcpMut
     + TryTruncatedUdpMut
+    + TryTruncatedIcmp4Mut
+    + TryTruncatedIcmp6Mut
     + TryEmbeddedTransportMut
 {
 }
@@ -713,6 +819,8 @@ impl<T> AbstractEmbeddedHeadersMut for T where
         + TryInnerIpMut
         + TryTruncatedTcpMut
         + TryTruncatedUdpMut
+        + TryTruncatedIcmp4Mut
+        + TryTruncatedIcmp6Mut
         + TryEmbeddedTransportMut
 {
 }
@@ -770,6 +878,24 @@ where
     }
 }
 
+impl<T> TryTruncatedIcmp4 for T
+where
+    T: TryEmbeddedHeaders,
+{
+    fn try_truncated_icmp4(&self) -> Option<&TruncatedIcmp4> {
+        self.embedded_headers()?.try_truncated_icmp4()
+    }
+}
+
+impl<T> TryTruncatedIcmp6 for T
+where
+    T: TryEmbeddedHeaders,
+{
+    fn try_truncated_icmp6(&self) -> Option<&TruncatedIcmp6> {
+        self.embedded_headers()?.try_truncated_icmp6()
+    }
+}
+
 impl<T> TryEmbeddedTransport for T
 where
     T: TryEmbeddedHeaders,
@@ -821,6 +947,24 @@ where
 {
     fn try_truncated_udp_mut(&mut self) -> Option<&mut TruncatedUdp> {
         self.embedded_headers_mut()?.try_truncated_udp_mut()
+    }
+}
+
+impl<T> TryTruncatedIcmp4Mut for T
+where
+    T: TryEmbeddedHeadersMut,
+{
+    fn try_truncated_icmp4_mut(&mut self) -> Option<&mut TruncatedIcmp4> {
+        self.embedded_headers_mut()?.try_truncated_icmp4_mut()
+    }
+}
+
+impl<T> TryTruncatedIcmp6Mut for T
+where
+    T: TryEmbeddedHeadersMut,
+{
+    fn try_truncated_icmp6_mut(&mut self) -> Option<&mut TruncatedIcmp6> {
+        self.embedded_headers_mut()?.try_truncated_icmp6_mut()
     }
 }
 

net/src/icmp4/mod.rs
@@ -4,8 +4,10 @@
 //! `ICMPv4` header type and logic.
 
 mod checksum;
+mod truncated;
 
 pub use checksum::*;
+pub use truncated::*;
 
 use crate::headers::{AbstractEmbeddedHeaders, EmbeddedHeaders, EmbeddedIpVersion};
 use crate::icmp_any::get_payload_for_checksum;
@@ -237,7 +239,7 @@ impl DeParse for Icmp4 {
 #[cfg(any(test, feature = "bolero"))]
 mod contract {
     use crate::headers::{EmbeddedHeaders, EmbeddedTransport, Net};
-    use crate::icmp4::Icmp4;
+    use crate::icmp4::{Icmp4, TruncatedIcmp4};
     use crate::ip::NextHeader;
     use crate::ipv4::GenWithNextHeader;
     use crate::parse::{DeParse, DeParseError, IntoNonZeroUSize, LengthError, Parse, ParseError};
@@ -364,12 +366,15 @@ mod contract {
 
         #[allow(clippy::unwrap_used)]
         fn generate<D: Driver>(&self, driver: &mut D) -> Option<Self::Output> {
-            let transport = match driver.produce::<u32>()? % 9 {
+            let transport = match driver.produce::<u32>()? % 11 {
                 0..=3 => Some(EmbeddedTransport::Tcp(
                     driver.produce::<TruncatedTcp>().unwrap(),
                 )),
                 4..=7 => Some(EmbeddedTransport::Udp(
                     driver.produce::<TruncatedUdp>().unwrap(),
+                )),
+                8..=9 => Some(EmbeddedTransport::Icmp4(
+                    driver.produce::<TruncatedIcmp4>().unwrap(),
                 )),
                 _ => None,
             };
@@ -381,6 +386,14 @@ mod contract {
                 Some(EmbeddedTransport::Udp(_)) => {
                     let net_gen = GenWithNextHeader(NextHeader::UDP);
                     Some(Net::Ipv4(net_gen.generate(driver)?))
+                }
+                Some(EmbeddedTransport::Icmp4(_)) => {
+                    let net_gen = GenWithNextHeader(NextHeader::ICMP);
+                    Some(Net::Ipv4(net_gen.generate(driver)?))
+                }
+                Some(EmbeddedTransport::Icmp6(_)) => {
+                    // We never produce ICMPv6 headers to embed inside ICMPv4 Error messages
+                    unreachable!()
                 }
                 None => {
                     if driver.produce::<bool>()? {

net/src/icmp6/mod.rs
@@ -4,8 +4,10 @@
 //! `Icmp6` header type and logic.
 
 mod checksum;
+mod truncated;
 
 pub use checksum::*;
+pub use truncated::*;
 
 use crate::headers::{AbstractEmbeddedHeaders, EmbeddedHeaders, EmbeddedIpVersion};
 use crate::icmp_any::get_payload_for_checksum;
@@ -173,7 +175,7 @@ impl DeParse for Icmp6 {
 #[cfg(any(test, feature = "bolero"))]
 mod contract {
     use crate::headers::{EmbeddedHeaders, EmbeddedTransport, Net};
-    use crate::icmp6::Icmp6;
+    use crate::icmp6::{Icmp6, TruncatedIcmp6};
     use crate::ip::NextHeader;
     use crate::ipv6::GenWithNextHeader;
     use crate::parse::{DeParse, DeParseError, IntoNonZeroUSize, LengthError, Parse};
@@ -285,10 +287,18 @@ mod contract {
     impl ValueGenerator for Icmp6EmbeddedHeadersGenerator {
         type Output = EmbeddedHeaders;
 
+        #[allow(clippy::unwrap_used)]
         fn generate<D: Driver>(&self, driver: &mut D) -> Option<Self::Output> {
-            let transport = match driver.produce::<u32>()? % 9 {
-                0..=3 => Some(EmbeddedTransport::Tcp(driver.produce::<TruncatedTcp>()?)),
-                4..=7 => Some(EmbeddedTransport::Udp(driver.produce::<TruncatedUdp>()?)),
+            let transport = match driver.produce::<u32>()? % 11 {
+                0..=3 => Some(EmbeddedTransport::Tcp(
+                    driver.produce::<TruncatedTcp>().unwrap(),
+                )),
+                4..=7 => Some(EmbeddedTransport::Udp(
+                    driver.produce::<TruncatedUdp>().unwrap(),
+                )),
+                8..=9 => Some(EmbeddedTransport::Icmp6(
+                    driver.produce::<TruncatedIcmp6>().unwrap(),
+                )),
                 _ => None,
             };
             let net = match transport {
@@ -298,6 +308,14 @@ mod contract {
                 }
                 Some(EmbeddedTransport::Udp(_)) => {
                     let net_gen = GenWithNextHeader(NextHeader::UDP);
+                    Some(Net::Ipv6(net_gen.generate(driver)?))
+                }
+                Some(EmbeddedTransport::Icmp4(_)) => {
+                    // We never produce ICMPv4 headers to embed inside ICMPv6 Error messages
+                    unreachable!()
+                }
+                Some(EmbeddedTransport::Icmp6(_)) => {
+                    let net_gen = GenWithNextHeader(NextHeader::ICMP);
                     Some(Net::Ipv6(net_gen.generate(driver)?))
                 }
                 None => {

nat/src/icmp_error_msg.rs
@@ -8,8 +8,8 @@ use super::NatTranslationData;
 use net::buffer::PacketBufferMut;
 use net::checksum::{Checksum, ChecksumError};
 use net::headers::{
-    TryEmbeddedHeaders, TryEmbeddedHeadersMut, TryEmbeddedTransportMut, TryHeaders, TryInnerIpMut,
-    TryInnerIpv4, TryIp, TryTransport,
+    EmbeddedTransport, TryEmbeddedHeaders, TryEmbeddedHeadersMut, TryEmbeddedTransportMut,
+    TryHeaders, TryInnerIpMut, TryInnerIpv4, TryIp, TryTransport,
 };
 use net::icmp_any::{IcmpAny, IcmpAnyChecksumErrorPlaceholder, IcmpAnyChecksumPayload};
 use net::ipv4::Ipv4;
@@ -135,10 +135,26 @@ pub(crate) fn stateful_translate_icmp_inner<Buf: PacketBufferMut>(
         // TODO: Log trace anyway?
         return Ok(());
     };
-    let (old_src_port, old_dst_port) = (transport.source().into(), transport.destination().into());
+    if matches!(
+        transport,
+        EmbeddedTransport::Icmp4(_) | EmbeddedTransport::Icmp6(_)
+    ) {
+        // FIXME: We don't support ICMP identifier's translation yet. We're done (for now).
+        return Ok(());
+    }
+    // We returned early for ICMP, so we have TCP or UDP, and always source and destination ports
+    let (old_src_port, old_dst_port) = (
+        transport.source().unwrap_or_else(|| unreachable!()).into(),
+        transport
+            .destination()
+            .unwrap_or_else(|| unreachable!())
+            .into(),
+    );
 
     if let Some(target_src_port) = target_src_port {
-        transport.set_source(target_src_port.into());
+        transport
+            .set_source(target_src_port.into())
+            .unwrap_or_else(|_| unreachable!());
         // We don't know whether the header and payload are full: the easiest way to deal with
         // transport checksum update is to do an unconditional, incremental update here. Note
         // that this checksum will not be updated again when deparsing the packet.
@@ -147,7 +163,9 @@ pub(crate) fn stateful_translate_icmp_inner<Buf: PacketBufferMut>(
         }
     }
     if let Some(target_dst_port) = target_dst_port {
-        transport.set_destination(target_dst_port.into());
+        transport
+            .set_destination(target_dst_port.into())
+            .unwrap_or_else(|_| unreachable!());
         if let Some(current_checksum) = transport.checksum() {
             transport.update_checksum(current_checksum, old_dst_port, target_dst_port.as_u16());
         }
@@ -357,7 +375,7 @@ mod bolero_tests {
     fn get_inner_ports(packet: &Packet<TestBuffer>) -> Option<(NonZero<u16>, NonZero<u16>)> {
         packet
             .try_embedded_transport()
-            .map(|tcp| (tcp.source(), tcp.destination()))
+            .and_then(|transport| transport.source().zip(transport.destination()))
     }
 
     #[test]

commit 62a948017e3034cf4de256b617b7ce62e129b1a6

fix(net): ICMP-in-ICMP for NAT: try harder
When we added support for parsing inner ICMP packets inside of ICMP
Error messages, we missed a number of additional changes that also need
to happen in the code, in particular if we want headers parsing to work
as expected. Discovered while wondering why Bolero tests wouldn't
produce inner ICMP headers, ever.

Fixes: f9652c527b2e ("feat(net): Support ICMP-in-ICMP")
Signed-off-by: Quentin Monnet <qmo@qmon.net>

net/src/headers/embedded.rs
@@ -183,7 +183,19 @@ impl EmbeddedHeaders {
         let transport_header_length = match &mut self.transport {
             Some(EmbeddedTransport::Tcp(TruncatedTcp::FullHeader(tcp))) => tcp.header_len().get(),
             Some(EmbeddedTransport::Udp(TruncatedUdp::FullHeader(_))) => 8,
-            _ => unreachable!(), // Checked at the beginning of the function
+            Some(EmbeddedTransport::Icmp4(TruncatedIcmp4::FullHeader(icmp))) => {
+                icmp.size().get() as usize
+            }
+            Some(EmbeddedTransport::Icmp6(TruncatedIcmp6::FullHeader(icmp))) => {
+                icmp.size().get() as usize
+            }
+            None
+            | Some(
+                EmbeddedTransport::Tcp(TruncatedTcp::PartialHeader(_))
+                | EmbeddedTransport::Udp(TruncatedUdp::PartialHeader(_))
+                | EmbeddedTransport::Icmp4(TruncatedIcmp4::PartialHeader(_))
+                | EmbeddedTransport::Icmp6(TruncatedIcmp6::PartialHeader(_)),
+            ) => unreachable!(), // Checked at the beginning of the function
         };
 
         // Compute the size of the IP headers
@@ -308,6 +320,12 @@ impl ParseWith for EmbeddedHeaders {
                 EmbeddedHeader::Udp(udp) => {
                     this.transport = Some(EmbeddedTransport::Udp(udp));
                 }
+                EmbeddedHeader::Icmp4(icmp4) => {
+                    this.transport = Some(EmbeddedTransport::Icmp4(icmp4));
+                }
+                EmbeddedHeader::Icmp6(icmp6) => {
+                    this.transport = Some(EmbeddedTransport::Icmp6(icmp6));
+                }
             }
             match header {
                 None => {
@@ -388,13 +406,15 @@ pub(crate) enum EmbeddedHeader {
     Ipv6(Ipv6),
     Tcp(TruncatedTcp),
     Udp(TruncatedUdp),
+    Icmp4(TruncatedIcmp4),
+    Icmp6(TruncatedIcmp6),
     IpAuth(IpAuth),
     IpV6Ext(Ipv6Ext), // TODO: break out nested enum.  Nesting is counter productive here
 }
 
 impl EmbeddedHeader {
     fn parse_payload(&self, cursor: &mut Reader) -> Option<EmbeddedHeader> {
-        use EmbeddedHeader::{IpAuth, IpV6Ext, Ipv4, Ipv6, Tcp, Udp};
+        use EmbeddedHeader::{Icmp4, Icmp6, IpAuth, IpV6Ext, Ipv4, Ipv6, Tcp, Udp};
         match self {
             Ipv4(ipv4) => ipv4
                 .parse_embedded_payload(cursor)
@@ -414,7 +434,7 @@ impl EmbeddedHeader {
                     None
                 }
             }
-            Tcp(_) | Udp(_) => None,
+            Tcp(_) | Udp(_) | Icmp4(_) | Icmp6(_) => None,
         }
     }
 }
@@ -425,6 +445,8 @@ impl_from_for_enum![
     Ipv6(Ipv6),
     Udp(TruncatedUdp),
     Tcp(TruncatedTcp),
+    Icmp4(TruncatedIcmp4),
+    Icmp6(TruncatedIcmp6),
     IpAuth(IpAuth),
     IpV6Ext(Ipv6Ext)
 ];

net/src/ipv4/mod.rs
@@ -4,7 +4,7 @@
 //! Ipv4 Address type and manipulation
 
 use crate::headers::{EmbeddedHeader, Header};
-use crate::icmp4::Icmp4;
+use crate::icmp4::{Icmp4, TruncatedIcmp4};
 use crate::impl_from_for_enum;
 use crate::ip::NextHeader;
 use crate::ip_auth::IpAuth;
@@ -318,6 +318,7 @@ impl Ipv4 {
         match self.0.protocol {
             IpNumber::TCP => cursor.parse_header::<TruncatedTcp, EmbeddedIpv4Next>(),
             IpNumber::UDP => cursor.parse_header::<TruncatedUdp, EmbeddedIpv4Next>(),
+            IpNumber::ICMP => cursor.parse_header::<TruncatedIcmp4, EmbeddedIpv4Next>(),
             IpNumber::AUTHENTICATION_HEADER => cursor.parse_header::<IpAuth, EmbeddedIpv4Next>(),
             _ => {
                 trace!("unsupported protocol: {:?}", self.0.protocol);
@@ -415,6 +416,7 @@ impl From<Ipv4Next> for Header {
 pub(crate) enum EmbeddedIpv4Next {
     Tcp(TruncatedTcp),
     Udp(TruncatedUdp),
+    Icmp4(TruncatedIcmp4),
     IpAuth(IpAuth),
 }
 
@@ -422,7 +424,8 @@ impl_from_for_enum![
     EmbeddedIpv4Next,
     Tcp(TruncatedTcp),
     Udp(TruncatedUdp),
-    IpAuth(IpAuth)
+    Icmp4(TruncatedIcmp4),
+    IpAuth(IpAuth),
 ];
 
 impl From<EmbeddedIpv4Next> for EmbeddedHeader {
@@ -430,6 +433,7 @@ impl From<EmbeddedIpv4Next> for EmbeddedHeader {
         match value {
             EmbeddedIpv4Next::Tcp(x) => EmbeddedHeader::Tcp(x),
             EmbeddedIpv4Next::Udp(x) => EmbeddedHeader::Udp(x),
+            EmbeddedIpv4Next::Icmp4(x) => EmbeddedHeader::Icmp4(x),
             EmbeddedIpv4Next::IpAuth(x) => EmbeddedHeader::IpAuth(x),
         }
     }

net/src/ipv6/mod.rs
@@ -4,7 +4,7 @@
 //! Ipv6 Address type and manipulation
 
 use crate::headers::{EmbeddedHeader, Header};
-use crate::icmp6::Icmp6;
+use crate::icmp6::{Icmp6, TruncatedIcmp6};
 use crate::impl_from_for_enum;
 use crate::ip::NextHeader;
 use crate::ip_auth::IpAuth;
@@ -221,6 +221,7 @@ impl Ipv6 {
         match self.0.next_header {
             IpNumber::TCP => cursor.parse_header::<TruncatedTcp, EmbeddedIpv6Next>(),
             IpNumber::UDP => cursor.parse_header::<TruncatedUdp, EmbeddedIpv6Next>(),
+            IpNumber::IPV6_ICMP => cursor.parse_header::<TruncatedIcmp6, EmbeddedIpv6Next>(),
             IpNumber::AUTHENTICATION_HEADER => cursor.parse_header::<IpAuth, EmbeddedIpv6Next>(),
             IpNumber::IPV6_HEADER_HOP_BY_HOP
             | IpNumber::IPV6_ROUTE_HEADER
@@ -324,6 +325,7 @@ impl_from_for_enum![
 pub(crate) enum EmbeddedIpv6Next {
     Tcp(TruncatedTcp),
     Udp(TruncatedUdp),
+    Icmp6(TruncatedIcmp6),
     IpAuth(IpAuth),
     Ipv6Ext(Ipv6Ext),
 }
@@ -332,6 +334,7 @@ impl_from_for_enum![
     EmbeddedIpv6Next,
     Tcp(TruncatedTcp),
     Udp(TruncatedUdp),
+    Icmp6(TruncatedIcmp6),
     IpAuth(IpAuth),
     Ipv6Ext(Ipv6Ext)
 ];
@@ -426,7 +429,7 @@ impl Ipv6Ext {
     ) -> Option<EmbeddedIpv6ExtNext> {
         use etherparse::ip_number::{
             AUTHENTICATION_HEADER, IPV6_DESTINATION_OPTIONS, IPV6_FRAGMENTATION_HEADER,
-            IPV6_HEADER_HOP_BY_HOP, IPV6_ROUTE_HEADER, TCP, UDP,
+            IPV6_HEADER_HOP_BY_HOP, IPV6_ICMP, IPV6_ROUTE_HEADER, TCP, UDP,
         };
         let next_header = self
             .inner
@@ -436,6 +439,7 @@ impl Ipv6Ext {
         match next_header {
             TCP => cursor.parse_header::<TruncatedTcp, EmbeddedIpv6ExtNext>(),
             UDP => cursor.parse_header::<TruncatedUdp, EmbeddedIpv6ExtNext>(),
+            IPV6_ICMP => cursor.parse_header::<TruncatedIcmp6, EmbeddedIpv6ExtNext>(),
             AUTHENTICATION_HEADER => {
                 debug!("nested ip auth header");
                 cursor.parse_header::<IpAuth, EmbeddedIpv6ExtNext>()
@@ -498,6 +502,7 @@ impl From<Ipv6ExtNext> for Header {
 pub(crate) enum EmbeddedIpv6ExtNext {
     Tcp(TruncatedTcp),
     Udp(TruncatedUdp),
+    Icmp6(TruncatedIcmp6),
     IpAuth(IpAuth),
     Ipv6Ext(Ipv6Ext),
 }
@@ -506,6 +511,7 @@ impl_from_for_enum![
     EmbeddedIpv6ExtNext,
     Tcp(TruncatedTcp),
     Udp(TruncatedUdp),
+    Icmp6(TruncatedIcmp6),
     IpAuth(IpAuth),
     Ipv6Ext(Ipv6Ext)
 ];
@@ -515,6 +521,7 @@ impl From<EmbeddedIpv6Next> for EmbeddedHeader {
         match value {
             EmbeddedIpv6Next::Tcp(x) => EmbeddedHeader::Tcp(x),
             EmbeddedIpv6Next::Udp(x) => EmbeddedHeader::Udp(x),
+            EmbeddedIpv6Next::Icmp6(x) => EmbeddedHeader::Icmp6(x),
             EmbeddedIpv6Next::IpAuth(x) => EmbeddedHeader::IpAuth(x),
             EmbeddedIpv6Next::Ipv6Ext(x) => EmbeddedHeader::IpV6Ext(x),
         }
@@ -526,6 +533,7 @@ impl From<EmbeddedIpv6ExtNext> for EmbeddedHeader {
         match value {
             EmbeddedIpv6ExtNext::Tcp(x) => EmbeddedHeader::Tcp(x),
             EmbeddedIpv6ExtNext::Udp(x) => EmbeddedHeader::Udp(x),
+            EmbeddedIpv6ExtNext::Icmp6(x) => EmbeddedHeader::Icmp6(x),
             EmbeddedIpv6ExtNext::IpAuth(x) => EmbeddedHeader::IpAuth(x),
             EmbeddedIpv6ExtNext::Ipv6Ext(x) => EmbeddedHeader::IpV6Ext(x),
         }