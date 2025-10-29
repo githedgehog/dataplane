githedgehog/dataplane

commit 51f2e5668635d066c5c17eb28727a50bf999ef06

feat(nat): Change NatPort to allow (possibly-null) identifiers
NatPort has been used in NAT to represent ports that cannot be zero (the
value is invalid for TCP and UDP). Now that we work with ICMP as well,
and that we need to translate an identifier that can be null, we need to
rework the structure. Change it to an enum, so we can keep the validated
type for TCP and UDP. This will be used in follow-up commits to extend
support for ICMP in the NAT pipeline stage.

Signed-off-by: Quentin Monnet <qmo@qmon.net>

nat/src/port.rs
@@ -9,8 +9,8 @@ use std::num::NonZero;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, thiserror::Error)]
 pub enum NatPortError {
-    #[error("reserved port ({0})")]
-    ReservedPort(u16),
+    #[error("invalid port ({0})")]
+    InvalidPort(u16),
 }
 
 /// `NatPort` is a type to represent L4 ports usable in stateful NAT. In fact, it is just a wrapper
@@ -21,26 +21,40 @@ pub enum NatPortError {
 // memory from 3 to 2 bytes.
 #[cfg_attr(test, derive(bolero::TypeGenerator))]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct NatPort(NonZero<u16>);
+pub enum NatPort {
+    Port(NonZero<u16>),
+    Identifier(u16),
+}
 
 impl NatPort {
-    pub fn new_checked(port: u16) -> Result<NatPort, NatPortError> {
-        NonZero::new(port).map_or(Err(NatPortError::ReservedPort(port)), |port| {
-            Ok(NatPort(port))
+    #[must_use]
+    pub fn new_port(port: NonZero<u16>) -> NatPort {
+        NatPort::Port(port)
+    }
+
+    pub fn new_port_checked(port: u16) -> Result<NatPort, NatPortError> {
+        NonZero::new(port).map_or(Err(NatPortError::InvalidPort(port)), |port| {
+            Ok(NatPort::Port(port))
         })
     }
 
     #[must_use]
+    pub fn new_identifier(port: u16) -> NatPort {
+        NatPort::Identifier(port)
+    }
+
+    #[must_use]
     pub fn as_u16(self) -> u16 {
-        self.0.into()
+        match self {
+            NatPort::Port(port) => port.into(),
+            NatPort::Identifier(port) => port,
+        }
     }
 }
 
-impl TryFrom<TcpPort> for NatPort {
-    type Error = NatPortError;
-
-    fn try_from(port: TcpPort) -> Result<Self, Self::Error> {
-        Self::new_checked(port.as_u16())
+impl From<TcpPort> for NatPort {
+    fn from(port: TcpPort) -> Self {
+        Self::new_port(port.into())
     }
 }
 
@@ -52,11 +66,9 @@ impl TryFrom<NatPort> for TcpPort {
     }
 }
 
-impl TryFrom<UdpPort> for NatPort {
-    type Error = NatPortError;
-
-    fn try_from(port: UdpPort) -> Result<Self, Self::Error> {
-        Self::new_checked(port.as_u16())
+impl From<UdpPort> for NatPort {
+    fn from(port: UdpPort) -> Self {
+        Self::new_port(port.into())
     }
 }
 
@@ -68,8 +80,12 @@ impl TryFrom<NatPort> for UdpPort {
     }
 }
 
-impl From<NatPort> for NonZero<u16> {
-    fn from(port: NatPort) -> Self {
-        port.0
+impl TryFrom<NatPort> for NonZero<u16> {
+    type Error = NatPortError;
+
+    fn try_from(port: NatPort) -> Result<Self, Self::Error> {
+        port.as_u16()
+            .try_into()
+            .map_err(|_| NatPortError::InvalidPort(port.as_u16()))
     }
 }

nat/src/icmp_error_msg.rs
@@ -24,6 +24,8 @@ pub enum IcmpErrorMsgError {
     BadChecksumIcmp(ChecksumError<IcmpAnyChecksumErrorPlaceholder>),
     #[error("failed to validate ICMP inner IP checksum")]
     BadChecksumInnerIpv4(ChecksumError<Ipv4>),
+    #[error("invalid transport-layer port {0}")]
+    InvalidPort(u16),
     #[error("invalid IP version")]
     InvalidIpVersion,
     #[error("IP address {0} is not unicast")]
@@ -153,7 +155,11 @@ pub(crate) fn stateful_translate_icmp_inner<Buf: PacketBufferMut>(
 
     if let Some(target_src_port) = target_src_port {
         transport
-            .set_source(target_src_port.into())
+            .set_source(
+                target_src_port
+                    .try_into()
+                    .map_err(|_| IcmpErrorMsgError::InvalidPort(target_src_port.as_u16()))?,
+            )
             .unwrap_or_else(|_| unreachable!());
         // We don't know whether the header and payload are full: the easiest way to deal with
         // transport checksum update is to do an unconditional, incremental update here. Note
@@ -164,7 +170,11 @@ pub(crate) fn stateful_translate_icmp_inner<Buf: PacketBufferMut>(
     }
     if let Some(target_dst_port) = target_dst_port {
         transport
-            .set_destination(target_dst_port.into())
+            .set_destination(
+                target_dst_port
+                    .try_into()
+                    .map_err(|_| IcmpErrorMsgError::InvalidPort(target_dst_port.as_u16()))?,
+            )
             .unwrap_or_else(|_| unreachable!());
         if let Some(current_checksum) = transport.checksum() {
             transport.update_checksum(current_checksum, old_dst_port, target_dst_port.as_u16());
@@ -415,8 +425,12 @@ mod bolero_tests {
                     stateful_translate_icmp_inner(&mut icmp_error_msg_clone, &tr_data).unwrap();
 
                     let (translation_src_port, translation_dst_port) = (
-                        tr_data.src_port.map(NonZero::<u16>::from),
-                        tr_data.dst_port.map(NonZero::<u16>::from),
+                        tr_data
+                            .src_port
+                            .map(|p| NonZero::<u16>::try_from(p.as_u16()).unwrap()),
+                        tr_data
+                            .dst_port
+                            .map(|p| NonZero::<u16>::try_from(p.as_u16()).unwrap()),
                     );
                     let new_outer_addresses = get_outer_addresses(&icmp_error_msg_clone).unwrap();
                     let new_inner_addresses = get_inner_addresses(&icmp_error_msg_clone).unwrap();

nat/src/stateful/apalloc/mod.rs
@@ -66,7 +66,6 @@
 
 use super::allocator::{AllocationResult, AllocatorError};
 use super::{NatAllocator, NatIp};
-use crate::port::NatPort;
 use crate::stateful::apalloc::alloc::IpAllocator;
 pub use crate::stateful::apalloc::natip_with_bitmap::NatIpWithBitmap;
 use net::ip::NextHeader;
@@ -343,15 +342,11 @@ impl NatDefaultAllocator {
     ) -> Result<AllocationMapping<I>, AllocatorError> {
         let reverse_src_mapping = match reverse_pool_src_opt {
             Some(pool_src) => {
-                let reverse_src_port_number = match flow_key.data().proto_key_info() {
+                let reservation_src_port_number = match flow_key.data().proto_key_info() {
                     IpProtoKey::Tcp(tcp) => tcp.dst_port.into(),
                     IpProtoKey::Udp(udp) => udp.dst_port.into(),
                     IpProtoKey::Icmp(_) => return Err(AllocatorError::PortNotFound),
                 };
-                let reserve_src_port_number = NatPort::new_checked(reverse_src_port_number)
-                    .map_err(|_| {
-                        AllocatorError::InternalIssue("Invalid source port number".to_string())
-                    })?;
 
                 Some(pool_src.reserve(
                     NatIp::try_from_addr(*flow_key.data().dst_ip()).map_err(|()| {
@@ -359,7 +354,7 @@ impl NatDefaultAllocator {
                             "Failed to convert IP address to Ipv4Addr".to_string(),
                         )
                     })?,
-                    reserve_src_port_number,
+                    reservation_src_port_number,
                 )?)
             }
             None => None,
@@ -367,15 +362,11 @@ impl NatDefaultAllocator {
 
         let reverse_dst_mapping = match reverse_pool_dst_opt {
             Some(pool_dst) => {
-                let reverse_dst_port_number = match flow_key.data().proto_key_info() {
+                let reservation_dst_port_number = match flow_key.data().proto_key_info() {
                     IpProtoKey::Tcp(tcp) => tcp.src_port.into(),
                     IpProtoKey::Udp(udp) => udp.src_port.into(),
                     IpProtoKey::Icmp(_) => return Err(AllocatorError::PortNotFound),
                 };
-                let reserve_dst_port_number = NatPort::new_checked(reverse_dst_port_number)
-                    .map_err(|_| {
-                        AllocatorError::InternalIssue("Invalid destination port number".to_string())
-                    })?;
 
                 Some(pool_dst.reserve(
                     NatIp::try_from_addr(*flow_key.data().src_ip()).map_err(|()| {
@@ -383,7 +374,7 @@ impl NatDefaultAllocator {
                             "Failed to convert IP address to Ipv4Addr".to_string(),
                         )
                     })?,
-                    reserve_dst_port_number,
+                    reservation_dst_port_number,
                 )?)
             }
             None => None,

nat/src/stateful/apalloc/port_alloc.rs
@@ -377,7 +377,7 @@ impl<I: NatIpWithBitmap> AllocatedPortBlock<I> {
             .allocate_port_from_bitmap()
             .map_err(|()| AllocatorError::NoFreePort(self.base_port_idx))?;
 
-        NatPort::new_checked(self.base_port_idx + bitmap_offset)
+        NatPort::new_port_checked(self.base_port_idx + bitmap_offset)
             .map_err(AllocatorError::PortAllocationFailed)
             .map(|port| AllocatedPort::new(port, self.clone()))
     }

nat/src/stateful/mod.rs
@@ -205,7 +205,11 @@ impl StatefulNat {
             headers
                 .try_transport_mut()
                 .ok_or(StatefulNatError::BadTransportHeader)?
-                .try_set_source(target_src_port.into())
+                .try_set_source(
+                    target_src_port
+                        .try_into()
+                        .map_err(|_| StatefulNatError::InvalidPort(target_src_port.as_u16()))?,
+                )
                 .map_err(|_| StatefulNatError::BadTransportHeader)?;
         }
 
@@ -217,7 +221,11 @@ impl StatefulNat {
             headers
                 .try_transport_mut()
                 .ok_or(StatefulNatError::BadTransportHeader)?
-                .try_set_destination(target_dst_port.into())
+                .try_set_destination(
+                    target_dst_port
+                        .try_into()
+                        .map_err(|_| StatefulNatError::InvalidPort(target_dst_port.as_u16()))?,
+                )
                 .map_err(|_| StatefulNatError::BadTransportHeader)?;
         }
         Ok(())
@@ -292,13 +300,21 @@ impl StatefulNat {
         // ... but adjust ports as necessary (use allocated ports for the reverse session)
         if let Some(src_port) = allocated_src_port_to_use {
             reverse_proto_key
-                .try_set_src_port(src_port.into())
-                .map_err(|_| StatefulNatError::InvalidPort(src_port.as_u16()))?;
+                .try_set_src_port(
+                    src_port
+                        .try_into()
+                        .map_err(|_| StatefulNatError::InvalidPort(src_port.as_u16()))?,
+                )
+                .map_err(|_| StatefulNatError::BadTransportHeader)?;
         }
         if let Some(dst_port) = allocated_dst_port_to_use {
             reverse_proto_key
-                .try_set_dst_port(dst_port.into())
-                .map_err(|_| StatefulNatError::InvalidPort(dst_port.as_u16()))?;
+                .try_set_dst_port(
+                    dst_port
+                        .try_into()
+                        .map_err(|_| StatefulNatError::InvalidPort(dst_port.as_u16()))?,
+                )
+                .map_err(|_| StatefulNatError::BadTransportHeader)?;
         }
 
         Ok(FlowKey::uni(
@@ -554,15 +570,19 @@ mod tests {
                 .set_destination(TcpPort::try_from(443).expect("Invalid port"))
                 .clone(),
         );
-        let target_port = NatPort::new_checked(1234).expect("Invalid port");
+        let target_port = NatPort::new_port_checked(1234).expect("Invalid port");
 
-        transport.try_set_source(target_port.into()).unwrap();
+        transport
+            .try_set_source(target_port.try_into().unwrap())
+            .unwrap();
         let Transport::Tcp(ref mut tcp) = transport else {
             unreachable!()
         };
         assert_eq!(tcp.source(), TcpPort::try_from(1234).unwrap());
 
-        transport.try_set_destination(target_port.into()).unwrap();
+        transport
+            .try_set_destination(target_port.try_into().unwrap())
+            .unwrap();
         let Transport::Tcp(ref mut tcp) = transport else {
             unreachable!()
         };
@@ -577,15 +597,19 @@ mod tests {
                 .set_destination(UdpPort::try_from(443).expect("Invalid port"))
                 .clone(),
         );
-        let target_port = NatPort::new_checked(1234).expect("Invalid port");
+        let target_port = NatPort::new_port_checked(1234).expect("Invalid port");
 
-        transport.try_set_source(target_port.into()).unwrap();
+        transport
+            .try_set_source(target_port.try_into().unwrap())
+            .unwrap();
         let Transport::Udp(ref mut udp) = transport else {
             unreachable!()
         };
         assert_eq!(udp.source(), UdpPort::try_from(1234).unwrap());
 
-        transport.try_set_destination(target_port.into()).unwrap();
+        transport
+            .try_set_destination(target_port.try_into().unwrap())
+            .unwrap();
         let Transport::Udp(ref mut udp) = transport else {
             unreachable!()
         };

commit ab84749ab3bc405ea58b22f61ad5c61752ba15be

fix(nat): Fix comment for enum NatPort
The comment is outdated, let's correct it.

Fixes: 51f2e5668635 ("feat(nat): Change NatPort to allow (possibly-null) identifiers")
Signed-off-by: Quentin Monnet <qmo@qmon.net>

nat/src/port.rs
@@ -13,12 +13,8 @@ pub enum NatPortError {
     InvalidPort(u16),
 }
 
-/// `NatPort` is a type to represent L4 ports usable in stateful NAT. In fact, it is just a wrapper
-/// around a non-zero `u16`.
-//
-// TODO: We may change this in the future. One suggestion was to use a regular u16, and encode the
-// "None" case with value 0 wherever we need to deal with Option(NatPort), to reduce the size in
-// memory from 3 to 2 bytes.
+/// `NatPort` is a type to represent L4 ports (TCP/UDP) or identifier (ICMP v4, v6) usable in
+/// stateful NAT.
 #[cfg_attr(test, derive(bolero::TypeGenerator))]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum NatPort {