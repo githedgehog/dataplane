ada10fe209f36699d8f0408b37c6ffbf2b4ebbe9
Fredi Raspall <fredi@githedgehog.com>
2025-10-30 22:25:39 +0100

    feat(pkt-io): add IO manager
    The IO manager is the entity responsible to communicate the pipeline
    with the kernel. It does so by:
      - capturing packets punted by the pipeline and writing them on
        the correct tap device.
      - capturing packets sent by the taps and queueing them on the
        pipeline injection queue(s).
    
    Note:
      - This function is the one that handles all of the traffic to be
        locally consumed by the gateway like ARP, BGP, etc.
      - It is implemented as a separate thread with its own tokio runtime.
      - It expects packets to be readily annotated to deliver them over
        the right tap device.
      - it exposes a control channel to enable/disable rx/tx on the tap
        devices representing ports.
      - When packets locally created need to be sent, the IO manager
        should queue them in a pkt-io injection queue. However, there
        exist N pipelines, one per worker thread/lcore. Also, a single
        pipeline could have multiple injection points. We solve this
        by:
           - indicating the queue that should be used when starting
             the IO manager.
           - since no criteria exists to select which of the workers
             should be transmitting an outgoing packet, we will let
    	 the pkt-io stages of the N pipelines share the same
    	 injection queue for the purposes of transmitting
    	 local traffic. As for reception, each pipeline could have
    	 its own separate queue. However, we'll do the same
    	 for simplicity in this first implementation.
    
    This implementation requires some cleanup and may be simplified.
    For instance, tasks are stopped via abort(). Cancellation tokens
    could be used instead.

--- pkt-io/src/lib.rs
+++ pkt-io/src/lib.rs
@@ -4,12 +4,19 @@
 //! Local packet I/O for gateway
 
 #![deny(clippy::all)]
+#![allow(clippy::collapsible_if)]
+
+mod ctl;
+mod io;
 
 mod nf;
 mod portmap;
 mod portmapper;
 mod tests;
 
+// re-exports
+pub use ctl::IoManagerCtl;
+pub use io::start_io;
 pub use nf::PktIo;
 pub use nf::PktQueue;
 pub use portmap::{PortMap, PortMapReader, PortMapReaderFactory, PortMapWriter};
