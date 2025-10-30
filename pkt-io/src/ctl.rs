b0eac7af9cd7c80ccfa9b1b36c905e666c2cc618
Fredi Raspall <fredi@githedgehog.com>
2025-10-30 22:25:39 +0100

    feat(pkt-io): add IO manager
    The IO manager is the entity responsible to communicate the pipeline
    with the kernel. It does so by:
      - capturing packets punted by the pipeline and writing them on
        the correct tap device.
      - capturing packets sent by the taps and queueing them on the
        pipeline injection queue(s).
    
    Note:
      - This function is the one that handles all of the traffic to be
        locally consumed by the gateway like ARP, BGP, etc.
      - It is implemented as a separate thread with its own tokio runtime.
      - It expects packets to be readily annotated to deliver them over
        the right tap device.
      - it exposes a control channel to enable/disable rx/tx on the tap
        devices representing ports.
      - When packets locally created need to be sent, the IO manager
        should queue them in a pkt-io injection queue. However, there
        exist N pipelines, one per worker thread/lcore. Also, a single
        pipeline could have multiple injection points. We solve this
        by:
           - indicating the queue that should be used when starting
             the IO manager.
           - since no criteria exists to select which of the workers
             should be transmitting an outgoing packet, we will let
    	 the pkt-io stages of the N pipelines share the same
    	 injection queue for the purposes of transmitting
    	 local traffic. As for reception, each pipeline could have
    	 its own separate queue. However, we'll do the same
    	 for simplicity in this first implementation.
    
    This implementation requires some cleanup and may be simplified.
    For instance, tasks are stopped via abort(). Cancellation tokens
    could be used instead.
    
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

---
+++ pkt-io/src/ctl.rs
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Open Network Fabric Authors
+
+//! Pkt IO controller management.
+//! This module defines the public interface to control the IO packet manager.
+//! The control is exerted over a channel via which the IO packet manager is told
+//! the taps it should be moving packets for.
+
+use net::interface::InterfaceName;
+use std::collections::HashSet;
+use std::fmt::Display;
+use tokio::sync::mpsc::Sender;
+use tokio::sync::mpsc::error::SendError;
+
+#[derive(Debug, Clone)]
+pub struct TapSet(pub(crate) HashSet<InterfaceName>);
+impl TapSet {
+    #[must_use]
+    pub(crate) fn new() -> Self {
+        Self(HashSet::new())
+    }
+    fn add(&mut self, tapname: InterfaceName) {
+        self.0.insert(tapname);
+    }
+    fn del(&mut self, tapname: &InterfaceName) {
+        self.0.remove(tapname);
+    }
+    pub(crate) fn iter(&self) -> impl Iterator<Item = &InterfaceName> {
+        self.0.iter()
+    }
+    pub(crate) fn contains(&self, tapname: &InterfaceName) -> bool {
+        self.0.contains(tapname)
+    }
+    pub(crate) fn clear(&mut self) {
+        self.0.clear();
+    }
+}
+
+/// The internal messages to control an `IoManager`.
+pub enum IoManagerMsg {
+    Enable(TapSet),
+    Stop,
+}
+
+/// The object used to drive an [`IoManager`].
+/// This object:
+///    - can keep a set of tap specifications internally.
+///    - calling commit on it issues a config request.
+///    - can be cloned.
+pub struct IoManagerCtl {
+    tapset: TapSet,
+    sender: Sender<IoManagerMsg>,
+}
+impl Clone for IoManagerCtl {
+    fn clone(&self) -> Self {
+        Self {
+            tapset: TapSet::new(), // we don't clone the internal set for convenience
+            sender: self.sender.clone(),
+        }
+    }
+}
+impl IoManagerCtl {
+    #[must_use]
+    pub(crate) fn new(sender: Sender<IoManagerMsg>) -> Self {
+        Self {
+            tapset: TapSet::new(),
+            sender,
+        }
+    }
+    pub fn add(&mut self, tapname: InterfaceName) {
+        self.tapset.add(tapname);
+    }
+    pub fn del(&mut self, tapname: &InterfaceName) {
+        self.tapset.del(tapname);
+    }
+    pub fn clear(&mut self) {
+        self.tapset.clear();
+    }
+    pub async fn commit(&mut self) -> Result<(), SendError<IoManagerMsg>> {
+        self.sender
+            .send(IoManagerMsg::Enable(self.tapset.clone()))
+            .await
+    }
+    pub async fn stop(&mut self) -> Result<(), SendError<IoManagerMsg>> {
+        self.sender.send(IoManagerMsg::Stop).await
+    }
+}
+
+impl Display for TapSet {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "━━━━ Interfaces to be active ━━━━")?;
+        for spec in self.0.iter() {
+            writeln!(f, "{spec}")?;
+        }
+        Ok(())
+    }
+}
