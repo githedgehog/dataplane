6adcb084b6ca15117a2b53936d53e131bc7f8608
Fredi Raspall <fredi@githedgehog.com>
2025-11-02 12:54:16 +0100

    feat(mgmt,dataplane): channelize/clean mgmt args
    Every now and then, we need to pass new args to mgmt module.
    This is painful and requires modifying multiple functions/methods
    every time. Define MgmtParams and ConfigProcessorParams and use
    them to pass all the required parameters from main.
    
    TODO(fredi): The mgmt module requires a significant tidy-up in
    terms of organization, type visibility and the like.
    
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

--- mgmt/src/lib.rs
+++ mgmt/src/lib.rs
@@ -4,16 +4,19 @@
 //! Dataplane management module
 
 /* gRPC entry point */
-pub mod grpc;
+mod grpc;
 
 /* Configuration processor */
-pub mod processor;
+mod processor;
 
 /* VPC manager */
 pub mod vpc_manager;
 
 #[cfg(test)]
 mod tests;
+
+pub use processor::launch::{MgmtParams, start_mgmt};
+pub use processor::proc::ConfigProcessorParams;
 
 use tracectl::trace_target;
 trace_target!("mgmt", LevelFilter::DEBUG, &["management"]);
