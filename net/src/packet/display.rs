ddd0abe82214189a6a5a154e2af7e4e0d4a07928
Daniel Noland <daniel@githedgehog.com>
2025-11-05 21:04:25 +0000

    fixup: feat(net): display metadata flags
    mark fmt as cold

--- net/src/packet/display.rs
+++ net/src/packet/display.rs
@@ -271,6 +271,7 @@ fn fmt_metadata_flags(meta: &PacketMeta, f: &mut Formatter<'_>) -> std::fmt::Res
     writeln!(f)
 }
 impl Display for PacketMeta {
+    #[cold]
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         writeln!(f, "  metadata:")?;
         write!(f, "   ")?;
