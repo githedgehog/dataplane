githedgehog/dataplane

commit d51311bd12ceedce30a63460af39338013fd3458

feat(nat): Add logic for processing ICMP in stateful NAT
We've designed the stateful NAT's allocator with TCP and UDP in mind,
with two ports. This commit adds support for translating ICMP, too.

Ideally, we should have a dedicated structure adapted for ICMP, reusing
some of the components of the TCP/UDP structures. We're not doing that:
we simply reuse the existing allocator, using just one of the two fields
to hold the identifier. It is expected that this code will be refactored
in the near future.

Signed-off-by: Quentin Monnet <qmo@qmon.net>

nat/src/stateful/mod.rs
@@ -20,7 +20,9 @@ pub use allocator_writer::NatAllocatorWriter;
 use concurrency::sync::Arc;
 use flow_info::{ExtractRef, FlowInfo};
 use net::buffer::PacketBufferMut;
-use net::headers::{Net, TryHeaders, TryHeadersMut, TryInnerIp, TryIp, TryIpMut, TryTransportMut};
+use net::headers::{
+    Net, Transport, TryHeaders, TryHeadersMut, TryInnerIp, TryIp, TryIpMut, TryTransportMut,
+};
 use net::packet::{DoneReason, Packet, VpcDiscriminant};
 use pipeline::NetworkFunction;
 use pkt_meta::flow_table::flow_key::{IcmpProtoKey, Uni};
@@ -54,6 +56,8 @@ pub enum StatefulNatError {
     NoSession,
     #[error("failed to translate ICMP inner packet: {0}")]
     IcmpErrorMsg(IcmpErrorMsgError),
+    #[error("unexpected IP protocol key variant")]
+    UnexpectedKeyVariant,
 }
 
 #[derive(Debug)]
@@ -202,15 +206,25 @@ impl StatefulNat {
             )
             .map_err(|_| StatefulNatError::InvalidIpVersion)?;
 
-            headers
+            let transport = headers
                 .try_transport_mut()
-                .ok_or(StatefulNatError::BadTransportHeader)?
-                .try_set_source(
-                    target_src_port
-                        .try_into()
-                        .map_err(|_| StatefulNatError::InvalidPort(target_src_port.as_u16()))?,
-                )
-                .map_err(|_| StatefulNatError::BadTransportHeader)?;
+                .ok_or(StatefulNatError::BadTransportHeader)?;
+            match transport {
+                Transport::Tcp(_) | Transport::Udp(_) => {
+                    transport
+                        .try_set_source(
+                            target_src_port.try_into().map_err(|_| {
+                                StatefulNatError::InvalidPort(target_src_port.as_u16())
+                            })?,
+                        )
+                        .map_err(|_| StatefulNatError::BadTransportHeader)?;
+                }
+                Transport::Icmp4(_) | Transport::Icmp6(_) => {
+                    transport
+                        .try_set_identifier(target_src_port.as_u16())
+                        .map_err(|_| StatefulNatError::BadTransportHeader)?;
+                }
+            }
         }
 
         let net = headers.try_ip_mut().ok_or(StatefulNatError::BadIpHeader)?;
@@ -227,6 +241,9 @@ impl StatefulNat {
                         .map_err(|_| StatefulNatError::InvalidPort(target_dst_port.as_u16()))?,
                 )
                 .map_err(|_| StatefulNatError::BadTransportHeader)?;
+
+            // No need to set the identifier for ICMP Echo messages, we already did it above using
+            // target_src_port.
         }
         Ok(())
     }
@@ -299,22 +316,45 @@ impl StatefulNat {
         let mut reverse_proto_key = flow_key.data().proto_key_info().reverse();
         // ... but adjust ports as necessary (use allocated ports for the reverse session)
         if let Some(src_port) = allocated_src_port_to_use {
-            reverse_proto_key
-                .try_set_src_port(
-                    src_port
-                        .try_into()
-                        .map_err(|_| StatefulNatError::InvalidPort(src_port.as_u16()))?,
-                )
-                .map_err(|_| StatefulNatError::BadTransportHeader)?;
+            match reverse_proto_key {
+                IpProtoKey::Tcp(_) | IpProtoKey::Udp(_) => {
+                    reverse_proto_key
+                        .try_set_src_port(
+                            src_port
+                                .try_into()
+                                .map_err(|_| StatefulNatError::InvalidPort(src_port.as_u16()))?,
+                        )
+                        .map_err(|_| StatefulNatError::BadTransportHeader)?;
+                }
+                IpProtoKey::Icmp(IcmpProtoKey::QueryMsgData(_)) => {
+                    // Nothing to do here: we reverse the identifier using "dst_port" below, and one
+                    // identifier is enough for ICMP.
+                }
+                IpProtoKey::Icmp(_) => {
+                    return Err(StatefulNatError::UnexpectedKeyVariant);
+                }
+            }
         }
         if let Some(dst_port) = allocated_dst_port_to_use {
-            reverse_proto_key
-                .try_set_dst_port(
-                    dst_port
-                        .try_into()
-                        .map_err(|_| StatefulNatError::InvalidPort(dst_port.as_u16()))?,
-                )
-                .map_err(|_| StatefulNatError::BadTransportHeader)?;
+            match reverse_proto_key {
+                IpProtoKey::Tcp(_) | IpProtoKey::Udp(_) => {
+                    reverse_proto_key
+                        .try_set_dst_port(
+                            dst_port
+                                .try_into()
+                                .map_err(|_| StatefulNatError::InvalidPort(dst_port.as_u16()))?,
+                        )
+                        .map_err(|_| StatefulNatError::BadTransportHeader)?;
+                }
+                IpProtoKey::Icmp(IcmpProtoKey::QueryMsgData(_)) => {
+                    reverse_proto_key
+                        .try_set_identifier(dst_port.as_u16())
+                        .map_err(|_| StatefulNatError::BadTransportHeader)?;
+                }
+                IpProtoKey::Icmp(_) => {
+                    return Err(StatefulNatError::UnexpectedKeyVariant);
+                }
+            }
         }
 
         Ok(FlowKey::uni(

nat/src/stateful/apalloc/mod.rs
@@ -66,12 +66,14 @@
 
 use super::allocator::{AllocationResult, AllocatorError};
 use super::{NatAllocator, NatIp};
+use crate::NatPort;
 use crate::stateful::apalloc::alloc::IpAllocator;
 pub use crate::stateful::apalloc::natip_with_bitmap::NatIpWithBitmap;
 use net::ip::NextHeader;
 use net::packet::VpcDiscriminant;
 use pkt_meta::flow_table::FlowKey;
 use pkt_meta::flow_table::IpProtoKey;
+use pkt_meta::flow_table::flow_key::IcmpProtoKey;
 use std::collections::BTreeMap;
 use std::net::{Ipv4Addr, Ipv6Addr};
 
@@ -322,6 +324,17 @@ impl NatDefaultAllocator {
         pool_src_opt: Option<&alloc::IpAllocator<I>>,
         pool_dst_opt: Option<&alloc::IpAllocator<I>>,
     ) -> Result<AllocationMapping<I>, AllocatorError> {
+        // Allocate IP and ports for source and destination NAT.
+        //
+        // In the case of ICMP Query messages, use dst_mapping to hold an allocated identifier
+        // instead of ports.
+        //
+        // FIXME: In the case of ICMP, we're only interested in the IP allocated for src_mapping,
+        // not the port. We need to translate a single value (the identifier), and we're using the
+        // dst_mapping to hold it. However, both source and destination IP need to come with a
+        // "port" with the current architecture of the allocator, which means we also allocate a
+        // port/identifier value for the src_mapping, even though we'll never use it. (This does not
+        // apply to TCP or UDP, for which we need and use both ports).
         let src_mapping = match pool_src_opt {
             Some(pool_src) => Some(pool_src.allocate()?),
             None => None,
@@ -345,7 +358,13 @@ impl NatDefaultAllocator {
                 let reservation_src_port_number = match flow_key.data().proto_key_info() {
                     IpProtoKey::Tcp(tcp) => tcp.dst_port.into(),
                     IpProtoKey::Udp(udp) => udp.dst_port.into(),
-                    IpProtoKey::Icmp(_) => return Err(AllocatorError::PortNotFound),
+                    // FIXME: We're doing a useless port reservation here, but without reserving a
+                    // "port" (or an ID for ICMP) we can't reserve an IP, given the current
+                    // architecture of the allocator. The ID will be overwritten by the ID for the
+                    // destination mapping. Note: this does not mean we're exhausting allocatable
+                    // identifiers sooner, because we allocate from a ports/identifier pool we don't
+                    // need.
+                    IpProtoKey::Icmp(icmp) => NatPort::Identifier(Self::get_icmp_query_id(icmp)?),
                 };
 
                 Some(pool_src.reserve(
@@ -365,7 +384,7 @@ impl NatDefaultAllocator {
                 let reservation_dst_port_number = match flow_key.data().proto_key_info() {
                     IpProtoKey::Tcp(tcp) => tcp.src_port.into(),
                     IpProtoKey::Udp(udp) => udp.src_port.into(),
-                    IpProtoKey::Icmp(_) => return Err(AllocatorError::PortNotFound),
+                    IpProtoKey::Icmp(icmp) => NatPort::Identifier(Self::get_icmp_query_id(icmp)?),
                 };
 
                 Some(pool_dst.reserve(
@@ -381,6 +400,17 @@ impl NatDefaultAllocator {
         };
 
         Ok((reverse_src_mapping, reverse_dst_mapping))
+    }
+
+    fn get_icmp_query_id(key: &IcmpProtoKey) -> Result<u16, AllocatorError> {
+        match key {
+            IcmpProtoKey::QueryMsgData(id) => Ok(*id),
+            IcmpProtoKey::ErrorMsgData(_) => Err(AllocatorError::InternalIssue(
+                "ICMP Error message should have been processed without allocating new mappings"
+                    .to_string(),
+            )),
+            IcmpProtoKey::Unsupported => Err(AllocatorError::UnsupportedIcmpCategory),
+        }
     }
 }
 

net/src/headers/mod.rs
@@ -145,6 +145,8 @@ pub enum NetExt {
 pub enum TransportError {
     #[error("transport protocol does not use ports")]
     UnsupportedPort,
+    #[error("transport protocol does not use identifier")]
+    UnsupportedIdentifier,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -269,6 +271,18 @@ impl Transport {
             }
         }
         Ok(())
+    }
+
+    pub fn try_set_identifier(&mut self, identifier: u16) -> Result<(), TransportError> {
+        match self {
+            Transport::Icmp4(icmp4) => icmp4
+                .try_set_identifier(identifier)
+                .map_err(|_| TransportError::UnsupportedIdentifier),
+            Transport::Icmp6(icmp6) => icmp6
+                .try_set_identifier(identifier)
+                .map_err(|_| TransportError::UnsupportedIdentifier),
+            _ => Err(TransportError::UnsupportedIdentifier),
+        }
     }
 }
 

net/src/icmp6/mod.rs
@@ -20,6 +20,14 @@ use std::num::NonZero;
 #[cfg(any(test, feature = "bolero"))]
 pub use contract::*;
 
+/// Errors which may occur when using ICMP v6 methods
+#[derive(Debug, thiserror::Error)]
+pub enum Icmp6Error {
+    /// The ICMP type does not allow setting an identifier.
+    #[error("Invalid ICMP type")]
+    InvalidIcmpType,
+}
+
 /// An `Icmp6` header.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Icmp6(pub(crate) Icmpv6Header);
@@ -63,6 +71,21 @@ impl Icmp6 {
         match self.icmp_type() {
             Icmpv6Type::EchoRequest(msg) | Icmpv6Type::EchoReply(msg) => Some(msg.id),
             _ => None,
+        }
+    }
+
+    /// Set the identifier field value
+    ///
+    /// # Errors
+    ///
+    /// This method returns [`Icmp6Error::InvalidIcmpType`] if the ICMP type does not allow setting an identifier.
+    pub fn try_set_identifier(&mut self, id: u16) -> Result<(), Icmp6Error> {
+        match self.icmp_type_mut() {
+            Icmpv6Type::EchoRequest(msg) | Icmpv6Type::EchoReply(msg) => {
+                msg.id = id;
+                Ok(())
+            }
+            _ => Err(Icmp6Error::InvalidIcmpType),
         }
     }
 

nat/src/port.rs
@@ -89,3 +89,9 @@ impl TryFrom<NatPort> for NonZero<u16> {
             .map_err(|_| NatPortError::InvalidPort(port.as_u16()))
     }
 }
+
+impl From<NatPort> for u16 {
+    fn from(port: NatPort) -> Self {
+        port.as_u16()
+    }
+}

nat/src/stateful/allocator.rs
@@ -21,6 +21,8 @@ pub enum AllocatorError {
     PortAllocationFailed(NatPortError),
     #[error("unsupported protocol: {0:?}")]
     UnsupportedProtocol(NextHeader),
+    #[error("unsupported ICMP message category")]
+    UnsupportedIcmpCategory,
     #[error("no port present for flow: NAT currently unsupported")]
     PortNotFound,
     #[error("missing VPC discriminant")]

pkt-meta/src/flow_table/flow_key.rs
@@ -221,8 +221,10 @@ impl IcmpProtoKey {
 
 #[derive(Debug, Clone, Eq, PartialEq, PartialOrd, Ord, thiserror::Error)]
 pub enum IpProtoKeyError {
-    #[error("Type does not use ports")]
+    #[error("Variant does not use ports (e.g. ICMP)")]
     NoPortsForType,
+    #[error("Variant, or variant's value, does not use identifier (e.g. TCP, ICMP Error message)")]
+    NoIdentifierForType,
 }
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
@@ -270,6 +272,20 @@ impl IpProtoKey {
             IpProtoKey::Icmp(_) => return Err(IpProtoKeyError::NoPortsForType),
         }
         Ok(())
+    }
+
+    /// Sets the ICMP Query identifier of the flow key, if possible
+    ///
+    /// # Errors
+    ///
+    /// Returns [`IpProtoKeyError::NoIdentifierForType`] if the [`IpProtoKey`] enum value does not
+    /// have an identifier.
+    pub fn try_set_identifier(&mut self, identifier: u16) -> Result<(), IpProtoKeyError> {
+        if let IpProtoKey::Icmp(IcmpProtoKey::QueryMsgData(_)) = self {
+            *self = IpProtoKey::Icmp(IcmpProtoKey::QueryMsgData(identifier));
+            return Ok(());
+        }
+        Err(IpProtoKeyError::NoIdentifierForType)
     }
 }
 

commit 5b3ddb345112117850cd9195b37f7b8b607f4338

fix(nat): Use dest port for ICMP identifier if source port is empty
Our support for ICMP in stateful NAT is a bit hacky, and at the moment
we reuse the "source port" field (for TCP/UDP) in order to store the
ICMP Echo messages identifier to translate in the flow table. This works
well enough, except that for return packets, we're not necessarily
doing source NAT, and the source port may be empty as a result. In that
case, use the destination port instead. Make sure to only do so when we
don't have a source port, to avoid conflicting values for the identifier
(the allocator currently allocates distinct values for source and
destination ports; it needs some rework to better support ICMP, but this
will be for future work).

With this change, stateful NAT on the return path in case of
unidirectional NAT should work. Add a test to ensure that it does.

Fixes: d51311bd12ce ("feat(nat): Add logic for processing ICMP in stateful NAT")
Signed-off-by: Quentin Monnet <qmo@qmon.net>

nat/src/stateful/mod.rs
@@ -284,9 +284,23 @@ impl StatefulNat {
                     );
                 }
                 Transport::Icmp4(_) | Transport::Icmp6(_) => {
-                    // No need to set the identifier for ICMP Echo messages, we already did it above
-                    // using target_src_port.
-                    debug!("{nfi}: Destination-NAT translated {old_dst_ip} -> {target_dst_ip}",);
+                    let old_identifier = transport.identifier();
+                    let new_identifier;
+                    // We may not need to set the identifier for ICMP Echo messages, as we may have
+                    // done it above using target_src_port. But if target_src_port is None, we need
+                    // to set it here using target_dst_port.
+                    if let Some(src_port) = target_src_port {
+                        new_identifier = src_port.as_u16();
+                    } else {
+                        // We haven't set the identifier yet.
+                        new_identifier = target_dst_port.as_u16();
+                        transport
+                            .try_set_identifier(new_identifier)
+                            .map_err(|_| StatefulNatError::BadTransportHeader)?;
+                    }
+                    debug!(
+                        "{nfi}: Destination-NAT translated {old_dst_ip}:{old_identifier:?} -> {target_dst_ip}:{new_identifier:?}",
+                    );
                 }
             }
         }
@@ -372,8 +386,13 @@ impl StatefulNat {
                         .map_err(|_| StatefulNatError::BadTransportHeader)?;
                 }
                 IpProtoKey::Icmp(IcmpProtoKey::QueryMsgData(_)) => {
-                    // Nothing to do here: we reverse the identifier using "dst_port" below, and one
-                    // identifier is enough for ICMP.
+                    // For ICMP, we only need to set the identifier once. Use the "dst_port" below if
+                    // available, otherwise, use the "src_port" here.
+                    if allocated_dst_port_to_use.is_none() {
+                        reverse_proto_key
+                            .try_set_identifier(src_port.as_u16())
+                            .map_err(|_| StatefulNatError::BadTransportHeader)?;
+                    }
                 }
                 IpProtoKey::Icmp(_) => {
                     return Err(StatefulNatError::UnexpectedKeyVariant);

nat/src/stateful/test.rs
@@ -836,6 +836,85 @@ mod tests {
         assert_eq!(done_reason, None);
     }
 
+    #[test]
+    #[traced_test]
+    fn test_icmp_echo_unidirectional_nat() {
+        let mut config = build_sample_config(build_overlay_2vpcs_unidirectional_nat());
+        config.validate().unwrap();
+
+        // Check that we can validate the allocator
+        let (mut nat, mut allocator) = StatefulNat::new_with_defaults();
+        allocator
+            .update_allocator(&config.external.overlay.vpc_table)
+            .unwrap();
+
+        // No NAT
+        let (orig_src, orig_dst, orig_identifier) = (addr_v4("8.8.8.8"), addr_v4("9.9.9.9"), 1337);
+        let (output_src, output_dst, output_identifier, done_reason) = check_packet_icmp_echo(
+            &mut nat,
+            vni(100),
+            vni(200),
+            orig_src,
+            orig_dst,
+            IcmpEchoDirection::Request,
+            orig_identifier,
+        );
+        assert_eq!(output_src, orig_src);
+        assert_eq!(output_dst, orig_dst);
+        assert_eq!(output_identifier, orig_identifier);
+        assert_eq!(done_reason, Some(DoneReason::Filtered));
+
+        // NAT: expose121 <-> expose211
+        let (orig_src, orig_dst, orig_identifier) = (addr_v4("1.1.2.3"), addr_v4("5.0.0.5"), 1337);
+        let (target_src, target_dst) = (addr_v4("2.2.0.0"), addr_v4("5.0.0.5"));
+        let (output_src, output_dst, output_identifier_1, done_reason) = check_packet_icmp_echo(
+            &mut nat,
+            vni(100),
+            vni(200),
+            orig_src,
+            orig_dst,
+            IcmpEchoDirection::Request,
+            orig_identifier,
+        );
+        assert_eq!(output_src, target_src);
+        assert_eq!(output_dst, target_dst);
+        assert!(output_identifier_1.is_multiple_of(256)); // First port of a 256-port "port block" from allocator
+        assert_eq!(done_reason, None);
+
+        // Reverse path
+        let (return_output_src, return_output_dst, return_output_identifier, done_reason) =
+            check_packet_icmp_echo(
+                &mut nat,
+                vni(200),
+                vni(100),
+                target_dst,
+                target_src,
+                IcmpEchoDirection::Reply,
+                output_identifier_1,
+            );
+        assert_eq!(return_output_src, orig_dst);
+        assert_eq!(return_output_dst, orig_src);
+        assert_eq!(return_output_identifier, orig_identifier);
+        assert_eq!(done_reason, None);
+
+        // Second request with same identifier: no reallocation
+        let (orig_src, orig_dst) = (addr_v4("1.1.2.3"), addr_v4("5.0.0.5"));
+        let (target_src, target_dst) = (addr_v4("2.2.0.0"), addr_v4("5.0.0.5"));
+        let (output_src, output_dst, output_identifier_2, done_reason) = check_packet_icmp_echo(
+            &mut nat,
+            vni(100),
+            vni(200),
+            orig_src,
+            orig_dst,
+            IcmpEchoDirection::Request,
+            orig_identifier,
+        );
+        assert_eq!(output_src, target_src);
+        assert_eq!(output_dst, target_dst);
+        assert_eq!(output_identifier_2, output_identifier_1); // Same identifier as before
+        assert_eq!(done_reason, None);
+    }
+
     #[allow(clippy::too_many_arguments)]
     fn check_packet_icmp_error(
         nat: &mut StatefulNat,