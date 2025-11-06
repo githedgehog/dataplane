githedgehog/dataplane

commit a09de33a3d786fa42f9ed3b78592042ad51408d3

feat(dataplane): Add stateful NAT stage to pipeline
Now that users have an API to specify whether they desire stateful or
stateless NAT, add a stateful NAT pipeline stage to dataplane's
pipeline, to support processing packets with, well, stateful NAT,
wheneverrequired.

To pass the reader to the DynPipeline closure, we create a
NatAllocatorReaderFactory object which is not really necessary (we could
just get the reader and clone() it), but helps make the code more
consistent with stateless NAT's setup.

Signed-off-by: Quentin Monnet <qmo@qmon.net>

dataplane/src/packet_processor/mod.rs
@@ -15,9 +15,9 @@ use concurrency::sync::Arc;
 use pkt_meta::dst_vpcd_lookup::{DstVpcdLookup, VpcDiscTablesWriter};
 use pkt_meta::flow_table::{ExpirationsNF, FlowTable, LookupNF};
 
-use nat::StatelessNat;
 use nat::stateful::NatAllocatorWriter;
 use nat::stateless::NatTablesWriter;
+use nat::{StatefulNat, StatelessNat};
 
 use net::buffer::PacketBufferMut;
 use pipeline::DynPipeline;
@@ -46,6 +46,7 @@ pub(crate) fn start_router<Buf: PacketBufferMut>(
     params: RouterParams,
 ) -> Result<InternalSetup<Buf>, RouterError> {
     let nattablew = NatTablesWriter::new();
+    let natallocatorw = NatAllocatorWriter::new();
     let vpcdtablesw = VpcDiscTablesWriter::new();
     let router = Router::new(params)?;
     let vpcmapw = VpcMapWriter::<VpcMapName>::new();
@@ -57,6 +58,7 @@ pub(crate) fn start_router<Buf: PacketBufferMut>(
     let vpcdtablesr_factory = vpcdtablesw.get_reader_factory();
     let atabler_factory = router.get_atabler_factory();
     let nattabler_factory = nattablew.get_reader_factory();
+    let natallocator_factory = natallocatorw.get_reader_factory();
 
     let pipeline_builder = move || {
         // Build network functions
@@ -66,6 +68,7 @@ pub(crate) fn start_router<Buf: PacketBufferMut>(
         let iprouter1 = IpForwarder::new("IP-Forward-1", fibtr_factory.handle());
         let iprouter2 = IpForwarder::new("IP-Forward-2", fibtr_factory.handle());
         let stateless_nat = StatelessNat::with_reader("stateless-NAT", nattabler_factory.handle());
+        let stateful_nat = StatefulNat::with_reader("stateful-NAT", natallocator_factory.handle());
         let dumper1 = PacketDumper::new("pre-ingress", true, Some(PacketDumper::vxlan_or_icmp()));
         let dumper2 = PacketDumper::new("post-egress", true, Some(PacketDumper::vxlan_or_icmp()));
         let stats_stage = Stats::new("stats", writer.clone());
@@ -82,6 +85,7 @@ pub(crate) fn start_router<Buf: PacketBufferMut>(
             .add_stage(dst_vpcd_lookup)
             .add_stage(flow_lookup_nf)
             .add_stage(stateless_nat)
+            .add_stage(stateful_nat)
             .add_stage(iprouter2)
             .add_stage(stage_egress)
             .add_stage(dumper2)
@@ -94,7 +98,7 @@ pub(crate) fn start_router<Buf: PacketBufferMut>(
         pipeline: Arc::new(pipeline_builder),
         vpcmapw,
         nattablew: Some(nattablew),
-        natallocatorw: None, // Not instanciated, current pipeline does not use stateful NAT
+        natallocatorw: Some(natallocatorw),
         vpcdtablesw,
         stats,
     })

nat/src/stateful/allocator_writer.rs
@@ -59,6 +59,11 @@ impl NatAllocatorWriter {
         NatAllocatorReader(self.allocator.clone())
     }
 
+    #[must_use]
+    pub fn get_reader_factory(&self) -> NatAllocatorReaderFactory {
+        self.get_reader().factory()
+    }
+
     pub fn update_allocator(&mut self, vpc_table: &VpcTable) -> Result<(), ConfigError> {
         let new_config = StatefulNatConfig::new(vpc_table);
 
@@ -120,11 +125,24 @@ impl Default for NatAllocatorWriter {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct NatAllocatorReader(Arc<ArcSwapOption<NatDefaultAllocator>>);
 
 impl NatAllocatorReader {
     pub fn get(&self) -> Option<Arc<NatDefaultAllocator>> {
         self.0.load().clone()
+    }
+    #[must_use]
+    pub fn factory(&self) -> NatAllocatorReaderFactory {
+        NatAllocatorReaderFactory(self.clone())
+    }
+}
+
+#[derive(Debug)]
+pub struct NatAllocatorReaderFactory(NatAllocatorReader);
+impl NatAllocatorReaderFactory {
+    #[must_use]
+    pub fn handle(&self) -> NatAllocatorReader {
+        self.0.clone()
     }
 }

commit 7e6d5c29d6b42581b62a6796656204820feeaf1f

fix(nat): Reuse flow table from flow table lookup stage
When we added the stateful NAT stage to dataplane's pipeline, we made it
create a new flow table, from scratch, rather than reusing the existing
one from the flow table lookup stage. This is bad: this means that we
create NAT sessions in a table different than the one we use for
lookups, so we never add entries to the lookup table and never find
existing sessions. For packets going out, this means we recreate a
session every time; for packets coming back, we deny session creation
and the packets are dropped.

Change stateful NAT setup in order to reuse the flow table from the
lookup stage.

Fixes: a09de33a3d78 ("feat(dataplane): Add stateful NAT stage to pipeline")
Signed-off-by: Quentin Monnet <qmo@qmon.net>

dataplane/src/packet_processor/mod.rs
@@ -78,7 +78,11 @@ pub(crate) fn start_router<Buf: PacketBufferMut>(
         let iprouter1 = IpForwarder::new("IP-Forward-1", fibtr_factory.handle());
         let iprouter2 = IpForwarder::new("IP-Forward-2", fibtr_factory.handle());
         let stateless_nat = StatelessNat::with_reader("stateless-NAT", nattabler_factory.handle());
-        let stateful_nat = StatefulNat::with_reader("stateful-NAT", natallocator_factory.handle());
+        let stateful_nat = StatefulNat::new(
+            "stateful-NAT",
+            flow_table.clone(),
+            natallocator_factory.handle(),
+        );
         let dumper1 = PacketDumper::new("pre-ingress", true, None);
         let dumper2 = PacketDumper::new("post-egress", true, None);
         let stats_stage = Stats::new("stats", writer.clone());

nat/src/stateful/mod.rs
@@ -94,31 +94,31 @@ pub struct StatefulNat {
     allocator: NatAllocatorReader,
 }
 
-#[allow(clippy::new_without_default)]
 impl StatefulNat {
-    /// Creates a new [`StatefulNat`] processor.
+    /// Creates a new [`StatefulNat`] processor from provided parameters.
     #[must_use]
-    pub fn new(name: &str) -> (Self, NatAllocatorWriter) {
+    pub fn new(name: &str, sessions: Arc<FlowTable>, allocator: NatAllocatorReader) -> Self {
+        Self {
+            name: name.to_string(),
+            sessions,
+            allocator,
+        }
+    }
+
+    /// Creates a new [`StatefulNat`] processor with empty allocator and session table, returning a
+    /// [`NatAllocatorWriter`] object.
+    #[must_use]
+    pub fn new_with_defaults() -> (Self, NatAllocatorWriter) {
         let allocator_writer = NatAllocatorWriter::new();
         let allocator_reader = allocator_writer.get_reader();
         (
-            Self {
-                name: name.to_string(),
-                sessions: Arc::new(FlowTable::default()),
-                allocator: allocator_reader,
-            },
+            Self::new(
+                "stateful-nat",
+                Arc::new(FlowTable::default()),
+                allocator_reader,
+            ),
             allocator_writer,
         )
-    }
-
-    /// Creates a new [`StatefulNat`] processor as `new()`, but uses the provided `NatAllocatorReader`.
-    #[must_use]
-    pub fn with_reader(name: &str, allocator: NatAllocatorReader) -> Self {
-        Self {
-            name: name.to_string(),
-            sessions: Arc::new(FlowTable::default()),
-            allocator,
-        }
     }
 
     /// Get the name of this instance

nat/src/stateful/test.rs
@@ -348,7 +348,7 @@ mod tests {
         config.validate().unwrap();
 
         // Check that we can validate the allocator
-        let (mut nat, mut allocator) = StatefulNat::new("test-nat");
+        let (mut nat, mut allocator) = StatefulNat::new_with_defaults();
         allocator
             .update_allocator(&config.external.overlay.vpc_table)
             .unwrap();
@@ -561,7 +561,7 @@ mod tests {
         config.validate().unwrap();
 
         // Check that we can validate the allocator
-        let (mut nat, mut allocator) = StatefulNat::new("test-nat");
+        let (mut nat, mut allocator) = StatefulNat::new_with_defaults();
         allocator
             .update_allocator(&config.external.overlay.vpc_table)
             .unwrap();
@@ -746,7 +746,7 @@ mod tests {
         config.validate().unwrap();
 
         // Check that we can validate the allocator
-        let (mut nat, mut allocator) = StatefulNat::new("test-nat");
+        let (mut nat, mut allocator) = StatefulNat::new_with_defaults();
         allocator
             .update_allocator(&config.external.overlay.vpc_table)
             .unwrap();
@@ -912,7 +912,7 @@ mod tests {
         config.validate().unwrap();
 
         // Check that we can validate the allocator
-        let (mut nat, mut allocator) = StatefulNat::new("test-nat");
+        let (mut nat, mut allocator) = StatefulNat::new_with_defaults();
         allocator
             .update_allocator(&config.external.overlay.vpc_table)
             .unwrap();