githedgehog/dataplane

commit a08366512b4949e6f4a95c424689db91f1b7c948

fix(nat): Allow un-NATed traffic from VpcExpose without NAT
With stateful NAT, we reject session creation when we are not able to
find source NAT information. This is to prevent connections coming "from
the outside" of a VPC with stateful NAT configured to open new sessions,
sessions can only be initiated "from the inside".

However, we enforce this rule on all connections, ... whether or not
they pertain to a VpcExpose that has NAT configured. As a consequence,
traffic associated to a peering that does not rely on NAT would be drop,
because we cannot find source NAT information (obviously).

To address this issue, we introduce stateless "exemption tables"
containing valid prefixes from VpcExpose objects that do not use
stateful NAT. Traffic associated with these prefixes can just go through
without being NAT-ed. Traffic associated with VpcExpose objects using
stateful NAT will be NAT-ed, as expected. Traffic associated with no
exposed IP will be dropped. We add a test with no NAT configured, to be
sure.

Fixes: fb45c063b418 ("feat(nat): Reject stateful NAT connection if there is no source NAT rule")
Signed-off-by: Quentin Monnet <qmo@qmon.net>

nat/src/stateful/mod.rs
@@ -547,6 +547,14 @@ impl StatefulNat {
         src_vpc_id: VpcDiscriminant,
         dst_vpc_id: VpcDiscriminant,
     ) -> Result<bool, StatefulNatError> {
+        if let Some(allocator) = self.allocator.get()
+            && I::is_exempt(allocator, flow_key).map_err(StatefulNatError::AllocationFailure)?
+        {
+            // Packet is allowed to go through without NAT, leave it unchanged
+            debug!("{}: Packet exempt from NAT", self.name());
+            return Ok(false);
+        }
+
         // Hot path: if we have a session, directly translate the address already
         if let Some(state) = Self::lookup_session::<I, Buf>(packet) {
             debug!("{}: Found session, translating packet", self.name());
@@ -560,19 +568,18 @@ impl StatefulNat {
         }
 
         let Some(allocator) = self.allocator.get() else {
-            // No allocator set - We refuse to process this packet if we don't have a way to tell
-            // whether it should be NAT-ed or not
+            // No allocator set - We refuse to process this packet further, as we can't allocate a
+            // new session.
             return Err(StatefulNatError::NoAllocator);
         };
 
         // Else, if we need NAT for this packet, create a new session and translate the address
         let alloc =
             I::allocate(allocator, flow_key).map_err(StatefulNatError::AllocationFailure)?;
+        // If we didn't find source NAT translation information, we should deny the creation of a
+        // new session: we don't allow packets "from the outside" to create new sessions.
+        debug_assert!(alloc.src.is_some());
 
-        if alloc.src.is_none() && alloc.dst.is_none() {
-            // No NAT for this tuple, leave the packet unchanged - Do not drop it
-            return Ok(false);
-        }
         debug!("{}: Allocated translation data: {}", self.name(), alloc);
 
         // Given that at least one of alloc.src or alloc.dst is set, we should always have at

nat/src/stateful/test.rs
@@ -616,6 +616,99 @@ mod tests {
         assert_eq!(done_reason, Some(DoneReason::Filtered));
     }
 
+    fn build_overlay_2vpcs_no_nat() -> Overlay {
+        fn add_expose(manifest: &mut VpcManifest, expose: VpcExpose) {
+            manifest.add_expose(expose).expect("Failed to add expose");
+        }
+
+        let mut vpc_table = VpcTable::new();
+        let _ = vpc_table.add(Vpc::new("VPC-1", "AAAAA", 100).expect("Failed to add VPC"));
+        let _ = vpc_table.add(Vpc::new("VPC-2", "BBBBB", 200).expect("Failed to add VPC"));
+
+        let expose121 = VpcExpose::empty().ip("1.1.0.0/16".into());
+        let expose211 = VpcExpose::empty().ip("2.2.0.0/16".into());
+
+        let mut manifest12 = VpcManifest::new("VPC-1");
+        add_expose(&mut manifest12, expose121);
+        let mut manifest21 = VpcManifest::new("VPC-2");
+        add_expose(&mut manifest21, expose211);
+
+        let peering12 = VpcPeering::new("VPC-1--VPC-2", manifest12, manifest21);
+
+        let mut peering_table = VpcPeeringTable::new();
+        peering_table.add(peering12).expect("Failed to add peering");
+
+        Overlay::new(vpc_table, peering_table)
+    }
+
+    #[test]
+    #[traced_test]
+    fn test_full_config_no_nat() {
+        let mut config = build_sample_config(build_overlay_2vpcs_no_nat());
+        config.validate().unwrap();
+
+        // Check that we can validate the allocator
+        let (mut nat, mut allocator) = StatefulNat::new_with_defaults();
+        allocator
+            .update_allocator(&config.external.overlay.vpc_table)
+            .unwrap();
+
+        // No NAT, and no exposed IPs
+        let (orig_src, orig_dst, orig_src_port, orig_dst_port) = ("8.8.8.8", "9.9.9.9", 9998, 443);
+        let (output_src, output_dst, output_src_port, output_dst_port, done_reason) = check_packet(
+            &mut nat,
+            vni(100),
+            vni(200),
+            orig_src,
+            orig_dst,
+            orig_src_port,
+            orig_dst_port,
+        );
+        assert_eq!(output_src, addr_v4(orig_src));
+        assert_eq!(output_dst, addr_v4(orig_dst));
+        assert_eq!(output_src_port, orig_src_port);
+        assert_eq!(output_dst_port, orig_dst_port);
+        assert_eq!(done_reason, Some(DoneReason::Filtered));
+
+        // No NAT, with valid exposed IPs
+        let (orig_src, orig_dst, orig_src_port, orig_dst_port) = ("1.1.2.3", "2.2.4.5", 9998, 443);
+        let (output_src, output_dst, output_src_port, output_dst_port, done_reason) = check_packet(
+            &mut nat,
+            vni(100),
+            vni(200),
+            orig_src,
+            orig_dst,
+            orig_src_port,
+            orig_dst_port,
+        );
+        assert_eq!(output_src, addr_v4(orig_src));
+        assert_eq!(output_dst, addr_v4(orig_dst));
+        assert_eq!(output_src_port, orig_src_port);
+        assert_eq!(output_dst_port, orig_dst_port);
+        assert_eq!(done_reason, None);
+        // Reverse path
+        let (
+            return_output_src,
+            return_output_dst,
+            return_output_src_port,
+            return_output_dst_port,
+            done_reason,
+        ) = check_packet(
+            &mut nat,
+            vni(200),
+            vni(100),
+            orig_dst,
+            orig_src,
+            output_dst_port,
+            output_src_port,
+        );
+        assert_eq!(return_output_src, addr_v4(orig_dst));
+        assert_eq!(return_output_dst, addr_v4(orig_src));
+        assert_eq!(return_output_src_port, orig_dst_port);
+        assert_eq!(return_output_dst_port, orig_src_port);
+        assert_eq!(done_reason, None);
+    }
+
     fn check_packet_icmp_echo(
         nat: &mut StatefulNat,
         src_vni: Vni,

nat/src/stateful/apalloc/mod.rs
@@ -74,9 +74,9 @@ use net::packet::VpcDiscriminant;
 use pkt_meta::flow_table::FlowKey;
 use pkt_meta::flow_table::IpProtoKey;
 use pkt_meta::flow_table::flow_key::IcmpProtoKey;
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, BTreeSet};
 use std::fmt::Display;
-use std::net::{Ipv4Addr, Ipv6Addr};
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
 mod alloc;
 mod natip_with_bitmap;
@@ -153,25 +153,60 @@ impl<I: NatIpWithBitmap, J: NatIpWithBitmap> PoolTable<I, J> {
         dst_id: VpcDiscriminant,
         addr: I,
     ) -> Option<&alloc::IpAllocator<J>> {
-        let key = PoolTableKey::new(
-            protocol,
-            src_id,
-            dst_id,
-            addr,
-            // This field is not usually relevant for the lookup. The only case it's considered is
-            // when all other fields match exactly with the fields from a key in the PoolTable. To
-            // make sure we pick the entry in this case, we need to ensure the value is always
-            // greater or equal to the one of the key from the PoolTable. So we set it to the
-            // largest possible value.
-            I::try_from_bits(u128::MAX)
-                .or(I::try_from_bits(u32::MAX.into()))
-                .ok()?, // Cannot fail - IPv6 and IPv4 can always be built from u32::MAX
-        );
+        let key = PoolTableKey::new(protocol, src_id, dst_id, addr, max_range::<I>());
         self.get(&key)
     }
 
     fn add_entry(&mut self, key: PoolTableKey<I>, allocator: alloc::IpAllocator<J>) {
         self.0.insert(key, allocator);
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// ExemptTableKey
+///////////////////////////////////////////////////////////////////////////////
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+struct ExemptTableKey<I: NatIp> {
+    src_id: VpcDiscriminant,
+    dst_id: VpcDiscriminant,
+    addr: I,
+    addr_range_end: I,
+}
+
+impl<I: NatIp> ExemptTableKey<I> {
+    fn new(src_id: VpcDiscriminant, dst_id: VpcDiscriminant, addr: I, addr_range_end: I) -> Self {
+        Self {
+            src_id,
+            dst_id,
+            addr,
+            addr_range_end,
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// ExemptTable
+///////////////////////////////////////////////////////////////////////////////
+
+#[derive(Debug)]
+struct ExemptTable<I: NatIp>(BTreeSet<ExemptTableKey<I>>);
+
+impl<I: NatIp> ExemptTable<I> {
+    fn new() -> Self {
+        Self(BTreeSet::new())
+    }
+
+    fn add(&mut self, key: ExemptTableKey<I>) {
+        self.0.insert(key);
+    }
+
+    fn contains(&self, src_id: VpcDiscriminant, dst_id: VpcDiscriminant, addr: I) -> bool {
+        let key = ExemptTableKey::new(src_id, dst_id, addr, max_range::<I>());
+        // See comment in PoolTable::get()
+        self.0.range(..=&key).next_back().is_some_and(|k| {
+            k.addr_range_end >= key.addr && k.src_id == key.src_id && k.dst_id == key.dst_id
+        })
     }
 }
 
@@ -205,6 +240,8 @@ pub struct NatDefaultAllocator {
     pools_dst44: PoolTable<Ipv4Addr, Ipv4Addr>,
     pools_src66: PoolTable<Ipv6Addr, Ipv6Addr>,
     pools_dst66: PoolTable<Ipv6Addr, Ipv6Addr>,
+    exempt4: ExemptTable<Ipv4Addr>,
+    exempt6: ExemptTable<Ipv6Addr>,
 }
 
 impl NatAllocator<AllocatedIpPort<Ipv4Addr>, AllocatedIpPort<Ipv6Addr>> for NatDefaultAllocator {
@@ -214,6 +251,8 @@ impl NatAllocator<AllocatedIpPort<Ipv4Addr>, AllocatedIpPort<Ipv6Addr>> for NatD
             pools_dst44: PoolTable::new(),
             pools_src66: PoolTable::new(),
             pools_dst66: PoolTable::new(),
+            exempt4: ExemptTable::new(),
+            exempt6: ExemptTable::new(),
         }
     }
 
@@ -229,6 +268,32 @@ impl NatAllocator<AllocatedIpPort<Ipv4Addr>, AllocatedIpPort<Ipv6Addr>> for NatD
         flow_key: &FlowKey,
     ) -> Result<AllocationResult<AllocatedIpPort<Ipv6Addr>>, AllocatorError> {
         Self::allocate_from_tables(flow_key, &self.pools_src66, &self.pools_dst66)
+    }
+
+    fn is_exempt_v4(&self, flow_key: &FlowKey) -> Result<bool, AllocatorError> {
+        let data = flow_key.data();
+        let IpAddr::V4(src_addr) = data.src_ip() else {
+            return Err(AllocatorError::InternalIssue(
+                "Failed to convert IP address to Ipv4addr (exempt lookup)".to_string(),
+            ));
+        };
+        let (Some(src_id), Some(dst_id)) = (data.src_vpcd(), data.dst_vpcd()) else {
+            return Ok(false);
+        };
+        Ok(self.exempt4.contains(src_id, dst_id, *src_addr))
+    }
+
+    fn is_exempt_v6(&self, flow_key: &FlowKey) -> Result<bool, AllocatorError> {
+        let data = flow_key.data();
+        let IpAddr::V6(src_addr) = data.src_ip() else {
+            return Err(AllocatorError::InternalIssue(
+                "Failed to convert IP address to Ipv6addr (exempt lookup)".to_string(),
+            ));
+        };
+        let (Some(src_id), Some(dst_id)) = (data.src_vpcd(), data.dst_vpcd()) else {
+            return Ok(false);
+        };
+        Ok(self.exempt6.contains(src_id, dst_id, *src_addr))
     }
 }
 
@@ -435,6 +500,17 @@ impl NatDefaultAllocator {
             IcmpProtoKey::Unsupported => Err(AllocatorError::UnsupportedIcmpCategory),
         }
     }
+}
+
+// This method is for setting a range end field that is not usually relevant for table lookups, for
+// example for PoolTable or ExemptTable lookups. The only case the resulting field is considered is
+// when all other fields from the lookup key match exactly with the fields from a key in a table. To
+// make sure we pick the entry in this case, we need to ensure the value is always greater or equal
+// to the one of the key from the PoolTable/ExemptTable. So we set it to the largest possible value.
+fn max_range<I: NatIp>() -> I {
+    I::try_from_bits(u128::MAX)
+        .or(I::try_from_bits(u32::MAX.into()))
+        .unwrap_or_else(|()| unreachable!()) // IPv4/IPv6 can always be built from u32::MAX
 }
 
 ///////////////////////////////////////////////////////////////////////////////

config/src/external/overlay/vpcpeering.rs
@@ -220,6 +220,21 @@ impl VpcExpose {
             &nat.not_as
         }
     }
+    // This method returns true if the list of allowed prefixes is IPv4.
+    // This method assumes that all prefixes the list are of the same IP version. It does not
+    // validate the list for consistency.
+    #[must_use]
+    pub fn is_v4(&self) -> bool {
+        self.ips.first().is_some_and(Prefix::is_ipv4)
+    }
+    // This method returns true if the list of allowed prefixes is IPv6.
+    // This method assumes that all prefixes the list are of the same IP version. It does not
+    // validate the list for consistency.
+    #[must_use]
+    pub fn is_v6(&self) -> bool {
+        self.ips.first().is_some_and(Prefix::is_ipv6)
+    }
+    // This method returns true if both allowed and translated prefixes are IPv4.
     // This method assumes that all prefixes in each list are of the same IP version. It does not
     // validate the list for consistency.
     #[must_use]
@@ -229,6 +244,7 @@ impl VpcExpose {
             (Some(Prefix::IPV4(_)), Some(Prefix::IPV4(_)))
         )
     }
+    // This method returns true if both allowed and translated prefixes are IPv6.
     // This method assumes that all prefixes in each list are of the same IP version. It does not
     // validate the list for consistency.
     #[must_use]
@@ -440,6 +456,18 @@ impl VpcManifest {
             .iter()
             .filter(|expose| expose.has_stateful_nat())
             .filter(|expose| expose.is_66())
+    }
+    pub fn no_stateful_nat_exposes_v4(&self) -> impl Iterator<Item = &VpcExpose> {
+        self.exposes
+            .iter()
+            .filter(|expose| !expose.has_stateful_nat())
+            .filter(|expose| expose.is_v4())
+    }
+    pub fn no_stateful_nat_exposes_v6(&self) -> impl Iterator<Item = &VpcExpose> {
+        self.exposes
+            .iter()
+            .filter(|expose| !expose.has_stateful_nat())
+            .filter(|expose| expose.is_v6())
     }
 }
 

nat/src/stateful/allocator.rs
@@ -108,6 +108,8 @@ where
     fn new() -> Self;
     fn allocate_v4(&self, flow_key: &FlowKey) -> Result<AllocationResult<T>, AllocatorError>;
     fn allocate_v6(&self, flow_key: &FlowKey) -> Result<AllocationResult<U>, AllocatorError>;
+    fn is_exempt_v4(&self, flow_key: &FlowKey) -> Result<bool, AllocatorError>;
+    fn is_exempt_v6(&self, flow_key: &FlowKey) -> Result<bool, AllocatorError>;
 
     // TODO: Should the method for building the allocator from a VpcTable be part of this trait?
 }

nat/src/stateful/apalloc/natip_with_bitmap.rs
@@ -33,6 +33,11 @@ pub trait NatIpWithBitmap: NatIp {
         allocator: Arc<A>,
         flow_key: &FlowKey,
     ) -> Result<AllocationResult<AllocatedIpPort<Self>>, AllocatorError>;
+
+    fn is_exempt<A: NatAllocator<AllocatedIpPort<Ipv4Addr>, AllocatedIpPort<Ipv6Addr>>>(
+        allocator: Arc<A>,
+        flow_key: &FlowKey,
+    ) -> Result<bool, AllocatorError>;
 }
 
 impl NatIpWithBitmap for Ipv4Addr {
@@ -55,6 +60,13 @@ impl NatIpWithBitmap for Ipv4Addr {
         flow_key: &FlowKey,
     ) -> Result<AllocationResult<AllocatedIpPort<Self>>, AllocatorError> {
         allocator.allocate_v4(flow_key)
+    }
+
+    fn is_exempt<A: NatAllocator<AllocatedIpPort<Ipv4Addr>, AllocatedIpPort<Ipv6Addr>>>(
+        allocator: Arc<A>,
+        flow_key: &FlowKey,
+    ) -> Result<bool, AllocatorError> {
+        allocator.is_exempt_v4(flow_key)
     }
 }
 
@@ -84,5 +96,12 @@ impl NatIpWithBitmap for Ipv6Addr {
         flow_key: &FlowKey,
     ) -> Result<AllocationResult<AllocatedIpPort<Self>>, AllocatorError> {
         allocator.allocate_v6(flow_key)
+    }
+
+    fn is_exempt<A: NatAllocator<AllocatedIpPort<Ipv4Addr>, AllocatedIpPort<Ipv6Addr>>>(
+        allocator: Arc<A>,
+        flow_key: &FlowKey,
+    ) -> Result<bool, AllocatorError> {
+        allocator.is_exempt_v6(flow_key)
     }
 }

nat/src/stateful/apalloc/setup.rs
@@ -3,7 +3,7 @@
 
 use super::NatIpWithBitmap;
 use super::alloc::{IpAllocator, NatPool, PoolBitmap};
-use super::{NatDefaultAllocator, PoolTable, PoolTableKey};
+use super::{ExemptTable, ExemptTableKey, NatDefaultAllocator, PoolTable, PoolTableKey};
 use crate::stateful::allocator::AllocatorError;
 use crate::stateful::allocator_writer::StatefulNatConfig;
 use crate::stateful::{NatAllocator, NatIp};
@@ -57,13 +57,37 @@ impl NatDefaultAllocator {
         let new_peering = collapse_prefixes_peering(peering)
             .map_err(|e| AllocatorError::InternalIssue(e.to_string()))?;
 
-        // Update table for source NAT
+        // Update tables for source NAT
+        self.build_exempt_pool_for_expose(&new_peering, src_vpc_id, dst_vpc_id)?;
         self.build_src_nat_pool_for_expose(&new_peering, src_vpc_id, dst_vpc_id)?;
 
         // Update table for destination NAT
         self.build_dst_nat_pool_for_expose(&new_peering, src_vpc_id, dst_vpc_id)?;
 
         Ok(())
+    }
+
+    fn build_exempt_pool_for_expose(
+        &mut self,
+        peering: &Peering,
+        src_vpc_id: VpcDiscriminant,
+        dst_vpc_id: VpcDiscriminant,
+    ) -> Result<(), AllocatorError> {
+        build_exempt_table_generic(
+            &peering.local,
+            src_vpc_id,
+            dst_vpc_id,
+            VpcManifest::no_stateful_nat_exposes_v4,
+            &mut self.exempt4,
+        )?;
+
+        build_exempt_table_generic(
+            &peering.local,
+            src_vpc_id,
+            dst_vpc_id,
+            VpcManifest::no_stateful_nat_exposes_v6,
+            &mut self.exempt6,
+        )
     }
 
     fn build_src_nat_pool_for_expose(
@@ -179,7 +203,7 @@ fn add_pool_entries<I: NatIpWithBitmap, J: NatIpWithBitmap>(
     icmp_proto: NextHeader,
 ) -> Result<(), AllocatorError> {
     for prefix in prefixes {
-        let key = pool_table_key_for_expose(prefix, src_vpc_id, dst_vpc_id)?;
+        let key = pool_table_tcp_key_for_expose(prefix, src_vpc_id, dst_vpc_id)?;
         insert_per_proto_entries(
             table,
             key,
@@ -248,31 +272,75 @@ fn create_natpool<J: NatIpWithBitmap>(
     ))
 }
 
-fn pool_table_key_for_expose<I: NatIp>(
+fn pool_table_tcp_key_for_expose<I: NatIp>(
     prefix: &Prefix,
     src_vpc_id: VpcDiscriminant,
     dst_vpc_id: VpcDiscriminant,
 ) -> Result<PoolTableKey<I>, AllocatorError> {
+    let (addr, addr_range_end) = prefix_bounds(prefix)?;
     Ok(PoolTableKey::new(
         NextHeader::TCP,
         src_vpc_id,
         dst_vpc_id,
-        I::try_from_addr(prefix.as_address()).map_err(|()| {
-            AllocatorError::InternalIssue("Failed to build IP address".to_string())
-        })?,
-        match prefix {
-            Prefix::IPV4(p) => I::try_from_ipv4_addr(p.last_address()).map_err(|()| {
-                AllocatorError::InternalIssue(
-                    "Failed to build IPv4 address from prefix".to_string(),
-                )
-            })?,
-            Prefix::IPV6(p) => I::try_from_ipv6_addr(p.last_address()).map_err(|()| {
-                AllocatorError::InternalIssue(
-                    "Failed to build IPv6 address from prefix".to_string(),
-                )
-            })?,
-        },
+        addr,
+        addr_range_end,
     ))
+}
+
+fn build_exempt_table_generic<'a, I: NatIpWithBitmap, F, Iter>(
+    manifest: &'a VpcManifest,
+    src_vpc_id: VpcDiscriminant,
+    dst_vpc_id: VpcDiscriminant,
+    // A filter to select relevant exposes: those with stateful NAT, for the relevant IP version
+    exposes_filter: F,
+    table: &mut ExemptTable<I>,
+) -> Result<(), AllocatorError>
+where
+    F: FnOnce(&'a VpcManifest) -> Iter,
+    Iter: Iterator<Item = &'a VpcExpose>,
+{
+    exposes_filter(manifest)
+        .try_for_each(|expose| add_exempt_entries(table, &expose.ips, src_vpc_id, dst_vpc_id))
+}
+
+fn add_exempt_entries<I: NatIpWithBitmap>(
+    table: &mut ExemptTable<I>,
+    prefixes: &BTreeSet<Prefix>,
+    src_vpc_id: VpcDiscriminant,
+    dst_vpc_id: VpcDiscriminant,
+) -> Result<(), AllocatorError> {
+    for prefix in prefixes {
+        table.add(exempt_table_key_for_expose(prefix, src_vpc_id, dst_vpc_id)?);
+    }
+    Ok(())
+}
+
+fn exempt_table_key_for_expose<I: NatIp>(
+    prefix: &Prefix,
+    src_vpc_id: VpcDiscriminant,
+    dst_vpc_id: VpcDiscriminant,
+) -> Result<ExemptTableKey<I>, AllocatorError> {
+    let (addr, addr_range_end) = prefix_bounds(prefix)?;
+    Ok(ExemptTableKey::new(
+        src_vpc_id,
+        dst_vpc_id,
+        addr,
+        addr_range_end,
+    ))
+}
+
+fn prefix_bounds<I: NatIp>(prefix: &Prefix) -> Result<(I, I), AllocatorError> {
+    let addr = I::try_from_addr(prefix.as_address())
+        .map_err(|()| AllocatorError::InternalIssue("Failed to build IP address".to_string()))?;
+    let addr_range_end = match prefix {
+        Prefix::IPV4(p) => I::try_from_ipv4_addr(p.last_address()).map_err(|()| {
+            AllocatorError::InternalIssue("Failed to build IPv4 address from prefix".to_string())
+        })?,
+        Prefix::IPV6(p) => I::try_from_ipv6_addr(p.last_address()).map_err(|()| {
+            AllocatorError::InternalIssue("Failed to build IPv6 address from prefix".to_string())
+        })?,
+    };
+    Ok((addr, addr_range_end))
 }
 
 // The allocator's bitmap contains u32 only. For IPv4, it maps well to the address space. For IPv6,

commit 7c8006c047a05da382075ddcf31a2d4a7deae764

fix(nat): Fix "NAT-exempt packet" lookup for stateful NAT
We introduced a lookup to check whether packets would be exempt of
stateful NAT. We added the lookup before checking for existing sessions
for the packet. This works for packets going forward and receiving
source NAT, because they fail the exemption lookup and go through the
session lookup. However, when packets come back, they may be associated
with a source VpcExpose that does not require NAT (so might be
considered as "exempt") but may still require destination NAT if they're
part of an existing session! In that case, passing the exemption check
will prevent them from getting destination NAT, and will result in these
packets being dropped by the router.

To fix this, move the exemption lookup after the flow table lookup.

Adjust the corresponding test that had empty "IPs" for the second
VpcExpose, and as such was not triggering the issue.

Fixes: a08366512b49 ("fix(nat): Allow un-NATed traffic from VpcExpose without NAT")
Signed-off-by: Quentin Monnet <qmo@qmon.net>

nat/src/stateful/mod.rs
@@ -566,18 +566,18 @@ impl StatefulNat {
         src_vpc_id: VpcDiscriminant,
         dst_vpc_id: VpcDiscriminant,
     ) -> Result<bool, StatefulNatError> {
+        // Hot path: if we have a session, directly translate the address already
+        if let Some(state) = Self::lookup_session::<I, Buf>(packet) {
+            debug!("{}: Found session, translating packet", self.name());
+            return Self::stateful_translate(self.name(), packet, &state).and(Ok(true));
+        }
+
         if let Some(allocator) = self.allocator.get()
             && I::is_exempt(allocator, flow_key).map_err(StatefulNatError::AllocationFailure)?
         {
             // Packet is allowed to go through without NAT, leave it unchanged
             debug!("{}: Packet exempt from NAT", self.name());
             return Ok(false);
-        }
-
-        // Hot path: if we have a session, directly translate the address already
-        if let Some(state) = Self::lookup_session::<I, Buf>(packet) {
-            debug!("{}: Found session, translating packet", self.name());
-            return Self::stateful_translate(self.name(), packet, &state).and(Ok(true));
         }
 
         match self.deal_with_icmp_error_msg::<Buf, I>(packet, flow_key) {

nat/src/stateful/test.rs
@@ -539,7 +539,7 @@ mod tests {
             .unwrap()
             .ip("1.1.0.0/16".into())
             .as_range("2.2.0.0/16".into());
-        let expose211 = VpcExpose::empty();
+        let expose211 = VpcExpose::empty().ip("5.0.0.0/8".into());
 
         let mut manifest12 = VpcManifest::new("VPC-1");
         add_expose(&mut manifest12, expose121);
@@ -606,14 +606,14 @@ mod tests {
         assert_eq!(return_output_dst_port, 9998);
         assert_eq!(done_reason, None);
 
-        // NAT: expose211 <-> expose121 (no source NAT)
+        // NAT: expose211 <-> expose121 (no source NAT - no NAT happens, packet will be dropped at routing stage)
         let (orig_src, orig_dst) = ("5.0.0.5", "2.2.0.2");
         let (target_src, target_dst) = ("5.0.0.5", "2.2.0.2");
         let (output_src, output_dst, _, _, done_reason) =
             check_packet(&mut nat, vni(200), vni(100), orig_src, orig_dst, 9090, 8080);
         assert_eq!(output_src, addr_v4(target_src));
         assert_eq!(output_dst, addr_v4(target_dst));
-        assert_eq!(done_reason, Some(DoneReason::Filtered));
+        assert_eq!(done_reason, None);
     }
 
     fn build_overlay_2vpcs_no_nat() -> Overlay {