githedgehog/dataplane

commit 01b20ce081470f9d4541cdc95839d8fa57c87529

Revert "feat(nat): unmark nat flag after successful nat"
This reverts commit cb9f242c0e5aced6c25ef641375173215dfef16e.

Stateful NAT is not integrated to the pipeline, and the temporary hack
prevents the stage from actually processing packets. Let's revert the
change.

Signed-off-by: Quentin Monnet <qmo@qmon.net>

nat/src/stateless/mod.rs
@@ -322,9 +322,6 @@ impl StatelessNat {
                 packet.done(translate_error(&error));
             }
             Ok(modified) => {
-                // we have already natted the packet. Prevent stateful from doing so.
-                // This is a temporary hack.
-                packet.get_meta_mut().set_nat(false);
                 if modified {
                     packet.get_meta_mut().set_checksum_refresh(true);
                     debug!("{nfi}: Packet was NAT'ed");

commit 3829a1e90446fe072de4afa2318929df41d8aef9

fix(nat): Disable stateful + stateless NAT
Re-introduce the drop of the NAT flag at the end of stateless NAT. This
commit differs from cb9f242c0e5a ("feat(nat): unmark nat flag after
successful nat"), because we only unmark the packet when stateless NAT
has changed the packet.

The issue with stateless + stateful is that the stateful NAT stage needs
to know whether or not the packet requires NAT, and for doing so it
currently runs a "NAT-exempt" check using the source IP for the packet.
But this source IP is validated against the list of IPs of the VpcExpose
blocks and does not account for address changes from stateless NAT.

There are a number of better, but more involved possibilities to address
this. Here we go with the simplest fix.

Fixes: 01b20ce08147 ("Revert "feat(nat): unmark nat flag after successful nat"")
Signed-off-by: Quentin Monnet <qmo@qmon.net>

nat/src/stateless/mod.rs
@@ -325,6 +325,8 @@ impl StatelessNat {
                 if modified {
                     packet.get_meta_mut().set_checksum_refresh(true);
                     debug!("{nfi}: Packet was NAT'ed");
+                    // We don't support stateless + stateful NAT at the moment
+                    packet.get_meta_mut().set_nat(false);
                 } else {
                     debug!("{nfi}: No NAT translation needed");
                 }