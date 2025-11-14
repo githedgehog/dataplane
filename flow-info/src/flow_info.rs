githedgehog/dataplane

commit 9d9e229061893edc53b9aee0c42031f17597ca33

fix(nat): Fix timeout for flow expiration
We need to set an expiry time different from the current time when
building new sessions. We also need to update the expiry time on a
successful lookup for a flow.

Remove cryptic FIXME message that was here to remind us of updating the
flow_info's timeout.

Fixes: 214b56358de8 ("feat(nat): Transition stateful NAT to new flow table implementation")
Signed-off-by: Quentin Monnet <qmo@qmon.net>

nat/src/stateful/mod.rs
@@ -35,7 +35,7 @@ use pkt_meta::flow_table::{FlowKey, FlowKeyData, FlowTable, IpProtoKey, TcpProto
 use std::fmt::Debug;
 use std::hash::Hash;
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
-use std::time::Instant;
+use std::time::{Duration, Instant};
 
 #[derive(Debug, Clone, PartialEq, Eq, thiserror::Error)]
 pub enum StatefulNatError {
@@ -88,6 +88,12 @@ impl<I: NatIp> NatTuple<I> {
             dst_vpc_id,
         }
     }
+}
+
+const SESSION_TIMEOUT: Duration = Duration::from_secs(60 * 60); // one hour
+
+fn session_timeout_time() -> Instant {
+    Instant::now() + SESSION_TIMEOUT
 }
 
 /// A stateful NAT processor, implementing the [`NetworkFunction`] trait. [`StatefulNat`] processes
@@ -194,8 +200,9 @@ impl StatefulNat {
     fn lookup_session<Buf: PacketBufferMut>(packet: &mut Packet<Buf>) -> Option<NatState> {
         let flow_info = packet.get_meta_mut().flow_info.as_mut()?;
         let value = flow_info.locked.read().unwrap();
-        let state = value.nat_state.as_ref()?.extract_ref::<NatState>();
-        Some(state?.clone())
+        let state = value.nat_state.as_ref()?.extract_ref::<NatState>()?;
+        flow_info.extend_expiry(SESSION_TIMEOUT).ok()?;
+        Some(state.clone())
     }
 
     fn create_session<I: NatIp>(&mut self, tuple: &NatTuple<I>, state: NatState) -> Result<(), ()> {
@@ -220,7 +227,7 @@ impl StatefulNat {
             tuple.dst_ip.to_ip_addr(),
             proto_key,
         ));
-        let flow_info = FlowInfo::new(Instant::now());
+        let flow_info = FlowInfo::new(session_timeout_time());
         flow_info.locked.write().unwrap().nat_state = Some(Box::new(state));
 
         self.sessions.insert(flow_key, flow_info);
@@ -413,7 +420,6 @@ impl StatefulNat {
         // Hot path: if we have a session, directly translate the address already
         if let Some(state) = Self::lookup_session(packet) {
             Self::stateful_translate::<Buf>(packet, &state, tuple.next_header);
-            //Self::update_stats(&state, total_bytes); // FIXME
             return Ok(true);
         }
 

commit 11151bac00dc7850c177e732d49eee21f4a8082e

fix(nat): Fix timeout reset for table flows in stateful NAT
When updating the timeout for a flow after a successful lookup in the
flow table, we add the desired timeout duration to the existing timeout
instant, instead of the current time, meaning that we linearly increment
the duration (beyond the desired period) each time we receive a packet
for that flow. Let's fix by resetting to the current time, plus the
desired duration, instead.

We do this by introducing a new method reset_expiry(), doing

    self.expires_at.store(
        Instant::now() + duration,
        std::sync::atomic::Ordering::Relaxed,
    );

To compare with extend_expiry(), which does:

    self.expires_at
        .fetch_add(duration, std::sync::atomic::Ordering::Relaxed);

Fixes: 9d9e22906189 ("fix(nat): Fix timeout for flow expiration")
Reported-by: Fredi Raspall <fredi@githedgehog.com>
Signed-off-by: Quentin Monnet <qmo@qmon.net>

flow-info/src/flow_info.rs
@@ -162,6 +162,33 @@ impl FlowInfo {
             .fetch_add(duration, std::sync::atomic::Ordering::Relaxed);
     }
 
+    /// Reset the expiry of the flow if it is not expired.
+    ///
+    /// # Errors
+    ///
+    /// Returns `FlowInfoError::FlowExpired` if the flow is expired with the expiry `Instant`
+    ///
+    pub fn reset_expiry(&self, duration: Duration) -> Result<(), FlowInfoError> {
+        if self.status.load(std::sync::atomic::Ordering::Relaxed) == FlowStatus::Expired {
+            return Err(FlowInfoError::FlowExpired(self.expires_at()));
+        }
+        self.reset_expiry_unchecked(duration);
+        Ok(())
+    }
+
+    /// Reset the expiry of the flow without checking if it is already expired.
+    ///
+    /// # Thread Safety
+    ///
+    /// This method is thread-safe.
+    ///
+    pub fn reset_expiry_unchecked(&self, duration: Duration) {
+        self.expires_at.store(
+            Instant::now() + duration,
+            std::sync::atomic::Ordering::Relaxed,
+        );
+    }
+
     pub fn status(&self) -> FlowStatus {
         self.status.load(std::sync::atomic::Ordering::Relaxed)
     }

nat/src/stateful/mod.rs
@@ -154,7 +154,7 @@ impl StatefulNat {
         let flow_info = packet.get_meta_mut().flow_info.as_mut()?;
         let value = flow_info.locked.read().unwrap();
         let state = value.nat_state.as_ref()?.extract_ref::<NatFlowState<I>>()?;
-        flow_info.extend_expiry(state.idle_timeout).ok()?;
+        flow_info.reset_expiry(state.idle_timeout).ok()?;
         let translation_data = Self::get_translation_info(&state.src_alloc, &state.dst_alloc);
         Some(translation_data)
     }