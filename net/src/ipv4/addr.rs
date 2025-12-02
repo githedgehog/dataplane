githedgehog/dataplane

commit efe3008734d75bc275ff1c3da61fb6edad0aa929

feat(net): forbid broadcast as a unicast ipv4 addr
Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

net/src/ipv4/addr.rs
@@ -33,7 +33,7 @@ impl UnicastIpv4Addr {
     ///
     /// Returns the supplied address back in the [`Err`] case if it is not a unicast address.
     pub fn new(ip: Ipv4Addr) -> Result<UnicastIpv4Addr, Ipv4Addr> {
-        if ip.is_multicast() {
+        if ip.is_multicast() || ip.is_broadcast() {
             Err(ip)
         } else {
             Ok(UnicastIpv4Addr(ip))

commit 760dc72edc5423229a6e141512eff1fb057ceb68

fix(net): Fix Bolero generator for UnicastIpv4Addr
Since commit efe3008734d7 ("feat(net): forbid broadcast as a unicast
ipv4 addr"), we forbid broadcast IPv4 addresses (namely,
255.255.255.255) when building a UnicastIpv4Addr:

    impl UnicastIpv4Addr {
        pub fn new(ip: Ipv4Addr) -> Result<UnicastIpv4Addr, Ipv4Addr> {
            if ip.is_multicast() || ip.is_broadcast() {
                Err(ip)
            } else {
                Ok(UnicastIpv4Addr(ip))
            }
        }

        [...]
    }

We need to adjust the Bolero generator for UnicastIpv4Addr accordingly,
or test unicast_ipv4_is_unicast_ip may end up trying to unwrap on an Err
value:

    fn unicast_ipv4_is_unicast_ip() {
        bolero::check!()
            .with_type()
            .cloned()
            .for_each(|ipv4: UnicastIpv4Addr| {
                let ip: UnicastIpAddr = ipv4.inner().try_into().unwrap();
                [...]
            });
    }

Fixes: efe3008734d7 ("feat(net): forbid broadcast as a unicast ipv4 addr")
Signed-off-by: Quentin Monnet <qmo@qmon.net>

net/src/ipv4/addr.rs
@@ -108,6 +108,11 @@ mod contract {
                     raw[2],
                     raw[3],
                 )))
+            } else if ip.is_broadcast() {
+                // Note: is_broadcast() means "is 255.255.255.255?", it doesn't account for the
+                // current network in use. So when we reach this case we can just replace by any
+                // valid unicast address.
+                Some(UnicastIpv4Addr(Ipv4Addr::new(1, 1, 1, 1)))
             } else {
                 Some(UnicastIpv4Addr(ip))
             }