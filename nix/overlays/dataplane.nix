ee81a9d4b0e32bf7ca255d3e8483cf031b659ace
Daniel Noland <daniel@githedgehog.com>
2025-12-15 00:52:02 +0000

    build(nix): intro addToEnv helper
    We begin the construction of our sysroot with a helper function
    designed to inject environment variables into a nixpkgs provided
    stdenv.
    
    This will (eventually) allow us to customize the flags used to
    compile each package needed for our sysroot.
    
    Signed-off-by: Daniel Noland <daniel@githedgehog.com>

--- nix/overlays/dataplane.nix
+++ nix/overlays/dataplane.nix
@@ -1,1 +1,11 @@
-{}: final: prev: {}
+{ }:
+final: prev:
+let
+  helpers.addToEnv =
+    add: orig:
+    orig
+    // (
+      with builtins; (mapAttrs (var: val: (toString (orig.${var} or "")) + " " + (toString val)) add)
+    );
+in
+{ }
