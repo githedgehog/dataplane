95abc1b7b3906ff04b15eff4767fa87afb2f3ba2
Quentin Monnet <qmo@qmon.net>
2025-12-19 01:54:43 +0000

    feat(flow-filter): Split overlapping prefixes for shared IPs support
    When looking up for the destination VPC for a packet, we need a special
    treatment when IP/port ranges are shared across peerings. We already
    have the checks in place to make sure that the lookup stage does not
    return an answer when there are multiple possible matching destination
    VPCs (based on source VPC and destination address).
    
    However, the restriction is too strong: when we have overlapping, but
    distinct IP ranges shared across peerings, we can tell the destination
    VPC for some portion of the ranges.
    
    Consider the following case, for example:
    
        VPC A        <->        VPC B        <->         VPC C
        1.0.0.0/24     2.0.0.0/24   2.0.0.0/24           1.0.0.0/24
    
    In this case, we just can't tell, for packets coming from VPC B, what
    the destination VPC is (unless there's some stateful NAT session in
    place, but that's beyond the destination VPC lookup stage's scope).
    
    But if, instead, we have:
    
        VPC A        <->        VPC B        <->         VPC C
        1.0.0.0/23     2.0.0.0/24   2.0.0.0/24           1.0.0.0/24
    
    Then for packets going to 1.0.1.0/24, we know the destination is VPC A.
    
    Right now the destination VPC lookup does not account for the
    unambiguous portion of prefixes.
    
    This commit does not change that, but it improves the flow-filter stage
    instead, which is used to determine whether packets belong to allowed
    connections (as defined by the user's peering configuration). This is
    not necessary to check that the packet is allowed (if multiple valid
    connections are present, the packet is definitely allowed), but due to
    the context present in the flow-filter table, we consider replacing the
    destination VPC lookup stage by the flow-filter stage only. This would
    make one less stage in the pipeline, and would address the shared IPs
    issues described above.
    
    Signed-off-by: Quentin Monnet <qmo@qmon.net>

--- flow-filter/src/setup.rs
+++ flow-filter/src/setup.rs
@@ -4,16 +4,20 @@
 use crate::FlowFilterTable;
 use config::ConfigError;
 use config::external::overlay::Overlay;
-use config::external::overlay::vpc::Peering;
-use config::external::overlay::vpcpeering::VpcExpose;
+use config::external::overlay::vpc::{Peering, Vpc};
+use config::external::overlay::vpcpeering::{VpcExpose, VpcManifest};
+use config::internal::interfaces::interface::InterfaceConfigTable;
 use config::utils::{ConfigUtilError, collapse_prefixes_peering};
+use lpm::prefix::{IpRangeWithPorts, PrefixWithOptionalPorts};
 use net::packet::VpcDiscriminant;
 
 impl FlowFilterTable {
     /// Build a [`FlowFilterTable`] from an overlay
     pub fn build_from_overlay(overlay: &Overlay) -> Result<Self, ConfigError> {
+        let clean_vpc_table = cleanup_vpc_table(overlay.vpc_table.values().collect())?;
         let mut table = FlowFilterTable::new();
-        for vpc in overlay.vpc_table.values() {
+
+        for vpc in &clean_vpc_table {
             for peering in &vpc.peerings {
                 // Get the destination VPC discriminant
                 let src_vpcd = VpcDiscriminant::VNI(vpc.vni);
@@ -48,20 +52,8 @@ impl FlowFilterTable {
         src_vpcd: VpcDiscriminant,
         dst_vpcd: VpcDiscriminant,
     ) -> Result<(), ConfigError> {
-        // "Collapse" prefixes to get rid of exclusion prefixes
-        let collapsed_peering = collapse_prefixes_peering(peering).map_err(|e| match e {
-            ConfigUtilError::SplitPrefixError(prefix) => {
-                ConfigError::FailureApply(format!("Failed to split prefix: {prefix}"))
-            }
-        })?;
-
-        for local_prefix in collapsed_peering
-            .local
-            .exposes
-            .iter()
-            .flat_map(VpcExpose::public_ips)
-        {
-            for remote_prefix in collapsed_peering
+        for local_prefix in peering.local.exposes.iter().flat_map(|expose| &expose.ips) {
+            for remote_prefix in peering
                 .remote
                 .exposes
                 .iter()
@@ -79,4 +71,99 @@ impl FlowFilterTable {
         }
         Ok(())
     }
+}
+
+fn clone_skipping_peerings(vpc: &Vpc) -> Vpc {
+    Vpc {
+        name: vpc.name.clone(),
+        id: vpc.id.clone(),
+        vni: vpc.vni,
+        interfaces: InterfaceConfigTable::default(),
+        peerings: vec![],
+    }
+}
+
+fn clone_skipping_local_exposes(peering: &Peering) -> Peering {
+    Peering {
+        name: peering.name.clone(),
+        local: VpcManifest {
+            name: peering.local.name.clone(),
+            exposes: vec![],
+        },
+        remote: peering.remote.clone(),
+        remote_id: peering.remote_id.clone(),
+    }
+}
+
+fn cleanup_vpc_table(vpcs: Vec<&Vpc>) -> Result<Vec<Vpc>, ConfigError> {
+    let mut new_set = Vec::new();
+    for vpc in vpcs {
+        let mut tmp_vpc = clone_skipping_peerings(vpc);
+
+        for peering in &vpc.peerings {
+            // "Collapse" prefixes to get rid of exclusion prefixes
+            let collapsed_peering = collapse_prefixes_peering(peering).map_err(|e| match e {
+                ConfigUtilError::SplitPrefixError(prefix) => {
+                    ConfigError::FailureApply(format!("Failed to split prefix: {prefix}"))
+                }
+            })?;
+            tmp_vpc.peerings.push(collapsed_peering);
+        }
+
+        let new_vpc = split_overlaps_in_src_prefixes(&mut tmp_vpc);
+
+        new_set.push(new_vpc);
+    }
+    Ok(new_set)
+}
+
+fn split_overlaps_in_src_prefixes(vpc: &mut Vpc) -> Vpc {
+    let mut new_vpc = clone_skipping_peerings(vpc);
+    while let Some(mut peering) = vpc.peerings.pop() {
+        let mut new_peering = clone_skipping_local_exposes(&peering);
+        while let Some(mut expose) = peering.local.exposes.pop() {
+            let mut new_expose = VpcExpose::default();
+            'next_prefix: while let Some(prefix) = expose.ips.pop_first() {
+                for other_peering in &vpc.peerings {
+                    for other_expose in &other_peering.local.exposes {
+                        for other_prefix in other_expose.ips.iter() {
+                            if prefix.overlaps(other_prefix) && !other_prefix.covers(&prefix) {
+                                expose.ips.extend(split_overlapping(prefix, *other_prefix));
+                                continue 'next_prefix;
+                            }
+                        }
+                    }
+                }
+                for new_peering in &new_vpc.peerings {
+                    for new_expose in &new_peering.local.exposes {
+                        for new_prefix in new_expose.ips.iter() {
+                            if prefix.overlaps(new_prefix) && !new_prefix.covers(&prefix) {
+                                expose.ips.extend(split_overlapping(prefix, *new_prefix));
+                                continue 'next_prefix;
+                            }
+                        }
+                    }
+                }
+                new_expose = new_expose.ip(prefix);
+            }
+            new_peering.local.exposes.push(new_expose);
+        }
+        new_vpc.peerings.push(new_peering);
+    }
+    new_vpc
+}
+
+fn split_overlapping(
+    prefix_to_split: PrefixWithOptionalPorts,
+    mask_prefix: PrefixWithOptionalPorts,
+) -> Vec<PrefixWithOptionalPorts> {
+    debug_assert!(prefix_to_split.overlaps(&mask_prefix) && !mask_prefix.covers(&prefix_to_split));
+    let mut split_prefixes = prefix_to_split.subtract(&mask_prefix);
+    split_prefixes.push(
+        prefix_to_split
+            .intersection(&mask_prefix)
+            // Intersection non-empty given that prefixes overlap
+            .unwrap_or_else(|| unreachable!()),
+    );
+    split_prefixes
 }
