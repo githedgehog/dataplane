6f8bac5fbe907ab6e9591252d298ad8a67afa14c
Daniel Noland <daniel@githedgehog.com>
2025-12-28 19:29:55 +0000

    fix nonsense

--- hardware/src/group/mod.rs
+++ hardware/src/group/mod.rs
@@ -2,21 +2,22 @@
 // Copyright Open Network Fabric Authors
 
 //! # Logical hardware groupings
-//! 
+//!
 //! This module provides types for representing logical groups of hardware
 //! components. Groups are used in hardware topology to represent collections
 //! of related components that don't fit into other specific categories.
-//! 
+//!
 //! Groups can represent:
-//! 
+//!
 //! - NUMA distances
 //! - I/O groups
 //! - Logical partitions
 //! - Custom hardware groupings defined by the system
-//! 
-//! #[cfg(any(test, feature = "scan"))]
-//! #[allow(unused_imports)] // re-export
-//! pub use self::scan::*;
+//!
+
+#[cfg(any(test, feature = "scan"))]
+#[allow(unused_imports)] // re-export
+pub use self::scan::*;
 
 /// Attributes for a logical hardware group.
 ///
