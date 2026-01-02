9e6ec51b904a9241a943d3908ff7471871ea2551
Daniel Noland <daniel@githedgehog.com>
2026-01-02 23:56:21 +0000

    build(nix): intro perftest package
    The perftest package is how we have been testing RDMA traffic.
    
    Signed-off-by: Daniel Noland <daniel@githedgehog.com>

---
+++ nix/pkgs/perftest/default.nix
@@ -0,0 +1,20 @@
+{
+  src,
+  stdenv,
+  rdma-core,
+  autoreconfHook,
+  pciutils,
+  ...
+}:
+stdenv.mkDerivation (final: {
+  pname = "perftest";
+  version = src.version;
+  src = src.outPath;
+  nativeBuildInputs = [
+    autoreconfHook
+  ];
+  buildInputs = [
+    pciutils
+    rdma-core
+  ];
+})
