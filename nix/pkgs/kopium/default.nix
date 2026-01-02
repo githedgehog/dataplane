2cd43ec23076550891c8390d0d2eef568c848ee3
Daniel Noland <daniel@githedgehog.com>
2026-01-02 23:55:44 +0000

    build(nix): intro kopium package
    The kopium package is needed for the generation of rust code from kubernetes CRDs, and must be
    included in the devroot in order to build the dataplane.

---
+++ nix/pkgs/kopium/default.nix
@@ -0,0 +1,12 @@
+{
+  src,
+  rustPlatform,
+  ...
+}:
+rustPlatform.buildRustPackage (final: {
+  pname = "kopium";
+  version = src.version;
+  src = src.outPath;
+  cargoLock.lockFile = "${final.src}/Cargo.lock";
+  doCheck = false;
+})
