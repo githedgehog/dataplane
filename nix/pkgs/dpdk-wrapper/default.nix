dd6ac633667c566880339fccb1aec2d4e525cb94
Daniel Noland <daniel@githedgehog.com>
2026-01-03 00:01:48 +0000

    build(nix): intro dpdk-wrapper package
    This builds all of the wrapper functions for static inline functions
    needed by our DPDK bindings.
    
    Signed-off-by: Daniel Noland <daniel@githedgehog.com>

---
+++ nix/pkgs/dpdk-wrapper/default.nix
@@ -0,0 +1,38 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright Open Network Fabric Authors
+{
+  stdenv,
+  dpdk,
+  libbsd,
+  ...
+}:
+stdenv.mkDerivation {
+  pname = "dpdk-wrapper";
+  version = dpdk.version;
+
+  src = ./src;
+
+  nativeBuildInptus = [
+    dpdk
+    libbsd
+  ];
+
+  outputs = [
+    "dev"
+    "out"
+  ];
+
+  # DPDK marks all experimental apis as deprecated, but we wish to wrap such apis as well. Thus, turn off deprecation
+  # warnings.
+  CFLAGS = [ "-Wno-deprecated-declarations" ];
+
+  buildPhase = ''
+    set euxo pipefail
+    mkdir -p $dev/include $out/lib
+    $CC $CFLAGS -I${dpdk}/include -I${libbsd.dev}/include -c $src/dpdk_wrapper.c -o wrapper.o;
+    $AR rcs $out/lib/libdpdk_wrapper.a wrapper.o;
+    $RANLIB $out/lib/libdpdk_wrapper.a;
+    cp $src/*.h $dev/include
+  '';
+
+}
