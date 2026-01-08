a0b33c05e7879db433c1e568142e6673f1f654fc
Daniel Noland <daniel@githedgehog.com>
2026-01-08 01:19:23 +0000

    build(nix): intro llvm overlay
    Signed-off-by: Daniel Noland <daniel@githedgehog.com>

--- nix/overlays/default.nix
+++ nix/overlays/default.nix
@@ -1,7 +1,10 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright Open Network Fabric Authors
 inputs@{
+  platform,
+  profile,
   ...
 }:
 {
+  llvm = import ./llvm.nix inputs; # requires rust
 }
