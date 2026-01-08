a4eeaa0c31135c209c5601a4b2a4d55c52eb5d96
Daniel Noland <daniel@githedgehog.com>
2026-01-08 04:13:53 +0000

    build(nix): intro dataplane-dev overlay
    The dataplane-dev overlay contains build instructions for all of the
    build tools needed by the dataplane.
    
    Signed-off-by: Daniel Noland <daniel@githedgehog.com>

---
+++ nix/overlays/dataplane-dev.nix
@@ -0,0 +1,37 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright Open Network Fabric Authors
+{
+  sources,
+  ...
+}:
+final: prev:
+let
+  override-packages = {
+    stdenv = final.llvmPackages.stdenv;
+    rustPlatform = final.rustPlatform'-dev;
+  };
+in
+{
+  kopium = import ../pkgs/kopium (
+    override-packages
+    // {
+      src = sources.kopium;
+    }
+  );
+  cargo-bolero = prev.cargo-bolero.override { inherit (override-packages) rustPlatform; };
+  cargo-deny = prev.cargo-deny.override { inherit (override-packages) rustPlatform; };
+  cargo-llvm-cov = prev.cargo-llvm-cov.override override-packages;
+  cargo-nextest = prev.cargo-nextest.override override-packages;
+  just = prev.just.override override-packages;
+  npins = prev.npins.override { inherit (override-packages) rustPlatform; };
+  gateway-crd =
+    let
+      p = "config/crd/bases/gwint.githedgehog.com_gatewayagents.yaml";
+    in
+    final.writeTextFile {
+      name = "gateway-crd";
+      text = builtins.readFile "${sources.gateway}/${p}";
+      executable = false;
+      destination = "/src/gateway/${p}";
+    };
+}
--- nix/overlays/default.nix
+++ nix/overlays/default.nix
@@ -1,10 +1,12 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright Open Network Fabric Authors
 inputs@{
+  sources,
   platform,
   profile,
   ...
 }:
 {
   llvm = import ./llvm.nix inputs; # requires rust
+  dataplane-dev = import ./dataplane-dev.nix inputs; # requires llvm
 }
