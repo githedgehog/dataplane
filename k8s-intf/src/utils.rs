7ce841ca65728341783e19198e3a2fb616813504
Fredi Raspall <fredi@githedgehog.com>
2026-01-08 12:20:41 +0100

    feat(k8s-intf): extend utils to save json/yaml
    Add utility functions to save objects in yaml and json.
    
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

--- k8s-intf/src/utils.rs
+++ k8s-intf/src/utils.rs
@@ -4,6 +4,7 @@
 //! Utils to build the gateway CRD `GatewayAgentSpec` from JSON / YAML text files.
 
 use crate::gateway_agent_crd::GatewayAgentSpec;
+use serde::Serialize;
 use serde_yaml_ng;
 use std::fs;
 use std::path::Path;
@@ -42,4 +43,44 @@ pub fn load_crd_from_file(path: &str) -> Result<GatewayAgentSpec, String> {
         Some(ext) => Err(format!("Unsupported file extension {}", ext.display())),
         None => Err("Missing file extension".to_string()),
     }
+}
+
+/// Serialize an object as JSON and store it in the file at path `path`.
+/// This function will create the file if it does not exist.
+///
+/// # Errors
+/// This function may fail if the object cannot be serialized or if the
+/// full directory path does not exist.
+pub fn save_as_json<T: Serialize>(path: &str, object: &T) -> Result<(), String> {
+    let json =
+        serde_json::to_string(object).map_err(|e| format!("Failed to serialize as JSON: {e}"))?;
+    fs::write(path, json).map_err(|e| format!("Failed to write json file at {path}: {e}"))
+}
+
+/// Serialize an object as YAML and store it in the file at path `path`.
+/// This function will create the file if it does not exist.
+///
+/// # Errors
+/// This function may fail if the object cannot be serialized or if the
+/// full directory path does not exist.
+pub fn save_as_yaml<T: Serialize>(path: &str, object: &T) -> Result<(), String> {
+    let yaml = serde_yaml_ng::to_string(object)
+        .map_err(|e| format!("Failed to serialize as YAML: {e}"))?;
+    fs::write(path, yaml).map_err(|e| format!("Failed to write YAML file at {path}: {e}"))
+}
+
+/// Serialize an object as JSON and YAML and store both in separate files
+/// with extensions .json and .yaml added to the indicated filename in `path`.
+/// The files will be created if they do not exist.
+///
+/// # Errors
+/// This function may fail if the object cannot be serialized or if the
+/// full directory path does not exist.
+pub fn save<T: Serialize>(path: &str, object: &T) -> Result<(), String> {
+    let p = Path::new(path);
+    let yaml_file = p.with_added_extension("yaml");
+    let json_file = p.with_added_extension("json");
+    save_as_yaml(yaml_file.to_str().unwrap_or_else(|| unreachable!()), object)?;
+    save_as_json(json_file.to_str().unwrap_or_else(|| unreachable!()), object)?;
+    Ok(())
 }
