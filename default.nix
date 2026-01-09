5da0b495ba711f810e6157a48e49e11d883e76ad
Daniel Noland <daniel@githedgehog.com>
2026-01-09 21:24:44 +0000

    build(nix): introduce split-str helper

--- default.nix
+++ default.nix
@@ -4,6 +4,13 @@
 }:
 let
   sources = import ./npins;
+  # helper method to work around nix's contrived builtin string split function.
+  split-str =
+    split-on: string:
+    if string == "" then
+      [ ]
+    else
+      builtins.filter (elm: builtins.isString elm) (builtins.split split-on string);
 in
 {
 
