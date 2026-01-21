githedgehog/dataplane

commit d081eb60135729643c7cd2d942204465ebf018e3

feat(flow-filter): Add support for "default" destination VpcExpose
Make sure that we account for "default" VpcExpose when we retrieve the
destination VPC discriminant for the packet.

Internally, we don't add a prefix for the default destination into the
tables, because we'd have to handle overlap to some extent. Instead, we
add a per-source-prefix value to use as a fallback when no other
destination prefix match.

At the moment, the code assumes that each VPC accepts at most one
"default" destination.

Signed-off-by: Quentin Monnet <qmo@qmon.net>

flow-filter/src/lib.rs
@@ -76,14 +76,13 @@ impl FlowFilter {
         });
         let log_str = format_packet_addrs_ports(&src_ip, &dst_ip, ports);
 
-        let Some(dst_vpcd) = tablesr.lookup(src_vpcd, &src_ip, &dst_ip, ports) else {
+        if let Some(dst_vpcd) = tablesr.lookup(src_vpcd, &src_ip, &dst_ip, ports) {
+            debug!("{nfi}: Flow allowed: {log_str}, setting packet dst_vpcd to {dst_vpcd}");
+            packet.meta.dst_vpcd = Some(dst_vpcd);
+        } else {
             debug!("{nfi}: Flow not allowed, dropping packet: {log_str}");
             packet.done(DoneReason::Filtered);
-            return;
-        };
-
-        debug!("{nfi}: Flow allowed: {log_str}, setting packet dst_vpcd to {dst_vpcd}");
-        packet.meta.dst_vpcd = Some(dst_vpcd);
+        }
     }
 }
 
@@ -123,14 +122,24 @@ mod tests {
     use super::*;
     use crate::filter_rw::FlowFilterTableWriter;
     use crate::tables::OptionalPortRange;
-    use lpm::prefix::Prefix;
+    use config::external::overlay::Overlay;
+    use config::external::overlay::vpc::{Vpc, VpcTable};
+    use config::external::overlay::vpcpeering::{
+        VpcExpose, VpcManifest, VpcPeering, VpcPeeringTable,
+    };
+    use lpm::prefix::{PortRange, Prefix};
     use net::buffer::TestBuffer;
     use net::headers::{Net, TryHeadersMut, TryIpMut};
     use net::ipv4::addr::UnicastIpv4Addr;
     use net::ipv6::addr::UnicastIpv6Addr;
-    use net::packet::test_utils::{build_test_ipv4_packet, build_test_ipv6_packet};
+    use net::packet::test_utils::{
+        IcmpEchoDirection, build_test_icmp4_echo, build_test_ipv4_packet, build_test_ipv6_packet,
+    };
     use net::packet::{DoneReason, Packet, VpcDiscriminant};
     use net::vxlan::Vni;
+    use std::net::Ipv4Addr;
+    use std::str::FromStr;
+    use tracing_test::traced_test;
 
     fn vpcd(vni: u32) -> VpcDiscriminant {
         VpcDiscriminant::VNI(Vni::new_checked(vni).unwrap())
@@ -171,6 +180,17 @@ mod tests {
         };
         set_src_addr(&mut packet, src_addr);
         set_dst_addr(&mut packet, dst_addr);
+        packet.meta.src_vpcd = src_vpcd.map(VpcDiscriminant::VNI);
+        packet
+    }
+
+    fn create_test_icmp_v4_packet(
+        src_vpcd: Option<Vni>,
+        src_addr: Ipv4Addr,
+        dst_addr: Ipv4Addr,
+    ) -> Packet<TestBuffer> {
+        let mut packet =
+            build_test_icmp4_echo(src_addr, dst_addr, 1, IcmpEchoDirection::Request).unwrap();
         packet.meta.src_vpcd = src_vpcd.map(VpcDiscriminant::VNI);
         packet
     }
@@ -350,6 +370,228 @@ mod tests {
         assert_eq!(packets.len(), 1);
         assert!(!packets[0].is_done());
         assert_eq!(packets[0].meta.dst_vpcd, Some(dst_vpcd));
+    }
+
+    #[test]
+    fn test_flow_filter_packet_icmp_allowed() {
+        // Setup table
+        let mut table = FlowFilterTable::new();
+        let src_vpcd = vpcd(100);
+        let dst_vpcd = vpcd(200);
+
+        table
+            .insert(
+                src_vpcd,
+                dst_vpcd,
+                Prefix::from("10.0.0.0/24"),
+                OptionalPortRange::NoPortRangeMeansAllPorts,
+                Prefix::from("20.0.0.0/24"),
+                OptionalPortRange::NoPortRangeMeansAllPorts,
+            )
+            .unwrap();
+
+        let mut writer = FlowFilterTableWriter::new();
+        writer.update_flow_filter_table(table);
+
+        let mut flow_filter = FlowFilter::new("test-filter", writer.get_reader());
+
+        // Create test packet
+        let packet = create_test_icmp_v4_packet(
+            Some(Vni::new_checked(100).unwrap()),
+            Ipv4Addr::from_str("10.0.0.5").unwrap(),
+            Ipv4Addr::from_str("20.0.0.10").unwrap(),
+        );
+
+        let packets = flow_filter
+            .process([packet].into_iter())
+            .collect::<Vec<_>>();
+
+        assert_eq!(packets.len(), 1);
+        assert!(!packets[0].is_done());
+        assert_eq!(packets[0].meta.dst_vpcd, Some(dst_vpcd));
+    }
+
+    #[test]
+    fn test_flow_filter_packet_icmp_filtered() {
+        // Setup table
+        let mut table = FlowFilterTable::new();
+        let src_vpcd = vpcd(100);
+        let dst_vpcd = vpcd(200);
+
+        table
+            .insert(
+                src_vpcd,
+                dst_vpcd,
+                Prefix::from("10.0.0.0/24"),
+                OptionalPortRange::Some(PortRange::new(1025, 1999).unwrap()),
+                Prefix::from("20.0.0.0/24"),
+                OptionalPortRange::NoPortRangeMeansAllPorts,
+            )
+            .unwrap();
+
+        let mut writer = FlowFilterTableWriter::new();
+        writer.update_flow_filter_table(table);
+
+        let mut flow_filter = FlowFilter::new("test-filter", writer.get_reader());
+
+        // Create test packet
+        let packet = create_test_icmp_v4_packet(
+            Some(Vni::new_checked(100).unwrap()),
+            Ipv4Addr::from_str("10.0.0.5").unwrap(),
+            Ipv4Addr::from_str("20.0.0.10").unwrap(),
+        );
+
+        let packets = flow_filter
+            .process([packet].into_iter())
+            .collect::<Vec<_>>();
+
+        assert_eq!(packets.len(), 1);
+        assert!(packets[0].is_done());
+        assert_eq!(packets[0].meta.dst_vpcd, None);
+    }
+
+    #[traced_test]
+    #[test]
+    fn test_flow_filter_table_from_overlay() {
+        let vni1 = Vni::new_checked(100).unwrap();
+        let vni2 = Vni::new_checked(200).unwrap();
+        let vni3 = Vni::new_checked(300).unwrap();
+
+        let mut vpc_table = VpcTable::new();
+        vpc_table
+            .add(Vpc::new("vpc1", "VPC01", vni1.as_u32()).unwrap())
+            .unwrap();
+        vpc_table
+            .add(Vpc::new("vpc2", "VPC02", vni2.as_u32()).unwrap())
+            .unwrap();
+        vpc_table
+            .add(Vpc::new("vpc3", "VPC03", vni3.as_u32()).unwrap())
+            .unwrap();
+
+        let mut peering_table = VpcPeeringTable::new();
+        peering_table
+            .add(VpcPeering::new(
+                "vpc1-to-vpc2",
+                VpcManifest {
+                    name: "vpc1".to_string(),
+                    exposes: vec![VpcExpose::empty().ip("1.0.0.0/24".into())],
+                },
+                VpcManifest {
+                    name: "vpc2".to_string(),
+                    exposes: vec![
+                        VpcExpose::empty().ip("5.0.0.0/24".into()),
+                        VpcExpose::empty().set_default(),
+                    ],
+                },
+                None,
+            ))
+            .unwrap();
+
+        peering_table
+            .add(VpcPeering::new(
+                "vpc1-to-vpc3",
+                VpcManifest {
+                    name: "vpc1".to_string(),
+                    exposes: vec![
+                        VpcExpose::empty().ip("1.0.0.0/24".into()),
+                        VpcExpose::empty().ip("2.0.0.0/24".into()),
+                    ],
+                },
+                VpcManifest {
+                    name: "vpc3".to_string(),
+                    exposes: vec![VpcExpose::empty().ip("6.0.0.0/24".into())],
+                },
+                None,
+            ))
+            .unwrap();
+
+        let mut overlay = Overlay::new(vpc_table, peering_table);
+        // Validation is necessary to build overlay.vpc_table's peerings from peering_table
+        overlay.validate().unwrap();
+
+        let table = FlowFilterTable::build_from_overlay(&overlay).unwrap();
+
+        let mut writer = FlowFilterTableWriter::new();
+        writer.update_flow_filter_table(table);
+
+        let mut flow_filter = FlowFilter::new("test-filter", writer.get_reader());
+
+        // Test with packets
+
+        // VPC-1 -> VPC-2 using prefix
+        let packet = create_test_packet(
+            Some(Vni::new_checked(100).unwrap()),
+            "1.0.0.5".parse().unwrap(),
+            "5.0.0.10".parse().unwrap(),
+        );
+
+        let packets = flow_filter
+            .process([packet].into_iter())
+            .collect::<Vec<_>>();
+
+        assert_eq!(packets.len(), 1);
+        assert!(!packets[0].is_done(), "{:?}", packets[0].get_done());
+        assert_eq!(packets[0].meta.dst_vpcd, Some(vpcd(vni2.into())));
+
+        // VPC-1 -> VPC-2 using default range
+        let packet = create_test_packet(
+            Some(Vni::new_checked(100).unwrap()),
+            "1.0.0.6".parse().unwrap(),
+            "17.34.51.68".parse().unwrap(),
+        );
+
+        let packets = flow_filter
+            .process([packet].into_iter())
+            .collect::<Vec<_>>();
+
+        assert_eq!(packets.len(), 1);
+        assert!(!packets[0].is_done(), "{:?}", packets[0].get_done());
+        assert_eq!(packets[0].meta.dst_vpcd, Some(vpcd(vni2.into())));
+
+        // VPC-1 -> VPC-3, using source prefix overlapping with VPC-1 <-> VPC-2 peering
+        let packet = create_test_packet(
+            Some(Vni::new_checked(100).unwrap()),
+            "1.0.0.7".parse().unwrap(),
+            "6.0.0.8".parse().unwrap(),
+        );
+
+        let packets = flow_filter
+            .process([packet].into_iter())
+            .collect::<Vec<_>>();
+
+        assert_eq!(packets.len(), 1);
+        assert!(!packets[0].is_done(), "{:?}", packets[0].get_done());
+        assert_eq!(packets[0].meta.dst_vpcd, Some(vpcd(vni3.into())));
+
+        // VPC-1 -> VPC-3, using the other source prefix
+        let packet = create_test_packet(
+            Some(Vni::new_checked(100).unwrap()),
+            "2.0.0.24".parse().unwrap(),
+            "6.0.0.8".parse().unwrap(),
+        );
+
+        let packets = flow_filter
+            .process([packet].into_iter())
+            .collect::<Vec<_>>();
+
+        assert_eq!(packets.len(), 1);
+        assert!(!packets[0].is_done(), "{:?}", packets[0].get_done());
+        assert_eq!(packets[0].meta.dst_vpcd, Some(vpcd(vni3.into())));
+
+        // Invalid: source from VPC-1 <-> VPC-3 peering, but invalid destination
+        let packet = create_test_packet(
+            Some(Vni::new_checked(100).unwrap()),
+            "2.0.0.24".parse().unwrap(),
+            "25.50.100.200".parse().unwrap(),
+        );
+
+        let packets = flow_filter
+            .process([packet].into_iter())
+            .collect::<Vec<_>>();
+
+        assert_eq!(packets.len(), 1);
+        assert!(packets[0].is_done());
+        assert_eq!(packets[0].meta.dst_vpcd, None);
     }
 
     #[test]

flow-filter/src/setup.rs
@@ -37,23 +37,30 @@ impl FlowFilterTable {
         let local_vpcd = VpcDiscriminant::VNI(vpc.vni);
         let dst_vpcd = VpcDiscriminant::VNI(overlay.vpc_table.get_remote_vni(peering));
         let local_prefixes = peering.local.exposes.iter().flat_map(|expose| &expose.ips);
-        let remote_prefixes = peering
-            .remote
-            .exposes
-            .iter()
-            .flat_map(|expose| expose.public_ips());
 
-        // For each local prefix, add one entry for each associated remote prefix
-        for local_prefix in local_prefixes {
-            for remote_prefix in remote_prefixes.clone() {
-                self.insert(
-                    local_vpcd,
-                    dst_vpcd,
-                    local_prefix.prefix(),
-                    local_prefix.ports().into(),
-                    remote_prefix.prefix(),
-                    remote_prefix.ports().into(),
-                )?;
+        for remote in &peering.remote.exposes {
+            if remote.default {
+                for local_prefix in local_prefixes.clone() {
+                    self.insert_default_remote(
+                        dst_vpcd,
+                        local_vpcd,
+                        local_prefix.prefix(),
+                        local_prefix.ports().into(),
+                    )?;
+                }
+            } else {
+                for local_prefix in local_prefixes.clone() {
+                    for remote_prefix in remote.public_ips() {
+                        self.insert(
+                            local_vpcd,
+                            dst_vpcd,
+                            local_prefix.prefix(),
+                            local_prefix.ports().into(),
+                            remote_prefix.prefix(),
+                            remote_prefix.ports().into(),
+                        )?;
+                    }
+                }
             }
         }
         Ok(())

commit 270aecd47de5dc7cfeb4b4daabf68b393824ce06

fix(flow-filter): Account for "default" source when filtering packets
We recently added support for "default" expose blocks used as a fallback
destination for packets from the VPC when the destination IP matches no
other exposed prefixes. From the "default" block we deduce information
about the destination VPC, and about whether the packet has a valid
destination. But we forgot to use it for the reverse check: when a
packet is emitted from a VPC, from behind a "default" block, its source
address should always be considered as valid. This is not the case! So
if we have VPC A and B peered together, and VPC B exposing a "default"
block, VPC A can send packets to VPC B through this block, but all
replies from VPC B are dropped.

This commit fixes the issue by adding a "default source" connection
information object to the context for the flow-filter stage, in the
per-source-VPC-discriminant table. This information is used to validate
the destination and retrieve the destination VPC when a packet is
emitted from behind a "default" expose block.

Also add unit tests to validate the behaviour.

Fixes: d081eb601357 ("feat(flow-filter): Add support for "default" destination VpcExpose")
Signed-off-by: Quentin Monnet <qmo@qmon.net>

flow-filter/src/lib.rs
@@ -595,6 +595,122 @@ mod tests {
         assert_eq!(packets[0].meta().dst_vpcd, None);
     }
 
+    #[traced_test]
+    #[test]
+    fn test_flow_filter_table_check_send_from_default() {
+        let vni1 = Vni::new_checked(100).unwrap();
+        let vni2 = Vni::new_checked(200).unwrap();
+
+        let mut vpc_table = VpcTable::new();
+        vpc_table
+            .add(Vpc::new("vpc1", "VPC01", vni1.as_u32()).unwrap())
+            .unwrap();
+        vpc_table
+            .add(Vpc::new("vpc2", "VPC02", vni2.as_u32()).unwrap())
+            .unwrap();
+
+        let mut peering_table = VpcPeeringTable::new();
+        peering_table
+            .add(VpcPeering::new(
+                "vpc1-to-vpc2",
+                VpcManifest {
+                    name: "vpc1".to_string(),
+                    exposes: vec![VpcExpose::empty().set_default()],
+                },
+                VpcManifest {
+                    name: "vpc2".to_string(),
+                    exposes: vec![VpcExpose::empty().ip("5.0.0.0/24".into())],
+                },
+                None,
+            ))
+            .unwrap();
+
+        let mut overlay = Overlay::new(vpc_table, peering_table);
+        // Validation is necessary to build overlay.vpc_table's peerings from peering_table
+        overlay.validate().unwrap();
+
+        let table = FlowFilterTable::build_from_overlay(&overlay).unwrap();
+
+        let mut writer = FlowFilterTableWriter::new();
+        writer.update_flow_filter_table(table);
+
+        let mut flow_filter = FlowFilter::new("test-filter", writer.get_reader());
+
+        // Test with a packet
+
+        let packet = create_test_packet(
+            Some(vni1),
+            "99.99.99.99".parse().unwrap(), // From "default" expose, use any address
+            "5.0.0.8".parse().unwrap(),
+        );
+
+        let packets = flow_filter
+            .process([packet].into_iter())
+            .collect::<Vec<_>>();
+
+        assert_eq!(packets.len(), 1);
+        assert!(!packets[0].is_done(), "{:?}", packets[0].get_done());
+        assert_eq!(packets[0].meta().dst_vpcd, Some(vni2.into()));
+    }
+
+    #[traced_test]
+    #[test]
+    fn test_flow_filter_table_check_default_to_default() {
+        let vni1 = Vni::new_checked(100).unwrap();
+        let vni2 = Vni::new_checked(200).unwrap();
+
+        let mut vpc_table = VpcTable::new();
+        vpc_table
+            .add(Vpc::new("vpc1", "VPC01", vni1.as_u32()).unwrap())
+            .unwrap();
+        vpc_table
+            .add(Vpc::new("vpc2", "VPC02", vni2.as_u32()).unwrap())
+            .unwrap();
+
+        let mut peering_table = VpcPeeringTable::new();
+        peering_table
+            .add(VpcPeering::new(
+                "vpc1-to-vpc2",
+                VpcManifest {
+                    name: "vpc1".to_string(),
+                    exposes: vec![VpcExpose::empty().set_default()],
+                },
+                VpcManifest {
+                    name: "vpc2".to_string(),
+                    exposes: vec![VpcExpose::empty().set_default()],
+                },
+                None,
+            ))
+            .unwrap();
+
+        let mut overlay = Overlay::new(vpc_table, peering_table);
+        // Validation is necessary to build overlay.vpc_table's peerings from peering_table
+        overlay.validate().unwrap();
+
+        let table = FlowFilterTable::build_from_overlay(&overlay).unwrap();
+
+        let mut writer = FlowFilterTableWriter::new();
+        writer.update_flow_filter_table(table);
+
+        let mut flow_filter = FlowFilter::new("test-filter", writer.get_reader());
+
+        // Test with packets
+
+        let packet = create_test_packet(
+            Some(vni1),
+            "99.99.99.99".parse().unwrap(),
+            "77.77.77.77".parse().unwrap(),
+        );
+
+        let packets = flow_filter
+            .process([packet].into_iter())
+            .collect::<Vec<_>>();
+
+        assert_eq!(packets.len(), 1);
+        assert!(!packets[0].is_done(), "{:?}", packets[0].get_done());
+        assert_eq!(packets[0].meta().dst_vpcd, Some(vni2.into()));
+    }
+
     #[test]
     fn test_flow_filter_batch_processing() {
         // Setup table

flow-filter/src/setup.rs
@@ -40,27 +40,46 @@ impl FlowFilterTable {
         for remote_expose in &peering.remote.exposes {
             if remote_expose.default {
                 for local_expose in &peering.local.exposes {
-                    for local_prefix in &local_expose.ips {
-                        self.insert_default_remote(
-                            dst_vpcd,
-                            local_vpcd,
-                            local_prefix.prefix(),
-                            local_prefix.ports(),
-                        )?;
-                    }
-                }
-            } else {
-                for local_expose in &peering.local.exposes {
-                    for local_prefix in &local_expose.ips {
-                        for remote_prefix in remote_expose.public_ips() {
-                            self.insert(
+                    if local_expose.default {
+                        // Both the local and remote expose are default exposes
+                        self.insert_default_source_to_default_remote(local_vpcd, dst_vpcd)?;
+                    } else {
+                        // Only the remote expose is a default expose
+                        for local_prefix in &local_expose.ips {
+                            self.insert_default_remote(
                                 local_vpcd,
                                 dst_vpcd,
                                 local_prefix.prefix(),
                                 local_prefix.ports(),
+                            )?;
+                        }
+                    }
+                }
+            } else {
+                for local_expose in &peering.local.exposes {
+                    if local_expose.default {
+                        // Only the local expose is a default expose
+                        for remote_prefix in remote_expose.public_ips() {
+                            self.insert_default_source(
+                                local_vpcd,
+                                dst_vpcd,
                                 remote_prefix.prefix(),
                                 remote_prefix.ports(),
                             )?;
+                        }
+                    } else {
+                        // No default expose
+                        for local_prefix in &local_expose.ips {
+                            for remote_prefix in remote_expose.public_ips() {
+                                self.insert(
+                                    local_vpcd,
+                                    dst_vpcd,
+                                    local_prefix.prefix(),
+                                    local_prefix.ports(),
+                                    remote_prefix.prefix(),
+                                    remote_prefix.ports(),
+                                )?;
+                            }
                         }
                     }
                 }

flow-filter/src/tables.rs
@@ -97,8 +97,12 @@ impl FlowFilterTable {
         let table = self.get_table(src_vpcd)?;
 
         let (src_port, dst_port) = ports.unzip();
-        // Look for valid connections information in the table that matches the source address and port
-        let (_, src_connection_data) = table.lookup(src_addr, src_port)?;
+        // Look for valid connections information in the table that matches the source address and port.
+        // If nothing matches, use the default source entry, if any.
+        let src_connection_data = table
+            .lookup(src_addr, src_port)
+            .map(|(_, data)| data)
+            .or(table.default_source_opt.as_ref())?;
         debug!("Found src_connection_data: {src_connection_data:?}");
 
         // We have a src_connection_data object for our source VPC and source IP, and source port
@@ -157,8 +161,8 @@ impl FlowFilterTable {
 
     pub(crate) fn insert_default_remote(
         &mut self,
-        dst_vpcd: VpcDiscriminant,
         src_vpcd: VpcDiscriminant,
+        dst_vpcd: VpcDiscriminant,
         src_prefix: Prefix,
         src_port_range: Option<PortRange>,
     ) -> Result<(), ConfigError> {
@@ -171,18 +175,56 @@ impl FlowFilterTable {
         }
         Ok(())
     }
+
+    pub(crate) fn insert_default_source(
+        &mut self,
+        src_vpcd: VpcDiscriminant,
+        dst_vpcd: VpcDiscriminant,
+        dst_prefix: Prefix,
+        dst_port_range: Option<PortRange>,
+    ) -> Result<(), ConfigError> {
+        if let Some(table) = self.get_table_mut(src_vpcd) {
+            table.update_default_source(dst_vpcd, dst_prefix, dst_port_range)?;
+        } else {
+            let mut table = VpcConnectionsTable::new();
+            table.create_default_source(dst_vpcd, dst_prefix, dst_port_range)?;
+            self.insert_table(src_vpcd, table);
+        }
+        Ok(())
+    }
+
+    pub(crate) fn insert_default_source_to_default_remote(
+        &mut self,
+        src_vpcd: VpcDiscriminant,
+        dst_vpcd: VpcDiscriminant,
+    ) -> Result<(), ConfigError> {
+        if let Some(table) = self.get_table_mut(src_vpcd) {
+            table.update_default_source_to_default_remote(dst_vpcd)?;
+        } else {
+            let mut table = VpcConnectionsTable::new();
+            table.create_default_source_to_default_remote(dst_vpcd)?;
+            self.insert_table(src_vpcd, table);
+        }
+        Ok(())
+    }
 }
 
 #[derive(Debug, Clone)]
-struct VpcConnectionsTable(IpPortPrefixTrie<SrcConnectionData>);
+struct VpcConnectionsTable {
+    trie: IpPortPrefixTrie<SrcConnectionData>,
+    default_source_opt: Option<SrcConnectionData>,
+}
 
 impl VpcConnectionsTable {
     fn new() -> Self {
-        Self(IpPortPrefixTrie::new())
+        Self {
+            trie: IpPortPrefixTrie::new(),
+            default_source_opt: None,
+        }
     }
 
     fn lookup(&self, addr: &IpAddr, port: Option<u16>) -> Option<(Prefix, &SrcConnectionData)> {
-        self.0.lookup(addr, port)
+        self.trie.lookup(addr, port)
     }
 
     fn insert(
@@ -193,12 +235,12 @@ impl VpcConnectionsTable {
         dst_prefix: Prefix,
         dst_port_range: Option<PortRange>,
     ) -> Result<(), ConfigError> {
-        if let Some(value) = self.0.get_mut(src_prefix) {
+        if let Some(value) = self.trie.get_mut(src_prefix) {
             value.update(src_port_range, dst_vpcd, dst_prefix, dst_port_range)?;
         } else {
             let value =
                 SrcConnectionData::new(src_port_range, dst_vpcd, dst_prefix, dst_port_range);
-            self.0.insert(src_prefix, value);
+            self.trie.insert(src_prefix, value);
         }
         Ok(())
     }
@@ -209,13 +251,73 @@ impl VpcConnectionsTable {
         src_prefix: Prefix,
         src_port_range: Option<PortRange>,
     ) -> Result<(), ConfigError> {
-        if let Some(value) = self.0.get_mut(src_prefix) {
-            value.update_for_default(src_port_range, dst_vpcd)?;
+        if let Some(value) = self.trie.get_mut(src_prefix) {
+            value.update_for_default(dst_vpcd, src_port_range)?;
         } else {
             let value = SrcConnectionData::new_for_default_remote(src_port_range, dst_vpcd);
-            self.0.insert(src_prefix, value);
+            self.trie.insert(src_prefix, value);
         }
         Ok(())
+    }
+
+    fn create_default_source(
+        &mut self,
+        dst_vpcd: VpcDiscriminant,
+        dst_prefix: Prefix,
+        dst_port_range: Option<PortRange>,
+    ) -> Result<(), ConfigError> {
+        if self.default_source_opt.is_some() {
+            return Err(ConfigError::InternalFailure(
+                "Trying to override existing default source".to_string(),
+            ));
+        } else {
+            self.default_source_opt = Some(SrcConnectionData::AllPorts(DstConnectionData::new(
+                dst_vpcd,
+                dst_prefix,
+                dst_port_range,
+            )))
+        }
+        Ok(())
+    }
+
+    fn create_default_source_to_default_remote(
+        &mut self,
+        dst_vpcd: VpcDiscriminant,
+    ) -> Result<(), ConfigError> {
+        if self.default_source_opt.is_some() {
+            return Err(ConfigError::InternalFailure(
+                "Trying to override existing default source".to_string(),
+            ));
+        } else {
+            self.default_source_opt = Some(SrcConnectionData::AllPorts(
+                DstConnectionData::new_for_default_remote(dst_vpcd),
+            ))
+        }
+        Ok(())
+    }
+
+    fn update_default_source(
+        &mut self,
+        dst_vpcd: VpcDiscriminant,
+        dst_prefix: Prefix,
+        dst_port_range: Option<PortRange>,
+    ) -> Result<(), ConfigError> {
+        if let Some(src_connection_data) = &mut self.default_source_opt {
+            src_connection_data.update(None, dst_vpcd, dst_prefix, dst_port_range)
+        } else {
+            self.create_default_source(dst_vpcd, dst_prefix, dst_port_range)
+        }
+    }
+
+    fn update_default_source_to_default_remote(
+        &mut self,
+        dst_vpcd: VpcDiscriminant,
+    ) -> Result<(), ConfigError> {
+        if let Some(src_connection_data) = &mut self.default_source_opt {
+            src_connection_data.update_for_default(dst_vpcd, None)
+        } else {
+            self.create_default_source_to_default_remote(dst_vpcd)
+        }
     }
 }
 
@@ -291,8 +393,8 @@ impl SrcConnectionData {
 
     fn update_for_default(
         &mut self,
-        src_port_range: Option<PortRange>,
         dst_vpcd: VpcDiscriminant,
+        src_port_range: Option<PortRange>,
     ) -> Result<(), ConfigError> {
         self.get_dst_connection_data_mut(src_port_range)?
             .update_for_default(dst_vpcd)