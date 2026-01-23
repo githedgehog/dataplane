3b491d1af5115af96eaaf0bea778378abda85241
Quentin Monnet <qmo@qmon.net>
2026-01-23 10:34:52 +0000

    feat(nat): Add debug_assert!() to ensure packet is overlay
    We want to make sure that all packets we NAT are overlay. At the moment
    we only NAT is the packet is marked as requiring it, and the requirement
    flag is set from the flow-filter stage that only processes overlay
    packets. Add a debug assertion to make sure we catch any change in this
    behaviour.
    
    Suggested-by: Fredi Raspall <fredi@githedgehog.com>
    Signed-off-by: Quentin Monnet <qmo@qmon.net>

--- nat/src/stateless/mod.rs
+++ nat/src/stateless/mod.rs
@@ -398,10 +398,12 @@ impl<Buf: PacketBufferMut> NetworkFunction<Buf> for StatelessNat {
                 && packet.meta().requires_stateless_nat()
                 && !packet.meta().is_natted()
             {
-                // fixme: ideally, we'd `enter` once for the whole batch. However,
-                // this requires boxing the closures, which may be worse than
-                // calling `enter` per packet? ... if not uglier
-                // (same thing for StatefulNat)
+                // Packet should never be marked for NAT and reach this point if it is not overlay
+                debug_assert!(packet.meta().is_overlay());
+
+                // FIXME: Ideally, we'd `enter` once for the whole batch. However, this requires
+                // boxing the closures, which may be worse than calling `enter` per packet? ... if
+                // not uglier (same thing for StatefulNat)
                 if let Some(tablesr) = &self.tablesr.enter() {
                     self.process_packet(tablesr, &mut packet);
                 } else {
