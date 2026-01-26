ef062b4b2fb834114c508a0de3aeb2b35bf8bad7
Daniel Noland <daniel@githedgehog.com>
2026-01-26 15:35:33 +0000

    build(nix): shell.nix support

--- shell.nix
+++ shell.nix
@@ -1,17 +1,11 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright Open Network Fabric Authors
 {
-  pkgs ? import <nixpkgs> { },
-}:
-(pkgs.buildFHSEnv {
-  name = "dataplane-shell";
-  targetPkgs =
-    pkgs:
-    (with pkgs; [
-      # dev tools
-      bash
-      direnv
-      just
-      nil
-      nixd
-      wget
-    ]);
-}).env
+  platform ? "x86-64-v3",
+  libc ? "gnu",
+  profile ? "debug",
+  instrumentation ? "none",
+  sanitize ? "",
+  tag ? "latest",
+}@inputs:
+(import ./default.nix inputs).devenv
