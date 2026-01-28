1d22d6c6d719076f261f475f938f96920903189a
Fredi Raspall <fredi@githedgehog.com>
2026-01-28 11:36:11 +0100

    feat(config): simplify impl of Ord for GwGroupMember
    Simplify the implementation of Ord for GwGroupMember, given that
    no two group members can have the same name.
    
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

--- config/src/external/gwgroup.rs
+++ config/src/external/gwgroup.rs
@@ -32,18 +32,13 @@ impl GwGroupMember {
 impl Ord for GwGroupMember {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         let ord_prio = self.priority.cmp(&other.priority);
-        let ord_ip = self.ipaddress.cmp(&other.ipaddress);
-        let ord_name = self.name.cmp(&other.name);
-
-        if ord_prio == Ordering::Equal {
-            if ord_ip == Ordering::Equal {
-                ord_name
-            } else {
-                ord_ip
-            }
+        let ord = if ord_prio == Ordering::Equal {
+            self.ipaddress.cmp(&other.ipaddress)
         } else {
             ord_prio
-        }
+        };
+        debug_assert_ne!(ord, Ordering::Equal);
+        ord
     }
 }
 impl PartialOrd for GwGroupMember {
@@ -234,6 +229,29 @@ mod test {
         for m in group.iter() {
             assert!(m.priority <= prio);
             prio = m.priority;
+        }
+
+        let mut gwtable = GwGroupTable::new();
+        gwtable.add_group(group).unwrap();
+        println!("{gwtable}");
+    }
+
+
+    #[test]
+    fn test_gw_group_ordering_fallback_ip() {
+        let mut group = GwGroup::new("gw-group-1");
+        group.add_member(GwGroupMember::new("gw1", 100, IpAddr::from_str("172.128.0.1").unwrap())).unwrap();
+        group.add_member(GwGroupMember::new("gw2", 100, IpAddr::from_str("172.128.0.2").unwrap())).unwrap();
+        group.add_member(GwGroupMember::new("gw3", 100, IpAddr::from_str("172.128.0.3").unwrap())).unwrap();
+        group.sort_members();
+
+        let mut prio = group.members[0].priority;
+        let mut ipaddress = group.members[0].ipaddress;
+        for m in group.iter() {
+            assert!(m.priority <= prio);
+            assert!(m.ipaddress <= ipaddress);
+            prio = m.priority;
+            ipaddress = m.ipaddress;
         }
 
         let mut gwtable = GwGroupTable::new();
