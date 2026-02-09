93698b6c5b19cbb9989f2c96f476afa3a8ce062c
Fredi Raspall <fredi@githedgehog.com>
2026-02-09 16:42:42 +0100

    feat(port-forwarding): add NF for port-forwarding
    This NF will attempt to perform port-forwarding for all packets.
    This behavior needs to be changed depending on what the flow-filter
    does for those packets.
    
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

--- nat/src/portfw/mod.rs
+++ nat/src/portfw/mod.rs
@@ -3,3 +3,11 @@
 
 //! Port forwarding
 
+mod flow_state;
+mod nf;
+mod packet;
+mod portfwtable;
+
+pub use flow_state::PortFwState;
+pub use nf::PortForwarder;
+pub use portfwtable::{PortFwEntry, PortFwKey, PortFwTable, PortFwTableRw};
