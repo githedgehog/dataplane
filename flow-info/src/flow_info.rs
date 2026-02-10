95b4e70deb4405af9acc2712b5664dab1efcc69f
Fredi Raspall <fredi@githedgehog.com>
2026-02-10 10:43:54 +0100

    feat(flow-info): impl Display for FlowInfoLocked
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

--- flow-info/src/flow_info.rs
+++ flow-info/src/flow_info.rs
@@ -286,16 +286,36 @@ impl FlowInfo {
     }
 }
 
+impl Display for FlowInfoLocked {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if let Some(data) = &self.dst_vpcd {
+            writeln!(f, "      dst-vpcd:{}", data)?;
+        }
+        if let Some(data) = &self.port_fw_state {
+            writeln!(f, "      port-forwarding:{}", data)?;
+        }
+        if let Some(data) = &self.nat_state {
+            writeln!(f, "      nat-state:{}", data)?;
+        }
+        Ok(())
+    }
+}
+
 impl Display for FlowInfo {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let expires_at = self.expires_at.load(Ordering::Relaxed);
         let expires_in = expires_at.saturating_duration_since(Instant::now());
-        write!(
+        writeln!(f)?;
+        if let Ok(info) = self.locked.try_read() {
+            write!(f, "{}", info)?;
+        } else {
+            write!(f, "could not lock!")?;
+        }
+        writeln!(
             f,
-            " status: {:?}, expires in {}s",
+            "      status: {:?}, expires in {}s",
             self.status,
             expires_in.as_secs()
         )
-        // we can't show the flowinfo yet.
     }
 }
