0c53b57e0b334ae09e2b75628f8851ffbba831f3
Quentin Monnet <qmo@qmon.net>
2026-02-11 15:32:46 +0000

    refactor(config): Move expose overlap-related code to utils submodule
    We've been growing a decent portion of the vpcpeering.rs code to deal
    with overlap validation for the expose blocks. In order to keep this
    file clearer, move the overlap-related methods to a separate file under
    the utils submodule.
    
    Signed-off-by: Quentin Monnet <qmo@qmon.net>

--- config/src/utils/mod.rs
+++ config/src/utils/mod.rs
@@ -4,8 +4,10 @@
 use lpm::prefix::Prefix;
 
 mod collapse;
+mod overlap;
 
 pub use collapse::collapse_prefixes_peering;
+pub(crate) use overlap::{check_private_prefixes_dont_overlap, check_public_prefixes_dont_overlap};
 
 #[derive(thiserror::Error, Debug, Clone)]
 pub enum ConfigUtilError {
