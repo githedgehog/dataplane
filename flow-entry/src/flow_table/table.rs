9ced7e945bde4c570e6cfc5a92bf920463198437
Fredi Raspall <fredi@githedgehog.com>
2026-02-14 18:40:49 +0100

    feat(flow-entry): add test entry expirations
    * Add a test that combines flow entry creation and expirations.
    * Simplify existing test.
    * add method to provide the number of entries in the flow-table.
    
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

--- flow-entry/src/flow_table/table.rs
+++ flow-entry/src/flow_table/table.rs
@@ -268,6 +268,12 @@ impl FlowTable {
         self.priority_queue
             .reap_all_expired_with_time(time, Self::decide_expiry, Self::do_reap)
     }
+
+    #[cfg(test)]
+    pub fn len(&self) -> Option<usize> {
+        let table = self.table.try_read().ok()?;
+        Some(table.len())
+    }
 }
 
 #[cfg(test)]
