97d5a31a758040071e6a3237e952965988d9afa0
Quentin Monnet <qmo@qmon.net>
2026-02-20 02:36:29 +0000

    feat(nat): Reserve ports to prevent masquerade/port forwarding conflicts
    In a configuration when VPC 1 is peered with VPC 2, and VPC 1 uses both
    port forwarding and masquerading (stateful NAT) on its side of the
    peering to communicate with VPC 2, we may need to block some ports from
    being allocated for stateful NAT.
    
    What happens if we don't reserve ports? For example:
    
    1. Endpoint A in VPC 1 attempts to open a connection with endpoint X in
       VPC 2.
    2. The connection gets masqueraded; bad luck, the allocated IP and port
       are exactly the same as those exposed via port forwarding. Flow table
       sessions are created for both directions, based on this IP and port.
    3. The packet is translated with these IP and port, and is sent to
       endpoint X.
    4. The packet is either dropped later on the path, or endpoint X closes
       the connection for some reason.
    5. Endpoint X attempts to reach the service exposed on VPC 1 via port
       forwarding. It uses the port forwarding IP and port and sends its
       packet. Bad luck (again), it also uses the same source port as the
       previous packet targeted.
    6. Because we have a matching flow table entry, we translate back the
       packet, and send it to endpoint A. But wait, endpoint X wanted to use
       the service behind port forwarding - we have no guarantee that it is
       endpoint A, it could well be some endpoint B on VPC 1 instead!
    
    To avoid this unlikely, but possible corner case, we need to block the
    ports used for port forwarding from being allocated for masquerading.
    Update the stateful NAT allocator to mark IPs (if all ports are
    concerned) or port ranges (otherwise) in a port block allocator as
    unavailable, when creating the object pools, so that stateful NAT can
    never assigns them when they should be "reserved" for port forwarding.
    
    Signed-off-by: Quentin Monnet <qmo@qmon.net>

--- nat/src/stateful/apalloc/alloc.rs
+++ nat/src/stateful/apalloc/alloc.rs
@@ -165,9 +165,14 @@ pub(crate) struct AllocatedIp<I: NatIpWithBitmap> {
 }
 
 impl<I: NatIpWithBitmap> AllocatedIp<I> {
-    fn new(ip: I, ip_allocator: IpAllocator<I>, disable_randomness: bool) -> Self {
+    fn new(
+        ip: I,
+        ip_allocator: IpAllocator<I>,
+        reserved_port_range: Option<PortRange>,
+        disable_randomness: bool,
+    ) -> Self {
         // Allow opt-in deterministic port allocation for tests
-        let port_allocator = Self::get_new_portallocator(disable_randomness);
+        let port_allocator = Self::get_new_portallocator(reserved_port_range, disable_randomness);
 
         Self {
             ip,
@@ -177,16 +182,22 @@ impl<I: NatIpWithBitmap> AllocatedIp<I> {
     }
 
     #[cfg(test)]
-    fn get_new_portallocator(disable_randomness: bool) -> port_alloc::PortAllocator<I> {
+    fn get_new_portallocator(
+        reserved_port_range: Option<PortRange>,
+        disable_randomness: bool,
+    ) -> port_alloc::PortAllocator<I> {
         if disable_randomness {
-            port_alloc::PortAllocator::new_no_randomness()
+            port_alloc::PortAllocator::new_no_randomness(reserved_port_range)
         } else {
-            port_alloc::PortAllocator::new()
+            port_alloc::PortAllocator::new(reserved_port_range)
         }
     }
     #[cfg(not(test))]
-    fn get_new_portallocator(_disable_randomness: bool) -> port_alloc::PortAllocator<I> {
-        port_alloc::PortAllocator::new()
+    fn get_new_portallocator(
+        reserved_port_range: Option<PortRange>,
+        _disable_randomness: bool,
+    ) -> port_alloc::PortAllocator<I> {
+        port_alloc::PortAllocator::new(reserved_port_range)
     }
 
     pub(crate) fn ip(&self) -> I {
@@ -282,7 +293,19 @@ impl<I: NatIpWithBitmap> NatPool<I> {
         let offset = self.bitmap.pop_ip()?;
 
         let ip = I::try_from_offset(offset, &self.bitmap_mapping)?;
-        Ok(AllocatedIp::new(ip, ip_allocator, disable_randomness))
+
+        // Check if the IP is in a reserved prefix, retrieve the reserved port range if any
+        let reserved_port_range = self
+            .reserved_prefixes_ports
+            .as_ref()
+            .and_then(|ranges| ranges.lookup(&ip.to_ip_addr()).map(|(_, range)| *range));
+
+        Ok(AllocatedIp::new(
+            ip,
+            ip_allocator,
+            reserved_port_range,
+            disable_randomness,
+        ))
     }
 
     fn deallocate_from_pool(&mut self, ip: I) {
@@ -315,7 +338,7 @@ impl<I: NatIpWithBitmap> NatPool<I> {
         // drops an AllocatedIp and its reference count goes to 0, but it hasn't called the drop()
         // function to remove the IP from the bitmap in that other thread yet).
         let _ = self.bitmap.set_ip_allocated(offset);
-        let arc_ip = Arc::new(AllocatedIp::new(ip, ip_allocator, disable_randomness));
+        let arc_ip = Arc::new(AllocatedIp::new(ip, ip_allocator, None, disable_randomness));
         self.add_in_use(&arc_ip);
         Ok(arc_ip)
     }
