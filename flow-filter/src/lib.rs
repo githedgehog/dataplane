githedgehog/dataplane

commit 2f1a14ebe33e304f01ecf863f66261bbc9258cf3

feat(flow-filter): Implement Display for flow-filter context table
To facilitate troubleshooting the flow-filter stage lookup, implement
the Display trait to dump the content of the context table in a more
human-friendly fashion.

Signed-off-by: Quentin Monnet <qmo@qmon.net>

flow-filter/src/lib.rs
@@ -22,6 +22,7 @@ use net::headers::{Transport, TryIp, TryTransport};
 use net::packet::{DoneReason, Packet, VpcDiscriminant};
 use pipeline::NetworkFunction;
 use std::collections::HashSet;
+use std::fmt::Display;
 use std::net::IpAddr;
 use std::num::NonZero;
 use tracing::{debug, error};
@@ -323,13 +324,24 @@ impl FlowTuple {
     }
 }
 
-impl std::fmt::Display for FlowTuple {
+impl Display for FlowTuple {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
             "srcVpc={} src={}{} dst={}{}",
             self.src_vpcd, self.src_addr, self.src_port, self.dst_addr, self.dst_port
         )
+    }
+}
+
+impl Display for FlowFilter {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "{}:", self.name)?;
+        if let Some(table) = self.tablesr.enter() {
+            write!(f, "{}", *table)
+        } else {
+            writeln!(f, "[no table]")
+        }
     }
 }
 

flow-filter/src/tables.rs
@@ -9,9 +9,8 @@ use lpm::prefix::range_map::DisjointRangesBTreeMap;
 use lpm::prefix::{L4Protocol, PortRange, Prefix};
 use lpm::trie::{IpPortPrefixTrie, ValueWithAssociatedRanges};
 use net::packet::VpcDiscriminant;
-use std::collections::HashMap;
-use std::collections::HashSet;
-use std::fmt::Debug;
+use std::collections::{BTreeMap, HashMap, HashSet};
+use std::fmt::{Debug, Display};
 use std::net::IpAddr;
 use std::ops::RangeBounds;
 
@@ -578,6 +577,123 @@ impl DstConnectionData {
             ));
         }
         self.default_remote_data = Some(PortRangeMap::AllPorts(result));
+        Ok(())
+    }
+}
+
+impl Display for FlowFilterTable {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        // Collect into a BTreeMap to get a deterministic order when dumping the entries
+        for (src_vpcd, table) in self.0.clone().into_iter().collect::<BTreeMap<_, _>>() {
+            writeln!(f, "source VPC {src_vpcd}:")?;
+            writeln!(f, "{table}")?;
+        }
+        Ok(())
+    }
+}
+
+impl Display for VpcConnectionsTable {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for (prefix, port_range_map) in self.trie.iter() {
+            match port_range_map {
+                PortRangeMap::AllPorts(data) => {
+                    writeln!(f, "  source: {prefix}")?;
+                    write!(f, "{data}")?;
+                }
+                PortRangeMap::Ranges(port_ranges) => {
+                    for (port_range, data) in port_ranges.iter() {
+                        writeln!(f, "  source: {prefix}:{port_range}")?;
+                        write!(f, "{data}")?;
+                    }
+                }
+            }
+        }
+        if let Some(default_source) = &self.default_source {
+            writeln!(f, "  local default:")?;
+            match default_source {
+                PortRangeMap::AllPorts(data) => {
+                    write!(f, "{data}")?;
+                }
+                PortRangeMap::Ranges(port_ranges) => {
+                    for (port_range, data) in port_ranges.iter() {
+                        writeln!(f, "  ports: {port_range}")?;
+                        write!(f, "{data}")?;
+                    }
+                }
+            }
+        } else {
+            writeln!(f, "  no local default")?;
+        }
+        Ok(())
+    }
+}
+
+impl Display for DstConnectionData {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for (prefix, port_range_map) in self.trie.iter() {
+            match port_range_map {
+                PortRangeMap::AllPorts(result) => {
+                    writeln!(f, "    destination: {prefix}, data:")?;
+                    write!(f, "{result}")?;
+                }
+                PortRangeMap::Ranges(port_ranges) => {
+                    for (port_range, result) in port_ranges.iter() {
+                        writeln!(f, "    destination: {prefix}:{port_range}, data:")?;
+                        write!(f, "{result}")?;
+                    }
+                }
+            }
+        }
+        if let Some(port_range_map) = &self.default_remote_data {
+            match port_range_map {
+                PortRangeMap::AllPorts(result) => {
+                    writeln!(f, "    remote default data:")?;
+                    write!(f, "{result}")?;
+                }
+                PortRangeMap::Ranges(port_ranges) => {
+                    writeln!(f, "    remote default:")?;
+                    for (port_range, result) in port_ranges.iter() {
+                        writeln!(f, "    ports: {port_range}, data:")?;
+                        write!(f, "{result}")?;
+                    }
+                }
+            }
+        } else {
+            writeln!(f, "    no remote default")?;
+        }
+        Ok(())
+    }
+}
+
+impl Display for VpcdLookupResult {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            VpcdLookupResult::Single(remote_data) => {
+                writeln!(f, "      {remote_data}")
+            }
+            VpcdLookupResult::MultipleMatches(remote_data_set) => {
+                for remote_data in remote_data_set {
+                    writeln!(f, "      {remote_data}")?;
+                }
+                Ok(())
+            }
+        }
+    }
+}
+
+impl Display for RemoteData {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "destination VPC: {}, ", self.vpcd)?;
+        if let Some(src_nat_req) = &self.src_nat_req {
+            write!(f, "source NAT: {src_nat_req:?}, ")?;
+        } else {
+            write!(f, "source NAT: -, ")?;
+        }
+        if let Some(dst_nat_req) = &self.dst_nat_req {
+            write!(f, "destination NAT: {dst_nat_req:?}")?;
+        } else {
+            write!(f, "destination NAT: -")?;
+        }
         Ok(())
     }
 }

lpm/src/trie/ip_port_prefix_trie.rs
@@ -98,6 +98,10 @@ where
         }
         None
     }
+
+    pub fn iter(&self) -> impl Iterator<Item = (Prefix, &V)> {
+        self.0.iter()
+    }
 }
 
 impl<V> Default for IpPortPrefixTrie<V>

commit bbe1be40cb4bad6e5f80929d56a15e347e00916d

refactor(flow-filter): Clean-up indent in Display implementation
When implementing the Display trait for the flow-filter context table, I
chose to implement Display recursively for all sub-objects used in the
table's structure, which I think is a clean approach, but forced me to
add manual indentation when printing various objects, because I didn't
know how to pass an indentation level that would remain consistent
across line breaks when displaying child objects.

This indentation is dirty, and troublesome, because it makes it hard to
reuse the Display output for the objects in a different context, and
even makes it hard to use it with the correct indentation level when
objects of a given type are supposed to appear at several indentation
levels in the generated output.

To address this, the solution is to add a wrapper around the formatter
before passing it down to child objects, and to overwrite the writing
function to add the necessary indentation level. As it turns out, we
already have an indirect dependency that does just that: let's use it to
help with the display of the flow-filter context table.

Fixes: 2f1a14ebe33e ("feat(flow-filter): Implement Display for flow-filter context table")
Signed-off-by: Quentin Monnet <qmo@qmon.net>

Cargo.toml
@@ -116,6 +116,7 @@ hashbrown = { version = "0.16.1", default-features = false, features = [] }
 hwlocality = { version = "1.0.0-alpha.11", default-features = false, features = [] }
 hyper = { version = "1.8.1", default-features = false, features = [] }
 hyper-util = { version = "0.1.20", default-features = false, features = [] }
+indenter = { version = "0.3.4", default-features = false, features = [] }
 inotify = { version = "0.11.1", default-features = false, features = [] }
 ipnet = { version = "2.12.0", default-features = false, features = [] }
 k8s-openapi = { version = "0.27.1", default-features = false, features = [] }

flow-filter/Cargo.toml
@@ -7,6 +7,7 @@ version.workspace = true
 
 [dependencies]
 config = { workspace = true }
+indenter = { workspace = true }
 left-right = { workspace = true }
 linkme = { workspace = true }
 lpm = { workspace = true }

flow-filter/src/lib.rs
@@ -14,6 +14,7 @@
 //!   peerings, get dropped.
 
 use crate::tables::{NatRequirement, RemoteData, VpcdLookupResult};
+use indenter::indented;
 use lpm::prefix::L4Protocol;
 use net::buffer::PacketBufferMut;
 use net::flows::FlowStatus;
@@ -22,7 +23,7 @@ use net::headers::{Transport, TryIp, TryTransport};
 use net::packet::{DoneReason, Packet, VpcDiscriminant};
 use pipeline::NetworkFunction;
 use std::collections::HashSet;
-use std::fmt::Display;
+use std::fmt::{Display, Write};
 use std::net::IpAddr;
 use std::num::NonZero;
 use tracing::{debug, error};
@@ -348,9 +349,9 @@ impl Display for FlowFilter {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         writeln!(f, "{}:", self.name)?;
         if let Some(table) = self.tablesr.enter() {
-            write!(f, "{}", *table)
+            write!(indented(f).with_str("  "), "{}", *table)
         } else {
-            writeln!(f, "[no table]")
+            writeln!(f, "  [no table]")
         }
     }
 }

flow-filter/src/tables.rs
@@ -5,12 +5,13 @@
 
 use config::ConfigError;
 use config::external::overlay::vpcpeering::{VpcExposeNat, VpcExposeNatConfig};
+use indenter::indented;
 use lpm::prefix::range_map::DisjointRangesBTreeMap;
 use lpm::prefix::{L4Protocol, PortRange, Prefix};
 use lpm::trie::{IpPortPrefixTrie, ValueWithAssociatedRanges};
 use net::packet::VpcDiscriminant;
 use std::collections::{BTreeMap, HashMap, HashSet};
-use std::fmt::{Debug, Display};
+use std::fmt::{Debug, Display, Write};
 use std::net::IpAddr;
 use std::ops::RangeBounds;
 
@@ -660,8 +661,9 @@ impl Display for FlowFilterTable {
             .into_iter()
             .collect::<BTreeMap<_, _>>()
         {
-            writeln!(f, "source VPC {src_vpcd}:")?;
-            writeln!(f, "{table}")?;
+            writeln!(f, "  source VPC {src_vpcd}:")?;
+            write!(indented(f).with_str("    "), "{table}")?;
+            writeln!(f)?;
         }
 
         writeln!(f, "subtable for ICMP:")?;
@@ -672,8 +674,9 @@ impl Display for FlowFilterTable {
             .into_iter()
             .collect::<BTreeMap<_, _>>()
         {
-            writeln!(f, "source VPC {src_vpcd}:")?;
-            writeln!(f, "{table}")?;
+            writeln!(f, "  source VPC {src_vpcd}:")?;
+            write!(indented(f).with_str("    "), "{table}")?;
+            writeln!(f)?;
         }
         Ok(())
     }
@@ -684,32 +687,32 @@ impl Display for VpcConnectionsTable {
         for (prefix, port_range_map) in self.trie.iter() {
             match port_range_map {
                 PortRangeMap::AllPorts(data) => {
-                    writeln!(f, "  source: {prefix}")?;
-                    write!(f, "{data}")?;
+                    writeln!(f, "source: {prefix}")?;
+                    write!(indented(f).with_str("  "), "{data}")?;
                 }
                 PortRangeMap::Ranges(port_ranges) => {
                     for (port_range, data) in port_ranges.iter() {
-                        writeln!(f, "  source: {prefix}:{port_range}")?;
-                        write!(f, "{data}")?;
+                        writeln!(f, "source: {prefix}:{port_range}")?;
+                        write!(indented(f).with_str("  "), "{data}")?;
                     }
                 }
             }
         }
         if let Some(default_source) = &self.default_source {
-            writeln!(f, "  local default:")?;
+            writeln!(f, "local default:")?;
             match default_source {
                 PortRangeMap::AllPorts(data) => {
-                    write!(f, "{data}")?;
+                    write!(indented(f).with_str("  "), "{data}")?;
                 }
                 PortRangeMap::Ranges(port_ranges) => {
                     for (port_range, data) in port_ranges.iter() {
                         writeln!(f, "  ports: {port_range}")?;
-                        write!(f, "{data}")?;
+                        write!(indented(f).with_str("    "), "{data}")?;
                     }
                 }
             }
         } else {
-            writeln!(f, "  no local default")?;
+            writeln!(f, "no local default")?;
         }
         Ok(())
     }
@@ -720,13 +723,13 @@ impl Display for DstConnectionData {
         for (prefix, port_range_map) in self.trie.iter() {
             match port_range_map {
                 PortRangeMap::AllPorts(result) => {
-                    writeln!(f, "    destination: {prefix}, data:")?;
-                    write!(f, "{result}")?;
+                    writeln!(f, "destination: {prefix}, data:")?;
+                    write!(indented(f).with_str("  "), "{result}")?;
                 }
                 PortRangeMap::Ranges(port_ranges) => {
                     for (port_range, result) in port_ranges.iter() {
-                        writeln!(f, "    destination: {prefix}:{port_range}, data:")?;
-                        write!(f, "{result}")?;
+                        writeln!(f, "destination: {prefix}:{port_range}, data:")?;
+                        write!(indented(f).with_str("  "), "{result}")?;
                     }
                 }
             }
@@ -734,19 +737,19 @@ impl Display for DstConnectionData {
         if let Some(port_range_map) = &self.default_remote_data {
             match port_range_map {
                 PortRangeMap::AllPorts(result) => {
-                    writeln!(f, "    remote default data:")?;
-                    write!(f, "{result}")?;
+                    writeln!(f, "remote default data:")?;
+                    write!(indented(f).with_str("  "), "{result}")?;
                 }
                 PortRangeMap::Ranges(port_ranges) => {
-                    writeln!(f, "    remote default:")?;
+                    writeln!(f, "remote default:")?;
                     for (port_range, result) in port_ranges.iter() {
-                        writeln!(f, "    ports: {port_range}, data:")?;
-                        write!(f, "{result}")?;
+                        writeln!(f, "  ports: {port_range}, data:")?;
+                        write!(indented(f).with_str("    "), "{result}")?;
                     }
                 }
             }
         } else {
-            writeln!(f, "    no remote default")?;
+            writeln!(f, "no remote default")?;
         }
         Ok(())
     }
@@ -756,11 +759,11 @@ impl Display for VpcdLookupResult {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             VpcdLookupResult::Single(remote_data) => {
-                writeln!(f, "      {remote_data}")
+                writeln!(f, "{remote_data}")
             }
             VpcdLookupResult::MultipleMatches(remote_data_set) => {
                 for remote_data in remote_data_set {
-                    writeln!(f, "      {remote_data}")?;
+                    writeln!(f, "{remote_data}")?;
                 }
                 Ok(())
             }