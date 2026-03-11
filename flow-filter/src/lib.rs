githedgehog/dataplane

commit e6a906c7f8c26a87ae0329bc72bc1e31c79e4b90

feat(flow-filter): Add support for masquerade/port forwarding overlap
As part of the work for port forwarding support, we want to update the
flow-filter stage and have it work with a specific configuration,
namely: when both port forwarding and masquerading are configured for
distinct expose blocks, but on the same side of one given VPC peering.

So far, when the flow-filter stage would find conflicting results for
the destination VPC lookup (when several destinations may match), it
would simply return the MultipleMatches variant and leave it at that. In
a previous commit, we extended this variant to make it able to hold
data: in the current commit, we populate this data to be able to
validate whether a flow is legit, and whether the NAT requirement for
the packet should be masquerading (stateful NAT) or port forwarding.

Tests come in a follow-up commit.

Signed-off-by: Quentin Monnet <qmo@qmon.net>

flow-filter/src/tables.rs
@@ -359,8 +359,25 @@ impl PortRangeMap<DstConnectionData> {
         dst_prefix: Prefix,
         dst_port_range: Option<PortRange>,
     ) -> Result<(), ConfigError> {
-        self.get_mut(src_port_range)?
-            .update(dst_data_result, dst_prefix, dst_port_range)
+        match self {
+            PortRangeMap::AllPorts(data) => {
+                data.update(dst_data_result, dst_prefix, dst_port_range)
+            }
+            PortRangeMap::Ranges(map) => {
+                let port_range = src_port_range.ok_or(ConfigError::InternalFailure(
+                    "Trying to update (local) port ranges map with overlapping ranges".to_string(),
+                ))?;
+                if let Some(data) = map.get_mut(&port_range) {
+                    data.update(dst_data_result, dst_prefix, dst_port_range)
+                } else {
+                    map.insert(
+                        port_range,
+                        DstConnectionData::new(dst_data_result, dst_prefix, dst_port_range),
+                    );
+                    Ok(())
+                }
+            }
+        }
     }
 
     fn update_for_default(
@@ -368,23 +385,17 @@ impl PortRangeMap<DstConnectionData> {
         dst_data_result: VpcdLookupResult,
         src_port_range: Option<PortRange>,
     ) -> Result<(), ConfigError> {
-        self.get_mut(src_port_range)?
-            .update_for_default(dst_data_result)
-    }
-
-    fn get_mut(
-        &mut self,
-        port_range: Option<PortRange>,
-    ) -> Result<&mut DstConnectionData, ConfigError> {
         match self {
-            PortRangeMap::AllPorts(data) => Ok(data),
+            PortRangeMap::AllPorts(data) => data.update_for_default(dst_data_result),
             PortRangeMap::Ranges(map) => {
-                let port_range = port_range.ok_or(ConfigError::InternalFailure(
+                let port_range = src_port_range.ok_or(ConfigError::InternalFailure(
                     "Trying to update (local) port ranges map with overlapping ranges".to_string(),
                 ))?;
-                map.get_mut(&port_range).ok_or(ConfigError::InternalFailure(
-                    "Cannot find entry to update in port ranges map".to_string(),
-                ))
+                map.get_mut(&port_range)
+                    .ok_or(ConfigError::InternalFailure(
+                        "Cannot find entry to update in port ranges map".to_string(),
+                    ))
+                    .and_then(|data| data.update_for_default(dst_data_result))
             }
         }
     }
@@ -491,31 +502,42 @@ impl DstConnectionData {
             (Some(PortRangeMap::Ranges(existing_range_map)), Some(range)) => {
                 let existing_result = existing_range_map.insert(range, result.clone());
 
-                // The only case we had an existing value is when we have multiple matches. Let's do
-                // a sanity check.
-                if existing_result.is_some_and(|r| {
-                    !(matches!(r, VpcdLookupResult::MultipleMatches(_))
-                        && matches!(result, VpcdLookupResult::MultipleMatches(_)))
-                }) {
-                    return Err(ConfigError::InternalFailure(
-                        "Trying to insert conflicting values for remote port range information"
-                            .to_string(),
-                    ));
+                if let Some(actual_existing_result) = existing_result {
+                    // The only case we had an existing value is when we have multiple matches.
+                    // Let's do a sanity check.
+                    if let VpcdLookupResult::MultipleMatches(existing_data_set) =
+                        actual_existing_result
+                        && let VpcdLookupResult::MultipleMatches(_) = result
+                    {
+                        // Fetch the data set we just inserted and merge the previous data set in
+                        let Some(VpcdLookupResult::MultipleMatches(data_set)) =
+                            existing_range_map.get_mut(&range)
+                        else {
+                            unreachable!() // We just added the entry
+                        };
+                        data_set.extend(existing_data_set);
+                    } else {
+                        return Err(ConfigError::InternalFailure(
+                            "Trying to insert conflicting values for remote port range information"
+                                .to_string(),
+                        ));
+                    }
                 }
             }
-            (Some(PortRangeMap::AllPorts(existing_data)), port_range)
+            (Some(PortRangeMap::AllPorts(existing_result)), port_range)
                 if port_range.is_none_or(|r| r.is_max_range()) =>
             {
                 // We should only hit this case if we already inserted an entry with the same
                 // destination VPC.
-                if !(matches!(*existing_data, VpcdLookupResult::MultipleMatches(_))
-                    && matches!(result, VpcdLookupResult::MultipleMatches(_)))
+                if let VpcdLookupResult::MultipleMatches(existing_data_set) = existing_result
+                    && let VpcdLookupResult::MultipleMatches(new_data_set) = result
                 {
+                    existing_data_set.extend(new_data_set);
+                } else {
                     return Err(ConfigError::InternalFailure(
                         "Trying to insert conflicting values for remote information".to_string(),
                     ));
                 }
-                // We already know we have multiple matches, no need to update
             }
             (Some(_), _) => {
                 // One of the entries, the existing or the new, covers all ports, so we can't add a

commit c0ddd22a7600d7e3d63cc86c7905f393c0a843eb

fix(flow-filter): Fix overlap masquerade/port fwd peered with default
When we added support for masquerade/port forwarding overlap on one end
of a peering's manifest, we forgot to adjust the paths for updating
information for the "default" expose blocks, leading to lookup failures
when one expose has such an overlap and is peered to a "default" expose
block. Let's fix it, and add a test.

Fixes: e6a906c7f8c2 ("feat(flow-filter): Add support for masquerade/port forwarding overlap")
Reported-by: Fredi Raspall <fredi@githedgehog.com>
Signed-off-by: Quentin Monnet <qmo@qmon.net>

flow-filter/src/lib.rs
@@ -2407,6 +2407,140 @@ mod tests {
         assert!(!packets[0].meta().requires_port_forwarding());
     }
 
+    #[traced_test]
+    #[test]
+    fn test_flow_filter_table_from_overlay_masquerade_port_forwarding_private_ips_overlap_to_default()
+     {
+        let vni1 = Vni::new_checked(100).unwrap();
+        let vni2 = Vni::new_checked(200).unwrap();
+
+        let mut vpc_table = VpcTable::new();
+        vpc_table
+            .add(Vpc::new("vpc1", "VPC01", vni1.as_u32()).unwrap())
+            .unwrap();
+        vpc_table
+            .add(Vpc::new("vpc2", "VPC02", vni2.as_u32()).unwrap())
+            .unwrap();
+
+        let mut expose_port_forwarding = VpcExpose::empty()
+            .make_port_forwarding(None)
+            .unwrap()
+            .ip(PrefixWithOptionalPorts::new(
+                "1.0.0.1/32".into(),
+                Some(PortRange::new(22, 22).unwrap()),
+            ))
+            .as_range(PrefixWithOptionalPorts::new(
+                "10.0.0.1/32".into(),
+                Some(PortRange::new(2222, 2222).unwrap()),
+            ))
+            .unwrap();
+        expose_port_forwarding.nat.as_mut().unwrap().proto = L4Protocol::Tcp;
+
+        let mut peering_table = VpcPeeringTable::new();
+        peering_table
+            .add(VpcPeering::with_default_group(
+                "vpc1-to-vpc2",
+                VpcManifest {
+                    name: "vpc1".to_string(),
+                    exposes: vec![VpcExpose::empty().set_default()],
+                },
+                VpcManifest {
+                    name: "vpc2".to_string(),
+                    exposes: vec![
+                        expose_port_forwarding,
+                        VpcExpose::empty()
+                            .make_stateful_nat(None)
+                            .unwrap()
+                            .ip("1.0.0.0/24".into())
+                            .as_range("10.0.0.0/24".into())
+                            .unwrap(),
+                    ],
+                },
+            ))
+            .unwrap();
+
+        let mut overlay = Overlay::new(vpc_table, peering_table);
+        // Validation is necessary to build overlay.vpc_table's peerings from peering_table
+        overlay.validate().unwrap();
+
+        let table = FlowFilterTable::build_from_overlay(&overlay).unwrap();
+
+        let mut writer = FlowFilterTableWriter::new();
+        writer.update_flow_filter_table(table);
+
+        let mut flow_filter = FlowFilter::new("test-filter", writer.get_reader());
+
+        // Test with packets
+
+        // VPC-1 -> VPC-2, outside of port forwarding range
+        //
+        // Only masquerading applies.
+        let packet = create_test_ipv4_tcp_packet_with_ports(
+            Some(vpcd(vni1.into())),
+            "7.7.7.7".parse().unwrap(),
+            "10.0.0.1".parse().unwrap(),
+            1234,
+            5678,
+        );
+
+        let packets = flow_filter
+            .process([packet].into_iter())
+            .collect::<Vec<_>>();
+
+        assert_eq!(packets.len(), 1);
+        assert!(!packets[0].is_done(), "{:?}", packets[0].get_done());
+        assert_eq!(packets[0].meta().dst_vpcd, Some(vpcd(vni2.into())));
+        assert!(packets[0].meta().requires_stateful_nat());
+        assert!(!packets[0].meta().requires_stateless_nat());
+        assert!(!packets[0].meta().requires_port_forwarding());
+
+        // VPC-1 -> VPC-2, inside port forwarding range
+        //
+        // Given that we have no flow table entry, port forwarding should take precedence in that
+        // direction.
+        let packet = create_test_ipv4_tcp_packet_with_ports(
+            Some(vpcd(vni1.into())),
+            "7.7.7.7".parse().unwrap(),
+            "10.0.0.1".parse().unwrap(),
+            1234,
+            2222,
+        );
+
+        let packets = flow_filter
+            .process([packet].into_iter())
+            .collect::<Vec<_>>();
+
+        assert_eq!(packets.len(), 1);
+        assert!(!packets[0].is_done(), "{:?}", packets[0].get_done());
+        assert_eq!(packets[0].meta().dst_vpcd, Some(vpcd(vni2.into())));
+        assert!(!packets[0].meta().requires_stateful_nat());
+        assert!(!packets[0].meta().requires_stateless_nat());
+        assert!(packets[0].meta().requires_port_forwarding());
+
+        // VPC-2 -> VPC-1, inside port forwarding range
+        //
+        // Given that we have no flow table entry, masquerading should take precedence in that
+        // direction.
+        let packet = create_test_ipv4_tcp_packet_with_ports(
+            Some(vpcd(vni2.into())),
+            "1.0.0.1".parse().unwrap(),
+            "7.7.7.7".parse().unwrap(),
+            22,
+            1234,
+        );
+
+        let packets = flow_filter
+            .process([packet].into_iter())
+            .collect::<Vec<_>>();
+
+        assert_eq!(packets.len(), 1);
+        assert!(!packets[0].is_done(), "{:?}", packets[0].get_done());
+        assert_eq!(packets[0].meta().dst_vpcd, Some(vpcd(vni1.into())));
+        assert!(packets[0].meta().requires_stateful_nat());
+        assert!(!packets[0].meta().requires_stateless_nat());
+        assert!(!packets[0].meta().requires_port_forwarding());
+    }
+
     #[test]
     fn test_flow_filter_batch_processing() {
         // Setup table

flow-filter/src/setup.rs
@@ -127,10 +127,16 @@ impl FlowFilterSubtable {
                             *remote_nat_req,
                         ))
                     }
-                    VpcdLookupResult::MultipleMatches(_) => {
-                        return Err(ConfigError::InternalFailure(
-                            "Unexpected multiple matches for destination VPC when handling local default expose".to_string(),
-                        ));
+                    VpcdLookupResult::MultipleMatches(dst_data) => {
+                        let data = dst_data
+                            .iter()
+                            .cloned()
+                            .map(|mut d| {
+                                d.src_nat_req = get_nat_requirement(local_default_expose);
+                                d
+                            })
+                            .collect();
+                        VpcdLookupResult::MultipleMatches(data)
                     }
                 };
                 self.insert_default_source(
@@ -144,12 +150,18 @@ impl FlowFilterSubtable {
 
         // Handle remote default expose (for all local prefixes)
         if let Some(remote_default_expose) = remote_default_expose {
-            for (local_prefix, _local_vpcd_result, local_nat_req) in &local_prefixes {
-                let dst_data_result = VpcdLookupResult::Single(RemoteData::new(
+            for (local_prefix, local_vpcd_result, local_nat_req) in &local_prefixes {
+                let remote_data = RemoteData::new(
                     dst_vpcd,
                     *local_nat_req,
                     get_nat_requirement(remote_default_expose),
-                ));
+                );
+                let dst_data_result = match local_vpcd_result {
+                    VpcdLookupResult::Single(_) => VpcdLookupResult::Single(remote_data),
+                    VpcdLookupResult::MultipleMatches(_) => {
+                        VpcdLookupResult::MultipleMatches(HashSet::from([remote_data]))
+                    }
+                };
                 self.insert_default_remote(
                     local_vpcd,
                     dst_data_result,

flow-filter/src/tables.rs
@@ -448,11 +448,15 @@ impl PortRangeMap<DstConnectionData> {
                 let port_range = src_port_range.ok_or(ConfigError::InternalFailure(
                     "Trying to update (local) port ranges map with overlapping ranges".to_string(),
                 ))?;
-                map.get_mut(&port_range)
-                    .ok_or(ConfigError::InternalFailure(
-                        "Cannot find entry to update in port ranges map".to_string(),
-                    ))
-                    .and_then(|data| data.update_for_default(dst_data_result))
+                if let Some(data) = map.get_mut(&port_range) {
+                    data.update_for_default(dst_data_result)
+                } else {
+                    map.insert(
+                        port_range,
+                        DstConnectionData::new_for_default_remote(dst_data_result),
+                    );
+                    Ok(())
+                }
             }
         }
     }
@@ -629,12 +633,18 @@ impl DstConnectionData {
     }
 
     fn update_for_default(&mut self, result: VpcdLookupResult) -> Result<(), ConfigError> {
-        if self.default_remote_data.is_some() {
+        if let Some(PortRangeMap::AllPorts(VpcdLookupResult::MultipleMatches(existing_set))) =
+            &mut self.default_remote_data
+            && let VpcdLookupResult::MultipleMatches(new_set) = result
+        {
+            existing_set.extend(new_set)
+        } else if self.default_remote_data.is_some() {
             return Err(ConfigError::InternalFailure(
                 "Trying to update default remote with an existing default remote".to_string(),
             ));
+        } else {
+            self.default_remote_data = Some(PortRangeMap::AllPorts(result));
         }
-        self.default_remote_data = Some(PortRangeMap::AllPorts(result));
         Ok(())
     }
 }