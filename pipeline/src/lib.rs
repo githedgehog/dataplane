c7cc8c09a7ff6f5433754755e96848eebbc7410d
Fredi Raspall <fredi@githedgehog.com>
2026-03-13 21:39:26 +0100

    feat(pipeline): add PipelineData and trait
    We need an easy way for NFs that belong to a pipeline to access
    some common data (e.g. the generation Id of the current configuration
    applied) in an easy way, without needing to propagate or change
    APIs each time.
    
    This commit:
      - defines a PipelineData struct with an atomic u64 for the generation
        id. A struct is defined so that other fields can be added later.
      - extends trait NetWorkFunction so that the stages that require so
        can have an Arc to the PipelineData when added to the pipeline.
      - Modifies the method to add stages to the pipeline so that those
        NFs that implement the new method get access to the shared
        PipelineData.
    
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

--- pipeline/src/lib.rs
+++ pipeline/src/lib.rs
@@ -116,7 +116,7 @@ pub(crate) mod test_utils;
 #[allow(unused)]
 pub use dyn_nf::{DynNetworkFunction, nf_dyn};
 #[allow(unused)]
-pub use pipeline::{DynPipeline, StageId};
+pub use pipeline::{DynPipeline, PipelineData, StageId};
 #[allow(unused)]
 pub use static_nf::{NetworkFunction, StaticChain};
 
