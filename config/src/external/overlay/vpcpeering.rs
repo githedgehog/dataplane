96cadac77a76e81ceb2ac87e2a9e3366ee6b846e
Fredi Raspall <fredi@githedgehog.com>
2026-03-14 20:02:20 +0100

    feat(config): let VpcExposeXXX timeouts be an Option
    The types represent the values that the user entered in the API.
    So, if nothing is specified, None should be the value. We could
    set defaults in the config, but the default values should come
    from the NF implementing the functionality. Since we don't
    want to add circular dependencies, let's make those optional and
    let the NF set the default as appropriate.
    
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

--- config/src/external/overlay/vpcpeering.rs
+++ config/src/external/overlay/vpcpeering.rs
@@ -15,30 +15,14 @@ use std::time::Duration;
 #[derive(Clone, Debug, Default, PartialEq)]
 pub struct VpcExposeStatelessNat;
 
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, Debug, Default, PartialEq)]
 pub struct VpcExposeStatefulNat {
-    pub idle_timeout: Duration,
+    pub idle_timeout: Option<Duration>,
 }
 
-impl Default for VpcExposeStatefulNat {
-    fn default() -> Self {
-        VpcExposeStatefulNat {
-            idle_timeout: Duration::from_secs(120),
-        }
-    }
-}
-
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, Debug, Default, PartialEq)]
 pub struct VpcExposePortForwarding {
-    pub idle_timeout: Duration,
-}
-
-impl Default for VpcExposePortForwarding {
-    fn default() -> Self {
-        VpcExposePortForwarding {
-            idle_timeout: Duration::from_secs(120),
-        }
-    }
+    pub idle_timeout: Option<Duration>,
 }
 
 #[derive(Clone, Debug, PartialEq)]
@@ -128,9 +112,7 @@ impl VpcExpose {
         mut self,
         idle_timeout: Option<Duration>,
     ) -> Result<Self, ConfigError> {
-        let options = idle_timeout
-            .map(|to| VpcExposeStatefulNat { idle_timeout: to })
-            .unwrap_or_default();
+        let options = VpcExposeStatefulNat { idle_timeout };
         match self.nat.as_mut() {
             Some(nat) if nat.is_stateful() => {
                 nat.config = VpcExposeNatConfig::Stateful(options);
@@ -163,9 +145,7 @@ impl VpcExpose {
         idle_timeout: Option<Duration>,
         proto: Option<L4Protocol>,
     ) -> Result<Self, ConfigError> {
-        let options = idle_timeout
-            .map(|to| VpcExposePortForwarding { idle_timeout: to })
-            .unwrap_or_default();
+        let options = VpcExposePortForwarding { idle_timeout };
         match self.nat.as_mut() {
             Some(nat) if nat.is_port_forwarding() => {
                 nat.config = VpcExposeNatConfig::PortForwarding(options);
@@ -194,7 +174,7 @@ impl VpcExpose {
     pub fn idle_timeout(&self) -> Option<Duration> {
         self.nat.as_ref().and_then(|nat| {
             if let VpcExposeNatConfig::Stateful(config) = &nat.config {
-                Some(config.idle_timeout)
+                config.idle_timeout
             } else {
                 None
             }
