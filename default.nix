530c0b01946eb800bd6957878e2bf3a08472c144
Daniel Noland <daniel@githedgehog.com>
2026-04-22 14:26:15 -0600

    fix(frr): make /etc a real directory
    This addresses a runc 1.4.x path-escape fix.  To quote Pau:
    
    > Unblocks the k3s 1.35.3 bump in fabricator#1671. That bump ships runc 1.4.x (CVE-2025-31133 / CVE-2025-52565 /
    CVE-2025-52881 container-escape mitigations), whose path resolver refuses to follow any absolute symlink during rootfs
    setup. The FRR image's /etc -> /nix/store/…-frr-config-0/etc trips it, and init-frr fails with openat etc/passwd: path
    escapes from parent, so the gateway never reaches Ready.
    
    The fix is to properly materialize the path in the nix image build rather than letting it be a symlink to the nix store.
    
    Signed-off-by: Daniel Noland <daniel@githedgehog.com>

--- default.nix
+++ default.nix
@@ -717,7 +717,15 @@ let
     inherit tag;
     contents = pkgs.buildEnv {
       name = "dataplane-frr-env";
-      pathsToLink = [ "/" ];
+      pathsToLink = [
+        "/bin"
+        "/etc"
+        "/lib"
+        "/libexec"
+        "/share"
+        "/usr"
+        "/var"
+      ];
       paths = with pkgs; [
         bash
         coreutils
@@ -746,6 +754,8 @@ let
       mkdir -p /var
       ln -s /run /var/run
       chown -R frr:frr /var/run/frr
+      rm /etc/passwd
+      cp ${pkgs.fancy.frr-config}/etc/passwd /etc/passwd
     '';
 
     enableFakechroot = true;
@@ -763,7 +773,13 @@ let
     contents = pkgs.buildEnv {
       name = "dataplane-frr-host-env";
       pathsToLink = [
-        "/"
+        "/bin"
+        "/etc"
+        "/lib"
+        "/libexec"
+        "/share"
+        "/usr"
+        "/var"
       ];
       paths = with pkgs; [
         bash
