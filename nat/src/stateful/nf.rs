fd19678380a6190cfd1a0980c35cdbdf9ea44077
Quentin Monnet <qmo@qmon.net>
2026-04-22 16:26:10 +0100

    chore(nat): Improve AllocatorError variants and messages
    AllocatorError::NoFreePort is not about no port block being available,
    but not ports being available in a block. The message is confusing:
    let's fix it. It's been so confusing that we used the error at a
    location where the error is not related (passing it an incorrect
    argument): let's add a new variant for that location, to make it easier
    to troubleshoot errors.
    
    Signed-off-by: Quentin Monnet <qmo@qmon.net>

--- nat/src/stateful/nf.rs
+++ nat/src/stateful/nf.rs
@@ -528,6 +528,7 @@ fn translate_error(error: &StatefulNatError) -> DoneReason {
         | StatefulNatError::IcmpError
         | StatefulNatError::AllocationFailure(
             AllocatorError::PortAllocationFailed(_)
+            | AllocatorError::PortReservationFailed(_)
             | AllocatorError::MissingDiscriminant
             | AllocatorError::UnsupportedDiscriminant,
         ) => DoneReason::NatFailure,
