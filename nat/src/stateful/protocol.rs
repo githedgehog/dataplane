65632f047f402018dcb07dec9af8eacd3a6b451e
Fredi Raspall <fredi@githedgehog.com>
2026-04-27 09:58:46 +0200

    feat(stateful-nat): add protocol submodule
    Adds simple flow status state machines for masqueraded flows.
    With the state machine defined:
      1) icmp traffic can only be oneway or twoway. The timeout for
         icmp traffic will not configurable by the user.
      2) UDP traffic can be in oneway, twoway or established (3 way).
         Only when reaching established flow timeouts will be the ones
         configured by the user.
      3) TCP traffic uses the same SM as for port-forwarding, reversed.
         Until flows reach established state, their timeouts will not
         be the user configured ones.
    
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

---
+++ nat/src/stateful/protocol.rs
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Open Network Fabric Authors
+
+//! Functions to represent tiny state machines for flows in the context
+//! of masquerading. We currently use these to know how much to extend the lifetime of flows
+//! for port conservation.
+
+use crate::common::{NatAction, NatFlowStatus};
+use net::buffer::PacketBufferMut;
+use net::headers::{TryIp, TryTcp};
+use net::ip::NextHeader;
+use net::packet::Packet;
+use net::tcp::Tcp;
+
+fn next_flow_status_udp<Buf: PacketBufferMut>(
+    _packet: &Packet<Buf>,
+    action: NatAction,
+    status: NatFlowStatus,
+) -> NatFlowStatus {
+    match action {
+        NatAction::SrcNat => match status {
+            NatFlowStatus::TwoWay => NatFlowStatus::Established,
+            _ => status,
+        },
+        NatAction::DstNat => match status {
+            NatFlowStatus::OneWay => NatFlowStatus::TwoWay,
+            _ => status,
+        },
+    }
+}
+fn next_flow_status_icmp(action: NatAction, status: NatFlowStatus) -> NatFlowStatus {
+    match action {
+        NatAction::SrcNat => match status {
+            _ => status,
+        },
+        NatAction::DstNat => match status {
+            NatFlowStatus::OneWay => NatFlowStatus::TwoWay,
+            _ => status,
+        },
+    }
+}
+
+fn next_flow_status_tcp(action: NatAction, status: NatFlowStatus, tcp: &Tcp) -> NatFlowStatus {
+    match action {
+        NatAction::SrcNat => match status {
+            NatFlowStatus::TwoWay if !tcp.syn() && tcp.ack() => NatFlowStatus::Established,
+            NatFlowStatus::Established if tcp.fin() => NatFlowStatus::CClosing,
+            NatFlowStatus::SClosing if !tcp.fin() && tcp.ack() => NatFlowStatus::SHalfClose,
+            NatFlowStatus::SClosing if tcp.fin() && tcp.ack() => NatFlowStatus::LastAck,
+            NatFlowStatus::SHalfClose if tcp.fin() => NatFlowStatus::LastAck,
+            NatFlowStatus::LastAck if tcp.ack() => NatFlowStatus::Closed,
+            _other if tcp.rst() => NatFlowStatus::Reset,
+            other => other,
+        },
+        NatAction::DstNat => match status {
+            NatFlowStatus::OneWay if tcp.syn() && tcp.ack() => NatFlowStatus::TwoWay,
+            NatFlowStatus::Established if tcp.fin() => NatFlowStatus::SClosing,
+            NatFlowStatus::CClosing if !tcp.fin() && tcp.ack() => NatFlowStatus::CHalfClose,
+            NatFlowStatus::CClosing if tcp.fin() && tcp.ack() => NatFlowStatus::LastAck,
+            NatFlowStatus::CClosing if !tcp.fin() && tcp.ack() => NatFlowStatus::CHalfClose,
+            NatFlowStatus::CHalfClose if tcp.fin() => NatFlowStatus::LastAck,
+            NatFlowStatus::LastAck if tcp.ack() => NatFlowStatus::Closed,
+            _other if tcp.rst() => NatFlowStatus::Reset,
+            other => other,
+        },
+    }
+}
+
+// Compute the next `NatFlowStatus` of a flow, given the current, the received packet and
+// the direction
+pub(crate) fn next_flow_status<Buf: PacketBufferMut>(
+    packet: &Packet<Buf>,
+    action: NatAction,     // action of the flow hit
+    status: NatFlowStatus, // current status
+) -> NatFlowStatus {
+    let proto = packet
+        .try_ip()
+        .unwrap_or_else(|| unreachable!()) // packet without IP hdr should not make it here
+        .next_header();
+
+    // match on next-header, instead of relying on headers, as those may not be present w/ fragmentation
+    match proto {
+        NextHeader::UDP => next_flow_status_udp(packet, action, status),
+        NextHeader::ICMP | NextHeader::ICMP6 => next_flow_status_icmp(action, status),
+        NextHeader::TCP => {
+            if let Some(tcp) = packet.try_tcp() {
+                next_flow_status_tcp(action, status, tcp)
+            } else {
+                status
+            }
+        }
+        _ => status,
+    }
+}
