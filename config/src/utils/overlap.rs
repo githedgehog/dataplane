735ca31227bdcc0349121b27bd623d6864b043d1
Quentin Monnet <qmo@qmon.net>
2026-04-28 17:29:37 +0100

    feat(config): Introduce ValidatedManifest
    Add a ValidatedManifest transparent wrapper around VpcManifest, exposing
    the contents of the manifest through a validated lens: callers can reach
    the list of validated expose blocks ("valexp"), but they cannot modify
    it. There is no way to obtain a ValidatedManifest currently. We'll
    provide one from the parent validated object.
    
    Simplify overlap detection functions:
    check_private_prefixes_dont_overlap() and
    check_public_prefixes_dont_overlap() now take a reference to a
    ValidatedExpose, meaning we no longer have to deal with exclusion
    prefixes.
    
    Signed-off-by: Quentin Monnet <qmo@qmon.net>

--- config/src/utils/overlap.rs
+++ config/src/utils/overlap.rs
@@ -2,22 +2,17 @@
 // Copyright Open Network Fabric Authors
 
 use crate::ConfigError;
-use crate::external::overlay::vpcpeering::VpcExpose;
-use lpm::prefix::{IpRangeWithPorts, PrefixPortsSet, ppsize_zero};
+use crate::external::overlay::vpcpeering::ValidatedExpose;
+use lpm::prefix::{IpRangeWithPorts, PrefixPortsSet};
 
 pub fn check_private_prefixes_dont_overlap(
-    expose_left: &VpcExpose,
-    expose_right: &VpcExpose,
+    expose_left: &ValidatedExpose,
+    expose_right: &ValidatedExpose,
 ) -> Result<(), ConfigError> {
     if port_forwarding_with_distinct_l4_protocols(expose_left, expose_right) {
         return Ok(());
     }
-    check_prefixes_dont_overlap(
-        &expose_left.ips,
-        &expose_left.nots,
-        &expose_right.ips,
-        &expose_right.nots,
-    )
+    check_prefixes_dont_overlap(expose_left.ips(), expose_right.ips())
 }
 
 // Check for overlap for the lists of public prefixes.
@@ -26,130 +21,44 @@ pub fn check_private_prefixes_dont_overlap(
 // - expose_left.ips      / expose_right.as_range
 // - expose_left.as_range / expose_right.ips
 // - expose_left.ips      / expose_right.ips
-// (along with the respective exclusion prefixes applied).
 pub fn check_public_prefixes_dont_overlap(
-    expose_left: &VpcExpose,
-    expose_right: &VpcExpose,
+    expose_left: &ValidatedExpose,
+    expose_right: &ValidatedExpose,
 ) -> Result<(), ConfigError> {
     if port_forwarding_with_distinct_l4_protocols(expose_left, expose_right) {
         return Ok(());
     }
-    check_prefixes_dont_overlap(
-        expose_left.public_ips(),
-        expose_left.public_excludes(),
-        expose_right.public_ips(),
-        expose_right.public_excludes(),
-    )
+    check_prefixes_dont_overlap(expose_left.public_ips(), expose_right.public_ips())
 }
 
-// If the two expose blocks have port fortwarding set up, one for TCP and one for UDP, then there is
+// If the two expose blocks have port forwarding set up, one for TCP and one for UDP, then there is
 // no overlap.
 fn port_forwarding_with_distinct_l4_protocols(
-    expose_left: &VpcExpose,
-    expose_right: &VpcExpose,
+    expose_left: &ValidatedExpose,
+    expose_right: &ValidatedExpose,
 ) -> bool {
-    if expose_left.has_port_forwarding()
+    expose_left.has_port_forwarding()
         && expose_right.has_port_forwarding()
-        && let Some(nat_left) = expose_left.nat.as_ref()
-        && let Some(nat_right) = expose_right.nat.as_ref()
-        && nat_left.proto.intersection(&nat_right.proto).is_none()
-    {
-        true
-    } else {
-        false
-    }
+        && expose_left.nat_proto().is_some_and(|proto_left| {
+            expose_right
+                .nat_proto()
+                .is_some_and(|proto_right| proto_left.intersection(proto_right).is_none())
+        })
 }
 
 // Validate that two sets of prefixes, with their exclusion prefixes applied, don't overlap
 fn check_prefixes_dont_overlap(
     prefixes_left: &PrefixPortsSet,
-    excludes_left: &PrefixPortsSet,
     prefixes_right: &PrefixPortsSet,
-    excludes_right: &PrefixPortsSet,
 ) -> Result<(), ConfigError> {
-    // Find colliding prefixes
-    let mut colliding = Vec::new();
     for prefix_left in prefixes_left {
         for prefix_right in prefixes_right {
             if prefix_left.overlaps(prefix_right) {
-                colliding.push((*prefix_left, *prefix_right));
+                return Err(ConfigError::OverlappingPrefixes(
+                    *prefix_left,
+                    *prefix_right,
+                ));
             }
-        }
-    }
-    // If not prefixes collide, we're good - exit.
-    if colliding.is_empty() {
-        return Ok(());
-    }
-
-    // How do we determine whether there is a collision between the set of available addresses on
-    // the left side, and the set of available addresses on the right side? A collision means:
-    //
-    // - Prefixes collide, in other words, they have a non-empty intersection (we've checked that
-    //   earlier)
-    //
-    // - This intersection is not fully covered by exclusion prefixes
-    //
-    // The idea in the loop below is that for each pair of colliding prefixes:
-    //
-    // - We retrieve the size of the intersection of the colliding prefixes.
-    //
-    // - We retrieve the size of the union of the intersections of all the exclusion prefixes (from
-    //   left and right sides) covering part of this intersection.
-    //
-    // - If the size of the intersection of colliding allowed prefixes is bigger than the size of
-    //   the union of the intersections of the exclusion prefixes applying to these allowed
-    //   prefixes, then it means that some addresses are effectively allowed in both the left-side
-    //   and the right-side set of available addresses, and this is an error. If the sizes are
-    //   identical, then all addresses in the intersection of the prefixes are excluded on at least
-    //   one side, so it's all good.
-    for (prefix_left, prefix_right) in colliding {
-        let intersection_prefix = prefix_left.intersection(&prefix_right).unwrap_or_else(|| {
-            unreachable!(); // These prefixes were paired precisely because they collide
-        });
-
-        // We need to compute the size of the union of the excluded prefixes. Start by adding the
-        // sizes of all exclusion prefixes, from both sides.
-        let mut union_excludes_size = ppsize_zero();
-
-        // Now we remove once the size of the intersection of each pair of excluded prefixes, to
-        // avoid double-counting some ranges. We know that all exclusion prefixes on the left side
-        // are disjoint, and all so are exclusion prefixes on the right side, which means that we
-        // cannot have more than two prefixes overlapping. It's enough to look for intersection of
-        // all left-side prefixes with each right-side prefix.
-        for exclude_left in excludes_left
-            .iter()
-            .filter(|exclude| exclude.overlaps(&intersection_prefix))
-        {
-            let exclude_covering_allowed_left = exclude_left
-                .intersection(&intersection_prefix)
-                .unwrap_or_else(|| {
-                    // We filtered prefixes with overlap with intersection_prefix
-                    unreachable!();
-                });
-            union_excludes_size += exclude_covering_allowed_left.size();
-            for exclude_right in excludes_right
-                .iter()
-                .filter(|exclude| exclude.overlaps(&intersection_prefix))
-            {
-                let exclude_covering_allowed_right = exclude_right
-                    .intersection(&intersection_prefix)
-                    .unwrap_or_else(|| {
-                        // We filtered prefixes with overlap with intersection_prefix
-                        unreachable!();
-                    });
-                union_excludes_size += exclude_covering_allowed_right.size();
-                // Remove size of intersection, to avoid double-counting for a given range
-                union_excludes_size -= exclude_covering_allowed_left
-                    .intersection(&exclude_covering_allowed_right)
-                    .map_or(ppsize_zero(), |p| p.size());
-            }
-        }
-
-        if union_excludes_size < intersection_prefix.size() {
-            // Some addresses at the intersection of both prefixes are not covered by the union of
-            // all exclusion prefixes, in other words, they are available from both prefixes. This
-            // is an error.
-            return Err(ConfigError::OverlappingPrefixes(prefix_left, prefix_right));
         }
     }
     Ok(())
