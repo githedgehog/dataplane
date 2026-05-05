cf89abcbe755ce2cf438b17726fc7b9619770e1c
Quentin Monnet <qmo@qmon.net>
2026-05-05 12:15:18 +0100

    feat(config): Merge overlapping prefixes from expose blocks
    After we've updated the list of prefixes to exclude all entries from the
    exclusion prefixes, merge overlapping prefixes together so that we can
    support overlapping prefixes passed by the user.
    
    Signed-off-by: Quentin Monnet <qmo@qmon.net>

--- config/src/utils/mod.rs
+++ config/src/utils/mod.rs
@@ -7,7 +7,10 @@ mod collapse;
 mod overlap;
 
 pub(crate) use collapse::collapse_prefixes;
-pub(crate) use overlap::{check_private_prefixes_dont_overlap, check_public_prefixes_dont_overlap};
+pub(crate) use overlap::{
+    check_private_prefixes_dont_overlap, check_public_prefixes_dont_overlap,
+    merge_overlapping_prefixes,
+};
 
 #[derive(thiserror::Error, Debug, Clone)]
 pub enum ConfigUtilError {
