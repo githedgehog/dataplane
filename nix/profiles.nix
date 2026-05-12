c1f6ac0f19b9313e0494e63a00105cea5a8e6fec
Daniel Noland <daniel@githedgehog.com>
2026-05-11 21:15:49 -0600

    maybe

--- nix/profiles.nix
+++ nix/profiles.nix
@@ -103,7 +103,7 @@ let
   sanitize.address.NIX_CXXFLAGS_COMPILE = sanitize.address.NIX_CFLAGS_COMPILE;
   sanitize.address.NIX_CFLAGS_LINK = sanitize.address.NIX_CFLAGS_COMPILE ++ [
     "-static-libasan"
-    "-Wl,--thinlto-jobs=1"
+    "-Wl,--thinlto-jobs=4"
   ];
   sanitize.address.RUSTFLAGS = [
     "-Zsanitizer=address"
