6898eed8b67242958b2856d30c3c22059c39af6e
Daniel Noland <daniel@githedgehog.com>
2026-05-22 17:30:12 -0600

    bump(dplane-plugin): accept glibc-ism removal
    This section is no longer needed because it was patched upstream.
    
    Signed-off-by: Daniel Noland <daniel@githedgehog.com>

--- nix/pkgs/dplane-plugin/default.nix
+++ nix/pkgs/dplane-plugin/default.nix
@@ -20,15 +20,6 @@ stdenv.mkDerivation (finalAttrs: {
   version = sources.dplane-plugin.revision;
   src = sources.dplane-plugin.outPath;
 
-  # workaround: src/hh_dp_msg.c reaches into a glibc-internal anonymous
-  # union name (`.__in6_u.__u6_addr8`) on struct in6_addr.  musl exposes
-  # the POSIX-standard `.s6_addr` member directly without that wrapping
-  # union, so the access fails to compile.
-  # remove once fixed upstream in githedgehog/dplane-plugin.
-  postPatch = ''
-    sed -i 's/\.__in6_u\.__u6_addr8/.s6_addr/g' src/hh_dp_msg.c
-  '';
-
   doCheck = false;
   doFixup = false;
   enableParallelBuilding = true;
