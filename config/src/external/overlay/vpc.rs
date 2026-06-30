a640219baff3e2b0825431be475c6d353e58615d
Quentin Monnet <qmo@qmon.net>
2026-06-30 17:55:29 +0100

    fix(config): Make sure both manifests of a peering have same IP version
    We don't support peerings with two manifests using different IP versions
    for the prefixes in their expose blocks. We should reject it at
    validation time.
    
    We already validate that all expose blocks in a given manifest use the
    same IP version for their prefixes, so there's no additional
    verification required on that side.
    
    Update a test in nat crate where we use both IP versions in a peering.
    
    Signed-off-by: Quentin Monnet <qmo@qmon.net>

--- config/src/external/overlay/vpc.rs
+++ config/src/external/overlay/vpc.rs
@@ -54,6 +54,15 @@ impl Peering {
             remote_id: self.remote_id.clone(),
             gwgroup: self.gwgroup.clone(),
         };
+
+        if valid_peering_candidate.local.is_v4_or_default()
+            != valid_peering_candidate.remote.is_v4_or_default()
+        {
+            return Err(ConfigError::Forbidden(
+                "The two manifests of a peering must contain expose blocks of the same IP version",
+            ));
+        }
+
         valid_peering_candidate.validate_nat_combinations()?;
 
         Ok(valid_peering_candidate)
