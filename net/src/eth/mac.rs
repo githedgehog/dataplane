32ba6c8de7cb28436ca9f5c323c2d7331a529cd9
Fredi Raspall <fredi@githedgehog.com>
2026-07-20 14:23:03 +0200

    feat(net): reimpl Display for Mac
    Impl Display for Mac such that formatting is preserved. We use
    ArrayString in stack so that no allocation is needed, using the
    fact that a mac, as a hex string, has a fixed length.
    
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

--- net/src/eth/mac.rs
+++ net/src/eth/mac.rs
@@ -3,7 +3,7 @@
 
 //! Mac address type and logic.
 
-use arrayvec::ArrayVec;
+use arrayvec::{ArrayString, ArrayVec};
 use std::{
     fmt::{Debug, Display, LowerHex, UpperHex},
     str::FromStr,
@@ -184,11 +184,15 @@ impl Mac {
 
 impl Display for Mac {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use std::fmt::Write as _; // bring in scope
+        // MAC always renders as 17 chars
+        let mut buf = ArrayString::<17>::new();
         write!(
-            f,
-            "{:<02x}:{:<02x}:{:<02x}:{:<02x}:{:<02x}:{:<02x}",
+            buf,
+            "{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}",
             self.0[0], self.0[1], self.0[2], self.0[3], self.0[4], self.0[5]
-        )
+        )?;
+        f.pad(&buf)
     }
 }
 
