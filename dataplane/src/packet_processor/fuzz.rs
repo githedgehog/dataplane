5c9f0664549e915a0ebf42ec935d02550d529875
Daniel Noland <daniel@githedgehog.com>
2026-08-27 20:10:35 -0600

    fix(dataplane): Drop three imports the new modules do not use
    Signed-off-by: Daniel Noland <daniel@githedgehog.com>

--- dataplane/src/packet_processor/fuzz.rs
+++ dataplane/src/packet_processor/fuzz.rs
@@ -2382,7 +2382,7 @@ mod generated {
 #[cfg(test)]
 mod burst {
     use super::round_trip::udp;
-    use super::routed::{exposes, inside, tunnelled, tunnelled_from};
+    use super::routed::{exposes, inside, tunnelled};
     use super::*;
     use net::headers::TryVxlan;
     use std::sync::LazyLock;
@@ -2720,7 +2720,7 @@ mod routed {
     use super::shapes::{Batch, Shape, aim, wire};
     use super::*;
     use super::{Load, drive};
-    use net::buffer::{PacketBufferMut, TestBuffer};
+    use net::buffer::TestBuffer;
     use net::headers::{TryEth, TryHeaders, TryHeadersMut, TryIpv4, TryVxlan};
     use net::ip::dscp::Dscp;
     use net::ip::ecn::Ecn;
@@ -2729,7 +2729,6 @@ mod routed {
     };
     use net::parse::DeParse;
     use net::vlan::Vid;
-    use std::collections::BTreeMap;
     use std::sync::LazyLock;
     use std::sync::atomic::{AtomicU64, Ordering};
 
