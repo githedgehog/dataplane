ecab65d7a59c436043e589074dd437b7fa8558fa
Fredi Raspall <fredi@githedgehog.com>
2026-09-14 12:53:26 +0200

    feat(routing): remove unnecessary generics
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

--- routing/src/cli/display.rs
+++ routing/src/cli/display.rs
@@ -15,7 +15,7 @@
 use crate::atable::adjacency::{Adjacency, AdjacencyTable};
 use crate::fib::fibgroupstore::FibRoute;
 use crate::fib::fibobjects::{EgressObject, FibEntry, FibGroup, PktInstruction};
-use crate::fib::fibtype::{Fib, FibKey};
+use crate::fib::fibtype::{Fib, FibKey, FibRouteV4Filter, FibRouteV6Filter};
 use crate::frr::frrmi::{FrrAppliedConfig, Frrmi, FrrmiStats};
 use crate::router::cpi::{CpiStats, CpiStatus, StatsRow};
 
@@ -23,6 +23,7 @@ use crate::rib::VrfTable;
 use crate::rib::encapsulation::{Encapsulation, VxlanEncapsulation};
 use crate::rib::nexthop::{FwAction, Nhop, NhopKey, NhopStore};
 use crate::rib::vrf::{Route, RouteFlags, RouteOrigin, ShimNhop, Vrf, VrfStatus};
+use crate::rib::vrf::{RouteV4Filter, RouteV6Filter};
 
 use crate::interfaces::iftable::IfTable;
 use crate::interfaces::interface::Attachment;
@@ -35,7 +36,7 @@ use chrono::DateTime;
 use common::cliprovider::{Heading, line};
 
 use clock::Instant;
-use lpm::prefix::{IpPrefix, Ipv4Prefix, Ipv6Prefix};
+use lpm::prefix::IpPrefix;
 use lpm::trie::{PrefixMapTrie, TrieMap};
 use net::vxlan::Vni;
 use std::fmt::Display;
@@ -328,14 +329,11 @@ impl Display for Vrf {
     }
 }
 
-pub struct VrfViewV4<'a, F>
-where
-    F: Fn(&(Ipv4Prefix, &Route)) -> bool,
-{
+pub struct VrfViewV4<'a> {
     pub vrf: &'a Vrf,
-    pub filter: &'a F,
+    pub filter: &'a RouteV4Filter,
 }
-impl<F: for<'a> Fn(&'a (Ipv4Prefix, &Route)) -> bool> Display for VrfViewV4<'_, F> {
+impl Display for VrfViewV4<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         // apply the filter
         let rt_iter = self.vrf.iter_v4().filter(&self.filter);
@@ -363,14 +361,11 @@ impl<F: for<'a> Fn(&'a (Ipv4Prefix, &Route)) -> bool> Display for VrfViewV4<'_, 
     }
 }
 
-pub struct VrfViewV6<'a, F>
-where
-    F: Fn(&(Ipv6Prefix, &Route)) -> bool,
-{
+pub struct VrfViewV6<'a> {
     pub vrf: &'a Vrf,
-    pub filter: &'a F,
+    pub filter: &'a RouteV6Filter,
 }
-impl<F: for<'a> Fn(&'a (Ipv6Prefix, &Route)) -> bool> Display for VrfViewV6<'_, F> {
+impl Display for VrfViewV6<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         // apply the filter
         let rt_iter = self.vrf.iter_v6().filter(&self.filter);
@@ -653,6 +648,7 @@ impl Display for RmacEntry {
         )
     }
 }
+
 impl Display for RmacStore {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         Heading(format!(
@@ -805,14 +801,11 @@ impl Display for Fib {
     }
 }
 
-pub struct FibViewV4<'a, F>
-where
-    F: Fn(&(Ipv4Prefix, &FibRoute)) -> bool,
-{
+pub struct FibViewV4<'a> {
     pub vrf: &'a Vrf,
-    pub filter: &'a F,
+    pub filter: &'a FibRouteV4Filter,
 }
-impl<F: for<'a> Fn(&'a (Ipv4Prefix, &FibRoute)) -> bool> Display for FibViewV4<'_, F> {
+impl Display for FibViewV4<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let Some(fibw) = &self.vrf.fibw else {
             return writeln!(f, "No fib");
@@ -843,14 +836,11 @@ impl<F: for<'a> Fn(&'a (Ipv4Prefix, &FibRoute)) -> bool> Display for FibViewV4<'
     }
 }
 
-pub struct FibViewV6<'a, F>
-where
-    F: Fn(&(Ipv6Prefix, &FibRoute)) -> bool,
-{
+pub struct FibViewV6<'a> {
     pub vrf: &'a Vrf,
-    pub filter: &'a F,
+    pub filter: &'a FibRouteV6Filter,
 }
-impl<F: for<'a> Fn(&'a (Ipv6Prefix, &FibRoute)) -> bool> Display for FibViewV6<'_, F> {
+impl Display for FibViewV6<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let Some(fibw) = &self.vrf.fibw else {
             return writeln!(f, "No fib");
