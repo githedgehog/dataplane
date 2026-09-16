bc53cc76028967f35a1a3c5b41f380c34df2b47d
Fredi Raspall <fredi@githedgehog.com>
2026-09-16 19:21:40 +0200

    feat(cli): ensure arg string consistency
    The completer uses a tree where nodes can be attached named args.
    Once the user input is entered, a parser checks the arg=value
    pairs entered and populates the request. Let the cmd tree args
    and the parser ones come from the same source so that there is
    no inconsistency.
    
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

--- cli/bin/argsparse.rs
+++ cli/bin/argsparse.rs
@@ -7,7 +7,95 @@ use dataplane_cli::cliproto::{RequestArgs, RouteProtocol};
 use std::collections::HashMap;
 use std::net::IpAddr;
 use std::str::FromStr;
+use strum::EnumIter;
 use thiserror::Error;
+
+// The identifier for a certain argument
+#[derive(Debug, EnumIter, PartialEq)]
+pub enum CliArgId {
+    Path,
+    BindAddr,
+    Vpc,
+    Vni,
+    Address,
+    Prefix,
+    Mac,
+    Ifname,
+    VrfId,
+    Protocol,
+}
+impl CliArgId {
+    // N.B. strings should be different for proper parsing
+    pub const ARG_PATH: &str = "path";
+    pub const ARG_BIND_ADDR: &str = "bind-address";
+    pub const ARG_VPC: &str = "vpc";
+    pub const ARG_VNI: &str = "vni";
+    pub const ARG_ADDRESS: &str = "address";
+    pub const ARG_PREFIX: &str = "prefix";
+    pub const ARG_MAC: &str = "mac-address";
+    pub const ARG_IFNAME: &str = "interface";
+    pub const ARG_VRFID: &str = "vrfid";
+    pub const ARG_PROTOCOL: &str = "protocol";
+
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Self::Path => Self::ARG_PATH,
+            Self::BindAddr => Self::ARG_BIND_ADDR,
+            Self::Vpc => Self::ARG_VPC,
+            Self::Vni => Self::ARG_VNI,
+            Self::Address => Self::ARG_ADDRESS,
+            Self::Prefix => Self::ARG_PREFIX,
+            Self::Mac => Self::ARG_MAC,
+            Self::Ifname => Self::ARG_IFNAME,
+            Self::VrfId => Self::ARG_VRFID,
+            Self::Protocol => Self::ARG_PROTOCOL,
+        }
+    }
+}
+
+impl FromStr for CliArgId {
+    type Err = ArgsError;
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        match value {
+            Self::ARG_PATH => Ok(Self::Path),
+            Self::ARG_BIND_ADDR => Ok(Self::BindAddr),
+            Self::ARG_VPC => Ok(Self::Vpc),
+            Self::ARG_VNI => Ok(Self::Vni),
+            Self::ARG_ADDRESS => Ok(Self::Address),
+            Self::ARG_PREFIX => Ok(Self::Prefix),
+            Self::ARG_MAC => Ok(Self::Mac),
+            Self::ARG_IFNAME => Ok(Self::Ifname),
+            Self::ARG_VRFID => Ok(Self::VrfId),
+            Self::ARG_PROTOCOL => Ok(Self::Protocol),
+            _ => Err(ArgsError::UnknownArgument(value.to_string())),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::CliArgId;
+    use std::str::FromStr;
+    use strum::IntoEnumIterator;
+
+    #[test]
+    // Test biject
+    fn test_arg_conversion() {
+        for a in CliArgId::iter() {
+            assert_eq!(a, CliArgId::from_str(a.as_str()).unwrap());
+        }
+    }
+
+    #[test]
+    // Test uniqueness of strings. Ideally, this would be enforced at build time
+    fn test_arg_string_uniqueness() {
+        use std::collections::BTreeSet;
+        let mut set = BTreeSet::new();
+        for a in CliArgId::iter() {
+            assert!(set.insert(a.as_str()));
+        }
+    }
+}
 
 /// Errors when parsing arguments
 #[derive(Error, Debug)]
@@ -23,6 +111,9 @@ pub enum ArgsError {
 
     #[error("Bad prefix format: {0}")]
     BadPrefixFormat(String),
+
+    #[error("Unknown argument: {0}")]
+    UnknownArgument(String),
 
     #[error("Unrecognized arguments")]
     UnrecognizedArgs(HashMap<String, String>),
@@ -41,7 +132,7 @@ pub enum ArgsError {
 pub struct CliArgs {
     pub connpath: Option<String>,     /* connection path; this is local */
     pub bind_address: Option<String>, /* address to bind unix sock to */
-    pub remote: RequestArgs,          /* args to send to remote */
+    pub remote: RequestArgs,          /* args to send to remote. These get serialized */
 }
 
 #[allow(unused)]
@@ -51,15 +142,15 @@ impl CliArgs {
     }
     pub fn from_args_map(mut args_map: HashMap<String, String>) -> Result<CliArgs, ArgsError> {
         let mut args = CliArgs::new();
-        if let Some(addr) = &args_map.remove("address") {
+        if let Some(addr) = &args_map.remove(CliArgId::ARG_ADDRESS) {
             let address =
                 IpAddr::from_str(addr).map_err(|_| ArgsError::BadPrefix(addr.to_owned()))?;
             args.remote.address = Some(address);
         }
-        if let Some(mac) = &args_map.remove("mac-address") {
+        if let Some(mac) = &args_map.remove(CliArgId::ARG_MAC) {
             args.remote.mac = Some(mac.to_owned());
         }
-        if let Some(prefix) = args_map.remove("prefix") {
+        if let Some(prefix) = args_map.remove(CliArgId::ARG_PREFIX) {
             if let Some((addr, len)) = prefix.split_once('/') {
                 let pfx =
                     IpAddr::from_str(addr).map_err(|_| ArgsError::BadPrefix(addr.to_owned()))?;
@@ -78,21 +169,21 @@ impl CliArgs {
                 return Err(ArgsError::BadPrefixFormat(prefix.clone()));
             }
         }
-        if let Some(path) = args_map.remove("path") {
+        if let Some(path) = args_map.remove(CliArgId::ARG_PATH) {
             if path.is_empty() {
-                return Err(ArgsError::MissingValue("path"));
+                return Err(ArgsError::MissingValue(CliArgId::ARG_PATH));
             }
             args.connpath = Some(path.clone());
         }
-        if let Some(path) = args_map.remove("bind-address") {
+        if let Some(path) = args_map.remove(CliArgId::ARG_BIND_ADDR) {
             if path.is_empty() {
-                return Err(ArgsError::MissingValue("bind-address"));
+                return Err(ArgsError::MissingValue(CliArgId::ARG_BIND_ADDR));
             }
             args.bind_address = Some(path.clone());
         }
-        if let Some(vrfid) = args_map.remove("vrfid") {
+        if let Some(vrfid) = args_map.remove(CliArgId::ARG_VRFID) {
             if vrfid.is_empty() {
-                return Err(ArgsError::MissingValue("vrfid"));
+                return Err(ArgsError::MissingValue(CliArgId::ARG_VRFID));
             }
             args.remote.vrfid = Some(
                 vrfid
@@ -100,27 +191,27 @@ impl CliArgs {
                     .map_err(|_| ArgsError::BadValue(vrfid))?,
             );
         }
-        if let Some(vpcname) = args_map.remove("vpc") {
+        if let Some(vpcname) = args_map.remove(CliArgId::ARG_VPC) {
             if vpcname.is_empty() {
-                return Err(ArgsError::MissingValue("vpc"));
+                return Err(ArgsError::MissingValue(CliArgId::ARG_VPC));
             }
             args.remote.vpc = Some(vpcname);
         }
-        if let Some(vni) = args_map.remove("vni") {
+        if let Some(vni) = args_map.remove(CliArgId::ARG_VNI) {
             if vni.is_empty() {
-                return Err(ArgsError::MissingValue("vni"));
+                return Err(ArgsError::MissingValue(CliArgId::ARG_VNI));
             }
             args.remote.vni = Some(vni.parse::<u32>().map_err(|_| ArgsError::BadValue(vni))?);
         }
-        if let Some(ifname) = args_map.remove("ifname") {
+        if let Some(ifname) = args_map.remove(CliArgId::ARG_IFNAME) {
             if ifname.is_empty() {
-                return Err(ArgsError::MissingValue("ifname"));
+                return Err(ArgsError::MissingValue(CliArgId::ARG_IFNAME));
             }
             args.remote.ifname.clone_from(&Some(ifname));
         }
-        if let Some(protocol) = args_map.remove("protocol") {
+        if let Some(protocol) = args_map.remove(CliArgId::ARG_PROTOCOL) {
             if protocol.is_empty() {
-                return Err(ArgsError::MissingValue("protocol"));
+                return Err(ArgsError::MissingValue(CliArgId::ARG_PROTOCOL));
             }
             args.remote.protocol = Some(
                 RouteProtocol::from_str(&protocol)
