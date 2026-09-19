751e875c0b9452d9328e072f7f8e082d815dcffb
Fredi Raspall <fredi@githedgehog.com>
2026-09-19 19:23:45 +0200

    feat(routing): revisit iftable tests
    Many were unfinished, scratch work and added little coverage.
    Add test that exercises both the iftable and its writer to
    make sure they behave identically.
    
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

--- routing/src/interfaces/mod.rs
+++ routing/src/interfaces/mod.rs
@@ -10,17 +10,19 @@ pub(crate) mod interface;
 #[cfg(test)]
 pub mod tests {
     use crate::RouterError;
-    use crate::fib::fibtype::FibWriter;
+    use crate::RouterVrfConfig;
+    use crate::VrfId;
+    use crate::fib::fibtable::FibTableWriter;
     use crate::interfaces::iftable::IfTable;
     use crate::interfaces::iftablerw::{IfTableReader, IfTableWriter};
     use crate::interfaces::interface::{
         IfDataDot1q, IfDataEthernet, IfState, IfType, RouterInterfaceConfig,
     };
-    use crate::rib::vrf::{RouterVrfConfig, Vrf};
+    use crate::rib::VrfTable;
     use net::eth::mac::SourceMac;
+    use net::interface::Mtu;
     use net::interface::address::IfAddr;
     use net::interface::{InterfaceIndex, InterfaceName};
-    use net::ip::UnicastIpAddr;
     use net::vlan::Vid;
     use std::net::IpAddr;
     use std::str::FromStr;
@@ -110,71 +112,217 @@ pub mod tests {
     }
 
     #[test]
-    fn test_interface_basic() {
-        /* create interface table  */
-        let mut iftable = build_test_iftable();
+    fn test_iftable_basic() {
+        const VRFID: VrfId = 123;
+        const IF_NAME: &str = "ethernet";
+        const IF_DESC: &str = "My interface";
+        const IF_ADDRESS: &str = "10.0.1.1";
+        const IF_MAC: &str = "00:aa:00:00:00:01";
 
-        /* Create a fib for the vrf created next */
-        let (fibw, _fibr) = FibWriter::new(0);
+        let mut iftable = IfTable::new();
 
-        /* Create a VRF for that fib */
-        let vrf_cfg = RouterVrfConfig::new(0, "default");
-        let mut vrf = Vrf::new(&vrf_cfg);
-        vrf.set_fibw(fibw);
+        // create interface config
+        let ifindex = 1;
+        let mut config = build_test_interface_cfg(IF_NAME, ifindex);
+        let ifindex = config.ifindex;
+        config.set_description(IF_DESC);
+        config.set_mtu(Some(Mtu::MIN));
+        config.set_admin_state(IfState::Up);
+        config.set_iftype(IfType::Ethernet(IfDataEthernet {
+            mac: SourceMac::try_from(IF_MAC).unwrap(),
+        }));
 
-        /* lookup interface with non-existent index */
-        let idx100 = InterfaceIndex::try_new(100).unwrap();
-        let iface = iftable.get_interface(idx100);
-        assert!(iface.is_none());
+        // add interface to table
+        iftable.add_interface(&config).unwrap();
+        assert_eq!(iftable.len(), 1);
+        assert!(
+            !iftable
+                .get_interface(ifindex)
+                .unwrap()
+                .is_attached_to_vrf(VRFID)
+        );
 
-        /* Lookup interface by ifindex 2 */
-        let idx2 = InterfaceIndex::try_new(2).unwrap();
-        let iface = iftable.get_interface_mut(idx2);
-        assert!(iface.is_some());
-        let eth0 = iface.unwrap();
-        assert_eq!(eth0.name.as_ref(), "eth0", "We should get eth0");
-        assert_eq!(eth0.ifindex, idx2, "eth0 has ifindex 2");
+        // attach to vrf
+        iftable.attach_iface_to_vrf(ifindex, VRFID).unwrap();
 
-        /* Add an ip address (the interface is in the iftable) */
-        let address = IpAddr::from_str("10.0.0.1").expect("Bad address");
-        let _ = eth0.add_ifaddr(IfAddr::new(address, 24).unwrap());
-        assert!(eth0.has_address(UnicastIpAddr::try_from(address).unwrap()));
+        // update adm/oper state
+        iftable.set_admin_state(ifindex, IfState::Down).unwrap();
+        iftable.set_oper_state(ifindex, IfState::Up).unwrap();
+
+        // Add same address twice
+        let address = IpAddr::from_str(IF_ADDRESS).expect("Bad address");
+        let ifaddr = IfAddr::new(address, 24).unwrap();
+        iftable.add_ifaddr(ifindex, ifaddr).unwrap();
+        iftable.add_ifaddr(ifindex, ifaddr).unwrap();
+
+        // get interface from the table and check
+        let interface = iftable.get_interface(config.ifindex).unwrap();
+        assert_eq!(interface.ifindex, ifindex);
+        assert_eq!(interface.name.to_string().as_str(), IF_NAME);
+        assert_eq!(interface.admin_state, IfState::Down);
+        assert_eq!(interface.oper_state, IfState::Up);
+        assert_eq!(
+            interface.description.as_ref().map(|d| d.as_str()),
+            Some(IF_DESC)
+        );
+        assert!(interface.has_address(ifaddr.address()));
+        assert_eq!(&interface.get_mac().unwrap().to_string(), IF_MAC);
+        assert!(interface.is_attached_to_vrf(VRFID));
+        assert_eq!(interface.vrf_attachment(), Some(VRFID));
+        let _ = interface;
+
+        // remove address
+        iftable.del_ifaddr(ifindex, ifaddr).unwrap();
+        let r = iftable.del_ifaddr(ifindex, ifaddr);
+        assert!(r.is_err_and(|e| matches!(e, RouterError::NoSuchAddress(_))));
+
+        // detach
+        iftable.detach_from_vrf(ifindex).unwrap();
+
+        // toggle states
+        iftable.set_admin_state(ifindex, IfState::Up).unwrap();
+        iftable.set_oper_state(ifindex, IfState::Down).unwrap();
+
+        // check
+        let interface = iftable.get_interface_mut(config.ifindex).unwrap();
+        assert_eq!(interface.admin_state, IfState::Up);
+        assert_eq!(interface.oper_state, IfState::Down);
+        assert!(!interface.is_attached_to_vrf(VRFID));
+        assert!(!interface.has_address(ifaddr.address()));
+
+        // attempt add interface with same ifindex
+        let r = iftable.add_interface(&config);
+        assert!(r.is_err_and(|e| matches!(e, RouterError::InterfaceExists(_))));
+
+        // remove interface
+        iftable.del_interface(ifindex).unwrap();
+        assert!(iftable.get_interface(ifindex).is_none());
+        let r = iftable.del_interface(ifindex);
+        assert!(r.is_err_and(|e| matches!(e, RouterError::NoSuchInterface(_))));
+        assert_eq!(iftable.len(), 0);
+    }
+
+    #[track_caller]
+    fn compare(reference: &IfTable, reader: &IfTableReader) {
+        let wrapped = reader.enter().unwrap();
+        assert_eq!(&*wrapped, reference);
     }
 
     #[test]
-    fn test_iftable_api() {
-        /* create interface table */
+    fn test_iftable_wrapped() {
+        const VRFID: VrfId = 123;
+        const IF_NAME: &str = "ethernet";
+        const IF_DESC: &str = "My interface";
+        const IF_ADDRESS: &str = "10.0.1.1";
+        const IF_MAC: &str = "00:aa:00:00:00:01";
+        const IF_MAC_MOD: &str = "00:bb:00:00:00:02";
+
+        let vrfconfig = RouterVrfConfig::new(VRFID, "some vrf");
+        let (fibtw, _fibtr) = FibTableWriter::new();
+        let mut vrftable = VrfTable::new(fibtw);
+        vrftable.add_vrf(&vrfconfig).unwrap();
+
+        // iftables: iftable acts as reference
         let mut iftable = IfTable::new();
+        let (mut iftw, iftr) = IfTableWriter::new();
 
-        /* create Eth0 */
-        let eth0_idx = InterfaceIndex::try_new(2).unwrap();
-        let mut eth0 = build_test_interface_cfg("eth0", eth0_idx.to_u32());
-        eth0.set_iftype(IfType::Ethernet(IfDataEthernet {
-            mac: SourceMac::try_from("00:aa:00:00:00:01").unwrap(),
+        // one interface
+        let ifindex = 1;
+        let mut config = build_test_interface_cfg(IF_NAME, ifindex);
+        let ifindex = config.ifindex;
+        config.set_description(IF_DESC);
+        config.set_mtu(Some(Mtu::MIN));
+        config.set_admin_state(IfState::Up);
+        config.set_iftype(IfType::Ethernet(IfDataEthernet {
+            mac: SourceMac::try_from(IF_MAC).unwrap(),
         }));
 
-        /* add to interface table */
-        iftable.add_interface(&eth0).expect("Should succeed");
-        assert_eq!(iftable.len(), 1, "Eth0 should be there");
+        // test interface additions
+        iftable.add_interface(&config).unwrap();
+        iftw.add_interface(config.clone()).unwrap();
+        compare(&iftable, &iftr);
 
-        /* test get_mac */
-        let iface = iftable.get_interface(eth0_idx).expect("Should be there");
-        assert_eq!(
-            SourceMac::try_from("00:aa:00:00:00:01").unwrap(),
-            iface.get_mac().unwrap()
-        );
+        // test addition reject: both reject
+        assert!(iftable.add_interface(&config).is_err());
+        assert!(iftw.add_interface(config.clone()).is_err());
 
-        /* Add interface again -- idempotence */
-        let mut eth0 = build_test_interface_cfg("eth0", eth0_idx.to_u32());
-        eth0.set_iftype(IfType::Ethernet(IfDataEthernet {
-            mac: SourceMac::try_from("00:aa:00:00:00:01").unwrap(),
+        // test attach to vrf
+        iftable.attach_iface_to_vrf(ifindex, VRFID).unwrap();
+        iftw.attach_interface_to_vrf(ifindex, VRFID, &vrftable)
+            .unwrap();
+        compare(&iftable, &iftr);
+
+        // test modify interface config or properties
+        config.set_description("modified config".into());
+        config.set_mtu(Some(Mtu::MAX));
+        config.set_iftype(IfType::Ethernet(IfDataEthernet {
+            mac: SourceMac::try_from(IF_MAC_MOD).unwrap(),
         }));
-        let iface = iftable.add_interface(&eth0);
-        assert!(iface.is_err_and(|e| matches!(e, RouterError::InterfaceExists(_))));
-        assert_eq!(iftable.len(), 1, "Only eth0 should be there");
+        iftable.mod_interface(&config).unwrap();
+        iftw.mod_interface(config.clone()).unwrap();
+        compare(&iftable, &iftr);
 
-        /* Delete eth0 by index */
-        iftable.del_interface(eth0_idx).unwrap();
-        assert_eq!(iftable.len(), 0, "No interface should be there");
+        // test add address
+        let address = IpAddr::from_str(IF_ADDRESS).expect("Bad address");
+        let ifaddr = IfAddr::new(address, 24).unwrap();
+        iftable.add_ifaddr(ifindex, ifaddr).unwrap();
+        iftw.add_ip_address(ifindex, ifaddr).unwrap();
+        compare(&iftable, &iftr);
+
+        // test add duplicate address: neither fail but don't add twice
+        iftable.add_ifaddr(ifindex, ifaddr).unwrap();
+        iftw.add_ip_address(ifindex, ifaddr).unwrap();
+        compare(&iftable, &iftr);
+
+        // test address removal
+        iftable.del_ifaddr(ifindex, ifaddr).unwrap();
+        iftw.del_ip_address(ifindex, ifaddr).unwrap();
+        compare(&iftable, &iftr);
+
+        // test removal non-existent address
+        let r1 = iftable.del_ifaddr(ifindex, ifaddr);
+        let r2 = iftw.del_ip_address(ifindex, ifaddr);
+        assert!(r1.is_err());
+        assert_eq!(r1, r2);
+        compare(&iftable, &iftr);
+
+        // test set admin state
+        iftable.set_admin_state(ifindex, IfState::Down).unwrap();
+        iftw.set_iface_admin_state(ifindex, IfState::Down).unwrap();
+        compare(&iftable, &iftr);
+
+        // test set oper state
+        iftable.set_oper_state(ifindex, IfState::Up).unwrap();
+        iftw.set_iface_oper_state(ifindex, IfState::Up).unwrap();
+        compare(&iftable, &iftr);
+
+        // test detach from vrf
+        iftable.detach_from_vrf(ifindex).unwrap();
+        iftw.detach_interface(ifindex).unwrap();
+        compare(&iftable, &iftr);
+
+        // test detach reject
+        let r1 = iftable.detach_from_vrf(ifindex);
+        let r2 = iftw.detach_interface(ifindex);
+        assert!(r1.is_err());
+        assert_eq!(r1, r2);
+        compare(&iftable, &iftr);
+
+        // test interface removal
+        iftable.del_interface(ifindex).unwrap();
+        iftw.del_interface(ifindex).unwrap();
+        compare(&iftable, &iftr);
+
+        // test interface removal reject
+        let r1 = iftable.del_interface(ifindex);
+        let r2 = iftw.del_interface(ifindex);
+        assert_eq!(r1, r2);
+        compare(&iftable, &iftr);
+
+        // test non existent interface update
+        let r1 = iftw.mod_interface(config.clone());
+        let r2 = iftable.mod_interface(&config);
+        assert_eq!(r1, r2);
+        compare(&iftable, &iftr);
     }
 }
